38dd627d214c1cdeaa4492013d7999cde09c2122
Mallory Row <mallory.row@noaa.gov>
2022-06-29 20:05:39 +0000

    Remove workaround for using MET v10.1.1 on WCOSS2
    Refs: #1

--- ush/global_det/global_det_atmos_load_modules.sh
+++ ush/global_det/global_det_atmos_load_modules.sh
@@ -40,9 +40,5 @@ fi
 module use ${HOMEevs}/modulefiles
 module load run.global_det_atmos.${machine}
 module list
-if [ $machine = WCOSS2 ]; then
-    export MET_BASE=/apps/ops/para/libs/intel/19.1.3.304/met/10.1.1/share/met
-    export MET_ROOT=/apps/ops/para/libs/intel/19.1.3.304/met/10.1.1
-    export PATH=/apps/ops/para/libs/intel/19.1.3.304/met/10.1.1/bin:${PATH}
-fi
+
 echo "END: $(basename ${BASH_SOURCE[0]})"
