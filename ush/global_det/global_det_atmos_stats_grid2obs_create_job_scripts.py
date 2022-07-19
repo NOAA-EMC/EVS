7cb0adaa43fd0030d7e0805e4fd0f39744ca085b
Mallory Row <mallory.row@noaa.gov>
2022-07-19 13:19:14 +0000

    Break down grid-to-obs stats Winds jobs
    Refs: #1

--- ush/global_det/global_det_atmos_stats_grid2obs_create_job_scripts.py
+++ ush/global_det/global_det_atmos_stats_grid2obs_create_job_scripts.py
@@ -256,7 +256,7 @@ generate_jobs_dict = {
         'Temp': {'env': {'prepbufr': 'gdas',
                          'obs_window': '1800',
                          'msg_type': "'ADPUPA, AIRCAR, AIRCFT'",
-                         'var1_name': 'HGT',
+                         'var1_name': 'TMP',
                          'var1_levels': ("'P1000, P925, P850, "
                                          +"P700, P500, P400, "
                                          +"P300, P250, P200, "
@@ -268,7 +268,7 @@ generate_jobs_dict = {
                                   'PointStat_fcstGLOBAL_DET_'
                                   +'obsPrepbufr.conf'
                               )]},
-        'Winds': {'env': {'prepbufr': 'gdas',
+        'UWind': {'env': {'prepbufr': 'gdas',
                           'obs_window': '1800',
                           'msg_type': "'ADPUPA, AIRCAR, AIRCFT'",
                           'var1_name': 'UGRD',
@@ -278,18 +278,48 @@ generate_jobs_dict = {
                                           +"P150, P100, P50, "
                                           +"P20, P10, P5, P1'"),
                           'var1_options': '',
-                          'var2_name': 'VGRD',
-                          'var2_levels': ("'P1000, P925, P850, "
+                          'met_config_overrides': ''},
+                  'commands': [gda_util.metplus_command(
+                                   'PointStat_fcstGLOBAL_DET_'
+                                   +'obsPrepbufr.conf'
+                               )]},
+        'VWind': {'env': {'prepbufr': 'gdas',
+                          'obs_window': '1800',
+                          'msg_type': "'ADPUPA, AIRCAR, AIRCFT'",
+                          'var1_name': 'VGRD',
+                          'var1_levels': ("'P1000, P925, P850, "
                                           +"P700, P500, P400, "
                                           +"P300, P250, P200, "
                                           +"P150, P100, P50, "
                                           +"P20, P10, P5, P1'"),
-                          'var2_options': '',
+                          'var1_options': '',
                           'met_config_overrides': ''},
                   'commands': [gda_util.metplus_command(
                                    'PointStat_fcstGLOBAL_DET_'
-                                   +'obsPrepbufr_Winds.conf'
-                               )]}
+                                   +'obsPrepbufr.conf'
+                               )]},
+        'VectorWind': {'env': {'prepbufr': 'gdas',
+                               'obs_window': '1800',
+                               'msg_type': "'ADPUPA, AIRCAR, AIRCFT'",
+                               'var1_name': 'UGRD',
+                               'var1_levels': ("'P1000, P925, P850, "
+                                               +"P700, P500, P400, "
+                                               +"P300, P250, P200, "
+                                               +"P150, P100, P50, "
+                                               +"P20, P10, P5, P1'"),
+                              'var1_options': '',
+                              'var2_name': 'VGRD',
+                              'var2_levels': ("'P1000, P925, P850, "
+                                              +"P700, P500, P400, "
+                                              +"P300, P250, P200, "
+                                              +"P150, P100, P50, "
+                                              +"P20, P10, P5, P1'"),
+                              'var2_options': '',
+                              'met_config_overrides': ''},
+                      'commands': [gda_util.metplus_command(
+                                       'PointStat_fcstGLOBAL_DET_'
+                                       +'obsPrepbufr_VectorWind.conf'
+                                   )]}
     },
     'sfc': {
         'CAPEMixedLayer': {'env': {'prepbufr': 'gdas',
