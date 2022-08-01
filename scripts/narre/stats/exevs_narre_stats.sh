28058ce476d268dc7f78d9aecabf97ef77bfcb38
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2022-08-01 15:38:33 +0000

    send first version of narre to folk feature/narre

--- scripts/narre/stats/exevs_narre_stats.sh
+++ scripts/narre/stats/exevs_narre_stats.sh
@@ -1,1 +1,40 @@
-example
+#!/bin/ksh
+#################################################################
+# Purpose:   To run grid-to-grid verification on all global ensembles
+#
+# Log History:  12/01/2021 Binbin Zhou  
+################################################################
+set -x
+
+export WORK=$DATA
+cd $WORK
+
+export MET_bin_exec='bin'
+export log_met_output_to_metplus=''
+export metplus_verbosity=2
+export met_verbosity=2
+
+
+msg="$job HAS BEGUN"
+postmsg "$jlogfile" "$msg"
+
+
+#############################################################
+# Step 0: Run copygb to convert URMA data to 4km WRF grid
+#############################################################
+
+if [ $stats = yes ] ; then
+ $USHevs/narre/evs_narre_stats.sh  
+fi
+
+
+#if [ $gather = yes ] ; then
+# $USHevs/narre/stats/evs_narre_gather.sh
+#fi
+
+msg="JOB $job HAS COMPLETED NORMALLY"
+postmsg "$jlogfile" "$msg"
+
+
+
+exit 0
---
+++ ush/narre/evs_get_narre_stat_file_link.sh
@@ -0,0 +1,21 @@
+#!/bin/ksh
+
+set -x 
+
+day=$1
+verify=grid2obs
+MODEL=NARRE_MEAN
+archive=$output_base_dir
+narre_stat=${COM_OUT}/stats/narre
+
+yyyymm=${day:0:6} 
+
+narre_archive_yyyymm=${archive}/grid2obs/$MODEL/$yyyymm
+mkdir -p $narre_archive_yyyymm
+
+cd ${narre_archive_yyyymm}
+
+ln -sf $GATHER_STAT/${MODEL}_${verify}_${day}.stat ${MODEL}_${day}.stat
+
+  
+
