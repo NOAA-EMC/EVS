e95badf17bd2d55098d1cd976a48a56dc364c73b
perry shafran <perry.shafran@clogin04.cactus.wcoss2.ncep.noaa.gov>
2022-10-17 20:48:31 +0000

    Restoring run.ver and machine.conf files

--- scripts/cam/plots/exevs_nam_firewxnest_grid2obs_plots.sh
+++ scripts/cam/plots/exevs_nam_firewxnest_grid2obs_plots.sh
@@ -34,6 +34,9 @@ while [ $DATE -ge $ENDDATE ]; do
         cp ${COMINnam}.$DAY/*stat $STATDIR/${MODELNAME}_${RUN}_${VERIF_CASE}_v${DAY}.stat 
         fi
 
+	sed "s/$model1/$MODELNAME/g" $STATDIR/${MODELNAME}_${RUN}_${VERIF_CASE}_v${DAY}.stat > $STATDIR/temp.stat
+	mv $STATDIR/temp.stat $STATDIR/${MODELNAME}_${RUN}_${VERIF_CASE}_v${DAY}.stat
+
 	DATE=`/apps/ops/prod/nco/core/prod_util.v2.0.7/exec/ndate -24 $DATE`
 
 done
@@ -42,8 +45,36 @@ for var in TMP2m DPT2m RH2m
 do
 	export var
 	export lev=Z2
+	export linetype=SL1L2
 	sh $USHevs/${COMPONENT}/py_plotting.config
+
+        mv ${DATA}/lead_average* ${PLOTDIR}/BCRMSE_${var}_${lev}_G221_VALID${PDYm31}to${VDATE}_DIEOFF.png
 done
+
+for var in UGRD10m VGRD10m UGRD_VGRD10m WIND10m
+do
+	export var
+	export lev=Z10
+	if [ $var = UGRD_VGRD10m ]
+        then
+         export linetype=VL1L2
+	else
+         export linetype=SL1L2
+	fi
+	sh $USHevs/${COMPONENT}/py_plotting.config
+
+        mv ${DATA}/lead_average* ${PLOTDIR}/BCRMSE_${var}_${lev}_G221_VALID${PDYm31}to${VDATE}_DIEOFF.png
+done
+
+for var in GUSTsfc
+do
+	export var
+	export lev=Z0
+	sh $USHevs/${COMPONENT}/py_plotting.config
+
+        mv ${DATA}/lead_average* ${PLOTDIR}/BCRMSE_${var}_${lev}_G221_VALID${PDYm31}to${VDATE}_DIEOFF.png
+done
+
 
 exit
 
