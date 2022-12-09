Improve plotting logic for unit conversion, fix bugs

ush/cam/performance_diagram.py
@@ -246,6 +246,11 @@ def plot_performance_diagram(df: pd.DataFrame, logger: logging.Logger,
         df, bool_success = plot_util.equalize_samples(logger, df, group_by)
         if not bool_success:
             sample_equalization = False
+        if df.empty:
+            logger.warning(f"Empty Dataframe. Continuing onto next plot...")
+            plt.close(num)
+            logger.info("========================================")
+            return None
     df_groups = df.groupby(group_by)
     # Aggregate unit statistics before calculating metrics
     df_aggregated = df_groups.sum()
@@ -275,7 +280,7 @@ def plot_performance_diagram(df: pd.DataFrame, logger: logging.Logger,
     metric_long_names = []
     for metric_name in [metric1_name, metric2_name, metric3_name]:
         stat_output = plot_util.calculate_stat(
-            logger, df_aggregated, str(metric_name).lower()
+            logger, df_aggregated, str(metric_name).lower(), [None, None]
         )
         df_aggregated[str(metric_name).upper()] = stat_output[0]
         metric_long_names.append(stat_output[2])
@@ -283,7 +288,7 @@ def plot_performance_diagram(df: pd.DataFrame, logger: logging.Logger,
             ci_output = df_groups.apply(
                 lambda x: plot_util.calculate_bootstrap_ci(
                     logger, bs_method, x, str(metric_name).lower(), bs_nrep,
-                    ci_lev, bs_min_samp
+                    ci_lev, bs_min_samp, [None, None]
                 )
             )
             if any(ci_output['STATUS'] == 1):
@@ -607,7 +612,10 @@ def plot_performance_diagram(df: pd.DataFrame, logger: logging.Logger,
     units = df['FCST_UNITS'].tolist()[0]
     if units in reference.unit_conversions:
         thresh_labels = [float(tlab) for tlab in thresh_labels]
-        thresh_labels = reference.unit_conversions[units]['formula'](thresh_labels)
+        thresh_labels = reference.unit_conversions[units]['formula'](
+            thresh_labels,
+            rounding=True
+        )
         thresh_diff_categories = np.array([
             [np.power(10., y)]
             for y in [-5,-4,-3,-2,-1,0,1,2,3,4,5]
@@ -845,14 +853,14 @@ def plot_performance_diagram(df: pd.DataFrame, logger: logging.Logger,
             level_savename = f'SB'
         else:
             level_string = ''
-            level_savename = '{level}'
+            level_savename = f'{level}'
     elif (str(verif_type).lower() 
             in ['sfc', 'conus_sfc', 'polar_sfc', 'mrms', 'metar']):
         if 'Z' in str(level):
             if str(level).upper() == 'Z0':
                 if str(var_long_name_key).upper() in ['MLSP', 'MSLET', 'MSLMA', 'PRMSL']:
                     level_string = ''
-                    level_savename = '{level}'
+                    level_savename = f'{level}'
                 else:
                     level_string = 'Surface '
                     level_savename = 'SFC'
@@ -866,7 +874,7 @@ def plot_performance_diagram(df: pd.DataFrame, logger: logging.Logger,
                     level_savename = f'{level_num}M'
         elif 'L' in str(level) or 'A' in str(level):
             level_string = ''
-            level_savename = '{level}'
+            level_savename = f'{level}'
         else:
             level_string = f'{level} '
             level_savename = f'{level}'

ush/cam/plot_util.py
@@ -627,7 +627,7 @@ def get_stat_plot_name(logger, stat):
    return stat_plot_name
 
 def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level, 
-                           bs_min_samp):
+                           bs_min_samp, conversion):
    """! Calculate the upper and lower bound bootstrap statistic from the 
         data from the read in MET .stat file(s)
 
@@ -667,6 +667,10 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
       else:
          stat_values = model_data.loc[:]['TOTAL']
    else:
+      if np.any(conversion):
+         bool_convert = True
+      else:
+         bool_convert = False
       if all(elem in model_data_columns for elem in
             ['FBAR', 'OBAR', 'MAE']):
          line_type = 'SL1L2'
@@ -676,6 +680,26 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
          fobar = model_data.loc[:]['FOBAR']
          ffbar = model_data.loc[:]['FFBAR']
          oobar = model_data.loc[:]['OOBAR']
+         if bool_convert:
+             coef, const = conversion
+             fbar = coef*fbar+const
+             obar = coef*obar+const
+             fobar = (
+                np.power(coef, 2)*fobar 
+                + coef*const*fbar 
+                + coef*const*obar
+                + np.power(const, 2)
+             )
+             ffbar = (
+                np.power(coef, 2)*ffbar 
+                + 2.*coef*const*fbar 
+                + np.power(const, 2)
+             )
+             oobar = (
+                np.power(coef, 2)*oobar 
+                + 2.*coef*const*obar
+                + np.power(const, 2)
+             )
       elif all(elem in model_data_columns for elem in 
             ['FABAR', 'OABAR', 'MAE']):
          line_type = 'SAL1L2'
@@ -685,6 +709,19 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
          foabar = model_data.loc[:]['FOABAR']
          ffabar = model_data.loc[:]['FFABAR']
          ooabar = model_data.loc[:]['OOABAR']
+         if bool_convert:
+             coef, const = conversion
+             fabar = coef*fabar
+             oabar = coef*oabar
+             foabar = (
+                np.power(coef, 2)*foabar 
+             )
+             ffabar = (
+                np.power(coef, 2)*ffabar 
+             )
+             ooabar = (
+                np.power(coef, 2)*ooabar 
+             )
       elif all(elem in model_data_columns for elem in
             ['UFBAR', 'VFBAR']):
          line_type = 'VL1L2'
@@ -696,6 +733,27 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
          uvfobar = model_data.loc[:]['UVFOBAR']
          uvffbar = model_data.loc[:]['UVFFBAR']
          uvoobar = model_data.loc[:]['UVOOBAR']
+         if bool_convert:
+             coef, const = conversion
+             ufbar = coef*ufbar+const
+             vfbar = coef*vfbar+const
+             uobar = coef*uobar+const
+             vobar = coef*vobar+const
+             uvfobar = (
+                np.power(coef, 2)*uvfobar 
+                + coef*const*(ufbar + uobar + vfbar + vobar) 
+                + np.power(const, 2)
+             )
+             uvffbar = (
+                np.power(coef, 2)*uvffbar 
+                + 2.*coef*const*(ufbar + vfbar) 
+                + np.power(const, 2)
+             )
+             uvoobar = (
+                np.power(coef, 2)*uvoobar 
+                + 2.*coef*const*(uobar + vobar) 
+                + np.power(const, 2)
+             )
       elif all(elem in model_data_columns for elem in 
             ['UFABAR', 'VFABAR']):
          line_type = 'VAL1L2'
@@ -707,6 +765,21 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
          uvfoabar = model_data.loc[:]['UVFOABAR']
          uvffabar = model_data.loc[:]['UVFFABAR']
          uvooabar = model_data.loc[:]['UVOOABAR']
+         if bool_convert:
+             coef, const = conversion
+             ufabar = coef*ufabar
+             vfabar = coef*vfabar
+             uoabar = coef*uoabar
+             voabar = coef*voabar
+             uvfoabar = (
+                np.power(coef, 2)*uvfoabar 
+             )
+             uvffabar = (
+                np.power(coef, 2)*uvffabar 
+             )
+             uvooabar = (
+                np.power(coef, 2)*uvooabar 
+             )
       elif all(elem in model_data_columns for elem in
             ['VDIFF_SPEED', 'VDIFF_DIR']):
          line_type = 'VCNT'
@@ -727,6 +800,11 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
          vdiff_dir = model_data.loc[:]['VDIFF_DIR']
          speed_err = model_data.loc[:]['SPEED_ERR']
          dir_err = model_data.loc[:]['DIR_ERR']
+         if bool_convert:
+            logger.error(
+               f"Cannot convert columns for line_type \"{line_type}\""
+            )
+            exit(1) 
       elif all(elem in model_data_columns for elem in
             ['FY_OY', 'FN_ON']):
          line_type = 'CTC'
@@ -1201,7 +1279,7 @@ def calculate_bootstrap_ci(logger, bs_method, model_data, stat, nrepl, level,
       dict(CI_LOWER=[stat_ci_lower], CI_UPPER=[stat_ci_upper], STATUS=[status])
    )
 
-def calculate_stat(logger, model_data, stat):
+def calculate_stat(logger, model_data, stat, conversion):
    """! Calculate the statistic from the data from the
         read in MET .stat file(s)
 
@@ -1230,6 +1308,10 @@ def calculate_stat(logger, model_data, stat):
       else:
          stat_values = model_data.loc[:]['TOTAL']
    else:
+      if np.any(conversion):
+         bool_convert = True
+      else:
+         bool_convert = False
       if all(elem in model_data_columns for elem in
             ['FBAR', 'OBAR', 'MAE']):
          line_type = 'SL1L2'
@@ -1238,6 +1320,26 @@ def calculate_stat(logger, model_data, stat):
          fobar = model_data.loc[:]['FOBAR']
          ffbar = model_data.loc[:]['FFBAR']
          oobar = model_data.loc[:]['OOBAR']
+         if bool_convert:
+             coef, const = conversion
+             fbar = coef*fbar+const
+             obar = coef*obar+const
+             fobar = (
+                np.power(coef, 2)*fobar 
+                + coef*const*fbar 
+                + coef*const*obar
+                + np.power(const, 2)
+             )
+             ffbar = (
+                np.power(coef, 2)*ffbar 
+                + 2.*coef*const*fbar 
+                + np.power(const, 2)
+             )
+             oobar = (
+                np.power(coef, 2)*oobar 
+                + 2.*coef*const*obar
+                + np.power(const, 2)
+             )
       elif all(elem in model_data_columns for elem in 
             ['FABAR', 'OABAR', 'MAE']):
          line_type = 'SAL1L2'
@@ -1246,6 +1348,19 @@ def calculate_stat(logger, model_data, stat):
          foabar = model_data.loc[:]['FOABAR']
          ffabar = model_data.loc[:]['FFABAR']
          ooabar = model_data.loc[:]['OOABAR']
+         if bool_convert:
+             coef, const = conversion
+             fabar = coef*fabar
+             oabar = coef*oabar
+             foabar = (
+                np.power(coef, 2)*foabar 
+             )
+             ffabar = (
+                np.power(coef, 2)*ffabar 
+             )
+             ooabar = (
+                np.power(coef, 2)*ooabar 
+             )
       elif all(elem in model_data_columns for elem in
             ['UFBAR', 'VFBAR']):
          line_type = 'VL1L2'
@@ -1256,6 +1371,27 @@ def calculate_stat(logger, model_data, stat):
          uvfobar = model_data.loc[:]['UVFOBAR']
          uvffbar = model_data.loc[:]['UVFFBAR']
          uvoobar = model_data.loc[:]['UVOOBAR']
+         if bool_convert:
+             coef, const = conversion
+             ufbar = coef*ufbar+const
+             vfbar = coef*vfbar+const
+             uobar = coef*uobar+const
+             vobar = coef*vobar+const
+             uvfobar = (
+                np.power(coef, 2)*uvfobar 
+                + coef*const*(ufbar + uobar + vfbar + vobar) 
+                + np.power(const, 2)
+             )
+             uvffbar = (
+                np.power(coef, 2)*uvffbar 
+                + 2.*coef*const*(ufbar + vfbar) 
+                + np.power(const, 2)
+             )
+             uvoobar = (
+                np.power(coef, 2)*uvoobar 
+                + 2.*coef*const*(uobar + vobar) 
+                + np.power(const, 2)
+             )
       elif all(elem in model_data_columns for elem in 
             ['UFABAR', 'VFABAR']):
          line_type = 'VAL1L2'
@@ -1266,6 +1402,21 @@ def calculate_stat(logger, model_data, stat):
          uvfoabar = model_data.loc[:]['UVFOABAR']
          uvffabar = model_data.loc[:]['UVFFABAR']
          uvooabar = model_data.loc[:]['UVOOABAR']
+         if bool_convert:
+             coef, const = conversion
+             ufabar = coef*ufabar
+             vfabar = coef*vfabar
+             uoabar = coef*uoabar
+             voabar = coef*voabar
+             uvfoabar = (
+                np.power(coef, 2)*uvfoabar 
+             )
+             uvffabar = (
+                np.power(coef, 2)*uvffabar 
+             )
+             uvooabar = (
+                np.power(coef, 2)*uvooabar 
+             )
       elif all(elem in model_data_columns for elem in
             ['VDIFF_SPEED', 'VDIFF_DIR']):
          line_type = 'VCNT'
@@ -1285,6 +1436,11 @@ def calculate_stat(logger, model_data, stat):
          vdiff_dir = model_data.loc[:]['VDIFF_DIR']
          speed_err = model_data.loc[:]['SPEED_ERR']
          dir_err = model_data.loc[:]['DIR_ERR']
+         if bool_convert:
+            logger.error(
+               f"Cannot convert column units for line_type \"{line_type}\""
+            )
+            exit(1) 
       elif all(elem in model_data_columns for elem in
             ['FY_OY', 'FN_ON']):
          line_type = 'CTC'
@@ -1670,11 +1826,12 @@ def equalize_samples(logger, df, group_by):
     df_equalized = df_equalized.loc[
         df_equalized[cols_to_check+['MODEL']].drop_duplicates().index
     ]
+    # Remove duplicates again, this time among both the columns 
     # Regroup the data and move forward with these groups!
     df_equalized_groups = df_equalized.groupby(group_by)
     # Check that groups are indeed equally sized for each independent variable
     df_groups_sizes = df_equalized_groups.size()
-    if df_groups_sizes:
+    if df_groups_sizes.size > 0:
         df_groups_sizes.index = df_groups_sizes.index.set_levels(
             df_groups_sizes.index.levels[-1].astype(str), level=-1
         )
@@ -1684,7 +1841,11 @@ def equalize_samples(logger, df, group_by):
             in np.unique(np.array(list(df_groups_sizes.keys())).T[1])
         ])
     else:
-        data_are_equalized = False
+        logger.info(
+            "Sample equalization was successful but resulted in an empty"
+            + f" dataframe."
+        )
+        data_are_equalized = True
     if data_are_equalized:
         logger.info(
             "Data were successfully equalized along the independent"

ush/cam/settings.py
@@ -3283,22 +3283,76 @@ class Reference():
             },
         }
 
+    '''
+    Each formula in the formulas class needs to have the capability of providing three things.
+    1) Simply, the complete conversion of the input quantity into the output units
+    2) The rounded conversion of the input to the output (e.g., for displaying some thresholds)
+    3) The coefficient and the constant that composes the formula (for converting base stats)
+    '''
     class formulas():
-        def mm_to_in(mm_vals):
-            inch_vals = np.divide(mm_vals, 25.4)
-            return inch_vals
-        def K_to_F(K_vals):
-            F_vals = (((np.array(K_vals)-273.15)*9./5.)+32.).round()
-            return F_vals
-        def C_to_F(C_vals):
-            F_vals = (np.array(C_vals)*9./5.)+32.
-            return F_vals
-        def mps_to_kt(mps_vals):
-            kt_vals = np.array(mps_vals)*1.94384449412
-            return kt_vals
-        def gpm_to_kft(gpm_vals):
-            kft_vals = (np.array(gpm_vals)/304.8).round(decimals=2)
-            return kft_vals
-        def m_to_mi(m_vals):
-            mi_vals = (np.array(m_vals)/1609.34).round(decimals=2)
-            return mi_vals
+        def mm_to_in(mm_vals, rounding=False, return_terms=False):
+            if return_terms:
+                M = np.divide(1., 25.4)
+                C = 0.
+                return M, C
+            else:
+                if rounding:
+                    inch_vals = np.divide(mm_vals, 25.4).round(decimals=2)
+                else:
+                    inch_vals = np.divide(mm_vals, 25.4)
+                return inch_vals
+        def K_to_F(K_vals, rounding=False, return_terms=False):
+            if return_terms:
+                M = np.divide(9., 5.)
+                C = ((-273.15)*9./5.)+32.
+                return M, C
+            else:
+                if rounding:
+                    F_vals = (((np.array(K_vals)-273.15)*9./5.)+32.).round()
+                else:
+                    F_vals = ((np.array(K_vals)-273.15)*9./5)+32.
+                return F_vals
+        def C_to_F(C_vals, rounding=False, return_terms=False):
+            if return_terms:
+                M = np.divide(9., 5.)
+                C = 32.
+                return M, C
+            else:
+                if rounding:
+                    F_vals = ((np.array(C_vals)*9./5.)+32.).round()
+                else:
+                    F_vals = (np.array(C_vals)*9./5.)+32.
+                return F_vals
+        def mps_to_kt(mps_vals, rounding=False, return_terms=False):
+            if return_terms:
+                M = 1.94384449412
+                C = 0.
+                return M, C
+            else:
+                if rounding:
+                    kt_vals = (np.multiply(mps_vals, 1.94384449412)).round()
+                else:
+                    kt_vals = np.multiply(mps_vals, 1.94384449412)
+                return kt_vals
+        def gpm_to_kft(gpm_vals, rounding=False, return_terms=False):
+            if return_terms:
+                M = np.divide(1., 304.8)
+                C = 0.
+                return M, C
+            else:
+                if rounding:
+                    kft_vals = (np.divide(gpm_vals, 304.8)).round(decimals=2)
+                else:
+                    kft_vals = np.divide(gpm_vals, 304.8)
+                return kft_vals
+        def m_to_mi(m_vals, rounding=False, return_terms=False):
+            if return_terms:
+                M = np.divide(1., 1609.34)
+                C = 0.
+                return M, C
+            else:
+                if rounding:
+                    mi_vals = (np.divide(m_vals, 1609.34)).round(decimals=2)
+                else:
+                    mi_vals = np.divide(m_vals, 1609.34)
+                return mi_vals