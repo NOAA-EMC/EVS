2e9b5e606c22b65a6c55dc281cd340d4597bd4e1
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2022-12-15 18:38:37 +0000

    Update conf files

--- scripts/global_ens/plots/exevs_global_ens_atmos_profile_plots.sh
+++ scripts/global_ens/plots/exevs_global_ens_atmos_profile_plots.sh
@@ -55,53 +55,54 @@ verif_case=$VERIF_CASE
 
 > run_all_poe.sh
 
-for stats in acc bias_mae crpss rmse_spread ; do 
-if [ $stats = acc ] ; then
+for stats in rmse_spread bias ; do 
+ if [ $stats = acc ] ; then
   stat_list='acc'
   line_tp='sal1l2'
-  #verif_type='anom'
-elif [ $stats = bias ] ; then
-  stat_list='bias'
-  line_tp='sl1l2'
-  #verif_type='pres'
-elif [ $stats = mae ] ; then
-  stat_list='mae'
-  line_tp='sl1l2'
-  #verif_type='pres'
-elif [ $stats = bias_mae  ] ; then
+  VARs='PRMSL TMP2m DPT2m UGRD10m VGRD10m'
+  score_types='time_series lead_average'
+ elif [ $stats = bias_mae  ] ; then
   stat_list='bias, mae'
   line_tp='sl1l2'
-  #verif_type='pres'
-elif [ $stats = crps  ] ; then
-  stat_list='crps'
-  line_tp='ecnt'
-  #verif_type='pres'
-elif [ $stats = crpss ] ; then
+  VARs='PRMSL TMP2m DPT2m UGRD10m VGRD10m RH2m'
+  score_types='time_series lead_average'
+ elif [ $stats = crpss ] ; then
   stat_list='crpss'
   line_tp='ecnt'
-  #verif_type='pres'
-elif [ $stats = rmse_spread  ] ; then
+  score_types='time_series lead_average'
+  VARs='PRMSL TMP2m DPT2m UGRD10m VGRD10m'
+ elif [ $stats = rmse_spread  ] ; then
   stat_list='rmse, spread'
   line_tp='ecnt'
-  #verif_type='pres'
-else
+  VARs='PRMSL TMP2m DPT2m UGRD10m VGRD10m RH2m'
+  score_types='time_series lead_average'
+ elif [ $stats = ets_fbias ] ; then
+  stat_list='ets, fbias'
+  line_tp='ctc'
+  VARs='CAPEsfc'
+  thresh_cape='>=250, >=500, >=1000, >=2000'
+  score_types='time_series lead_average'
+ elif [ $stats = sratio_pod_csi ] ; then
+  stat_list='sratio, pod, csi'
+  line_tp='ctc'
+  VARs='CAPEsfc'
+  thresh_cape='>=250, >=500, >=1000, >=2000'
+  score_types='performance_diagram'
+ else
   echo $stats is wrong stat
   exit
-fi   
+ fi   
 
- for score_type in time_series lead_average ; do
+ for score_type in $score_types ; do
+
+  export fcst_init_hour="0,12"
+  export fcst_valid_hour="0,12"
+  valid_time='valid00z_12z'
+  init_time='init00z_12z'
 
   if [ $score_type = time_series ] ; then
-    export fcst_init_hour="0,12"
-    export fcst_valid_hour="0,12"
-    valid_time='valid00z_12z'
-    init_time='init00z_12z'
     export fcst_leads="120 240 360"
-  else
-    export fcst_init_hour="0,12"
-    export fcst_valid_hour="0,12"
-    valid_time='valid00z_12z'
-    init_time='init00z_12z'
+  else 
     export fcst_leads="vs_lead" 
   fi
  
@@ -113,17 +114,15 @@ fi
         export fcst_lead=$lead
    fi
 
-    for VAR in HGT TMP UGRD VGRD PRMSL ; do 
+    for VAR in $VARs ; do 
 
        var=`echo $VAR | tr '[A-Z]' '[a-z]'` 
 	    
-       if [ $VAR = HGT ] ; then
-          FCST_LEVEL_values="P500 P700 P1000"
-       elif [ $VAR = TMP ] ; then
-          FCST_LEVEL_values="P500 P850"
-       elif [ $VAR = UGRD ] || [ $VAR = VGRD ] ; then
-          FCST_LEVEL_values="P850 P250 Z10"
-       elif [ $VAR = PRMSL ] ; then
+       if [ $VAR = TMP2m ] || [ $VAR = DPT2m ] || [ $VAR = RH2m ] ; then 
+          FCST_LEVEL_values="Z2"
+       elif [ $VAR = UGRD10m ] || [ $VAR = VGRD10m ] ; then
+          FCST_LEVEL_values="Z10"
+       elif [ $VAR = PRMSL ] || [ $VAR = CAPEsfc ] ; then
           FCST_LEVEL_values="L0"
        fi
 
@@ -137,15 +136,7 @@ fi
 
          > run_${stats}.${score_type}.${lead}.${VAR}.${FCST_LEVEL_value}.${line_type}.sh  
 
-        if [ $stats = acc ] ; then
-	   verif_type=anom
-        else
-           if [ $FCST_LEVEL_value = L0 ] || [ $FCST_LEVEL_value = Z10 ] ;  then
-             verif_type=sfc
-           else
-	     verif_type=pres
-	   fi
-	fi 
+        verif_type=conus_sfc
 
         echo "export PLOT_TYPE=$score_type" >> run_${stats}.${score_type}.${lead}.${VAR}.${FCST_LEVEL_value}.${line_type}.sh
 
@@ -176,7 +167,12 @@ fi
          echo "export interp=NEAREST" >> run_${stats}.${score_type}.${lead}.${VAR}.${FCST_LEVEL_value}.${line_type}.sh
          echo "export score_py=$score_type" >> run_${stats}.${score_type}.${lead}.${VAR}.${FCST_LEVEL_value}.${line_type}.sh
 
-	 thresh=' '
+	 if [ $VAR = CAPEsfc ] && [ $line_type = ctc ] ; then
+	   thresh=$thresh_cape
+	 else
+	   thresh=' '
+	 fi 
+
          sed -e "s!model_list!$models!g" -e "s!stat_list!$stat_list!g"  -e "s!thresh_list!$thresh!g"  -e "s!fcst_init_hour!$fcst_init_hour!g" -e "s!fcst_valid_hour!$fcst_valid_hour!g" -e "s!fcst_lead!$fcst_lead!g"  $USHevs/global_ens/evs_gens_atmos_plots_config.sh > run_py.${stats}.${score_type}.${lead}.${VAR}.${FCST_LEVEL_value}.${line_type}.sh
 
          chmod +x  run_py.${stats}.${score_type}.${lead}.${VAR}.${FCST_LEVEL_value}.${line_type}.sh
@@ -203,25 +199,41 @@ chmod +x run_all_poe.sh
 
 if [ $run_mpi = yes ] ; then
   export LD_LIBRARY_PATH=/apps/dev/pmi-fix:$LD_LIBRARY_PATH
-   mpiexec -np 192 -ppn 192 --cpu-bind verbose,depth cfp run_all_poe.sh
+   mpiexec -np 93 -ppn 93 --cpu-bind verbose,depth cfp run_all_poe.sh
 else
  sh run_all_poe.sh
 fi
 
-
 cd $plot_dir
-for stats in acc bias_mae crpss rmse_spread ; do
+
+for domain in g003 nhem shem tropics conus ; do
+   mv performance_diagram_regional_${domain}_valid_00z_12z_cape_f12_to_f384_ge250ge500ge1000ge2000.png  evs.global_ens.ctc.cape_l0.last31days.perfdiag_valid_00z_12z_f12_to_f384.${domain}.png
+
+done
+
+for stats in  acc bias_mae crpss rmse_spread ets_fbias ; do
  for score_type in time_series lead_average ; do
 
-  if [ $score_type = time_series ] ; then
-    leads='_f120.png _f240.png _f360.png'
-    scoretype='timeseries' 
+  if [ $stats = ets_fbias ] ; then
+    if [ $score_type = time_series ] ; then
+      leads='_f120_ge250ge500ge1000ge2000.png _f240_ge250ge500ge1000ge2000.png _f360_ge250ge500ge1000ge2000.png'
+      scoretype='timeseries'
+    elif [ $score_type = lead_average ] ; then
+      leads='_ge250ge500ge1000ge2000.png'
+      scoretype='fhrmean'
+    fi
+    vars='cape'
+  else
 
-  elif [ $score_type = lead_average ] ; then
-    leads='.png'
-    scoretype='fhrmean'
+    if [ $score_type = time_series ] ; then
+      leads='_f120.png _f240.png _f360.png'
+      scoretype='timeseries' 
+    elif [ $score_type = lead_average ] ; then
+      leads='.png'
+      scoretype='fhrmean'
+    fi
+    vars='prmsl tmp dpt ugrd vgrd rh'
   fi
-
 
   for lead in $leads ; do
     
@@ -233,58 +245,24 @@ for stats in acc bias_mae crpss rmse_spread ; do
 
    for domain in g003 nhem shem tropics conus ; do
 
-    for var in hgt tmp ugrd vgrd prmsl ; do
-      if [ $var = hgt ] ; then
-	 levels='500 700 1000'
-	 unit='mb'
-      elif [ $var = tmp ] ; then
-	 levels='500 850'
-	 unit='mb'
+    for var in $vars ; do
+      if [ $var = tmp ] || [ $var = dpt ] || [ $var = rh ]; then
+	 levels='2m'
       elif [ $var = ugrd ] || [ $var = vgrd ] ; then
-	 levels='850 250 10'
-	 unit='mb'
-      elif [ $var = prmsl ] ; then
+	 levels='10m'
+      elif [ $var = prmsl ] || [ $var = cape ] ; then
 	 levels='l0'
-	 unit=''
       fi
 
       for level in $levels ; do
 
-	 if [ $level = 10 ] ; then
-	   unit='m'
-	 fi
+         if [ $var = prmsl ] || [ $var = cape ] ; then
 
-         if [ $level = 1000 ] || [ $level = 850 ] ||  [ $level = 500  ] ||  [ $level = 250  ] ||  [ $level = 700 ] ; then
-           plevel=p${level}
-         elif [ $level = 10 ] ; then
-           plevel=${level}m
-         else
-           plevel=$level
-         fi
-
-         if [ $var = prmsl ] ; then
-
-             if [ $stats = acc ] ; then 
-                  mv ${score_type}_regional_${domain}_valid_00z_12z_l0_${var}_${stats}${lead}  evs.global_ens.${stats}.${var}.${plevel}.last${past_days}days.${scoretype}_${valid_time}.${init_time}${lead_time}.${domain}_glb.png
-             else  
-		  mv ${score_type}_regional_${domain}_valid_00z_12z_${var}_${stats}${lead}  evs.global_ens.${stats}.${var}.${plevel}.last${past_days}days.${scoretype}_${valid_time}.${init_time}${lead_time}.${domain}_glb.png
-             fi
+             mv ${score_type}_regional_${domain}_valid_00z_12z_${var}_${stats}${lead}  evs.global_ens.${stats}.${var}.${level}.last${past_days}days.${scoretype}_${valid_time}.${init_time}${lead_time}.${domain}_glb.png
 
          else
+             mv ${score_type}_regional_${domain}_valid_00z_12z_${level}_${var}_${stats}${lead}  evs.global_ens.${stats}.${var}.${level}.last${past_days}days.${scoretype}_${valid_time}.${init_time}${lead_time}.${domain}_glb.png
 
-             if [ $var = ugrd ] || [ $var = vgrd ] ; then
-
-	       if [ $level = 10 ] && [ $stats = acc ] ; then
-	           mv ${score_type}_regional_${domain}_valid_00z_12z_z10_${var}_${stats}${lead}  evs.global_ens.${stats}.${var}.${plevel}.last${past_days}days.${scoretype}_${valid_time}.${init_time}${lead_time}.${domain}_glb.png
-               else
-	           mv ${score_type}_regional_${domain}_valid_00z_12z_${level}${unit}_${var}_${stats}${lead}  evs.global_ens.${stats}.${var}.${plevel}.last${past_days}days.${scoretype}_${valid_time}.${init_time}${lead_time}.${domain}_glb.png	     
-               fi
-
-            else
-
-               mv ${score_type}_regional_${domain}_valid_00z_12z_${level}${unit}_${var}_${stats}${lead}  evs.global_ens.${stats}.${var}.${plevel}.last${past_days}days.${scoretype}_${valid_time}.${init_time}${lead_time}.${domain}_glb.png
-
-            fi
         fi
                
       done #level
@@ -295,11 +273,10 @@ for stats in acc bias_mae crpss rmse_spread ; do
  done    #score_type
 done     #stats
 
-scp *.png wd20bz@emcrzdm:/home/people/emc/www/htdocs/bzhou/evs_plots/gens/grid2grid
 
-tar -cvf plots_gefs_grid2grid_v${VDATE}_$past_days.tar *.png
+tar -cvf plots_gefs_grid2obs_v${VDATE}_${past_days}_both_valid_times.tar *.png
 
-cp plots_gefs_grid2grid_v${VDATE}_$past_days.tar  $COMOUT/.  
+cp plots_gefs_grid2grid_v${VDATE}_${past_days}_both_valid_time.tar  $COMOUT/.  
 
 
 
