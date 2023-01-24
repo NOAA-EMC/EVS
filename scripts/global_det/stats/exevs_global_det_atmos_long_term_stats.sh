65f430e46980efb648a3f7489a7417719a7804df
Mallory Row <mallory.row@noaa.gov>
2023-01-24 18:38:18 +0000

    Combine generation of monthly and yearly stats
    Refs: #1

--- scripts/global_det/stats/exevs_global_det_atmos_long_term_stats.sh
+++ scripts/global_det/stats/exevs_global_det_atmos_long_term_stats.sh
@@ -21,13 +21,13 @@ if [ $evs_run_mode = production ] && [ $envir = prod ]; then
     exit 0
 fi
 
-# Make monthly stats
-python $USHevs/global_det/global_det_atmos_stats_long_term_monthly.py
+# Make stats
+python $USHevs/global_det/global_det_atmos_stats_long_term.py
 status=$?
 [[ $status -ne 0 ]] && exit $status
-[[ $status -eq 0 ]] && echo "Succesfully ran global_det_atmos_stats_long_term_monthly.py"
+[[ $status -eq 0 ]] && echo "Succesfully ran global_det_atmos_stats_long_term.py"
 echo
-
+exit
 # Copy files to desired location
 if [ $SENDCOM = YES ]; then
     for MODEL_PATH in $DATA/monthly/grid2grid/monthly_means/*; do
@@ -37,4 +37,13 @@ if [ $SENDCOM = YES ]; then
             cp -v $FILE $COMOUTmonthlystats/$MODEL_SUBDIR/.
         done
     done
+    if [ $VDATEmm = 12 ]; then
+        for MODEL_PATH in $DATA/yearly/grid2grid/yearly_means/*; do
+            MODEL_SUBDIR=$(echo ${MODEL_PATH##*/})
+            mkdir -p $COMOUTyearlystats/$MODEL_SUBDIR
+            for FILE in $DATA/yearly/grid2grid/yearly_means/$MODEL_SUBDIR/*; do
+                cp -v $FILE $COMOUTyearlystats/$MODEL_SUBDIR/.
+            done
+        done
+    fi
 fi
