266dddd2710e410479daf1c738701d3f487998f1
Mallory Row <mallory.row@noaa.gov>
2023-02-16 17:39:01 +0000

    Add monthly ACC plot for archive
    Refs: #1

--- ush/global_det/global_det_atmos_plots_long_term.py
+++ ush/global_det/global_det_atmos_plots_long_term.py
@@ -6,9 +6,11 @@ Abstract: This script generates monthly and yearly long term stats plots.
 
 import sys
 import os
+import shutil
 import logging
 import datetime
 import dateutil
+import calendar
 import pandas as pd
 pd.plotting.deregister_matplotlib_converters()
 #pd.plotting.register_matplotlib_converters()
@@ -25,7 +27,10 @@ RUN = os.environ['RUN']
 STEP = os.environ['STEP']
 COMPONENT = os.environ['COMPONENT']
 FIXevs = os.environ['FIXevs']
+MET_ROOT = os.environ['MET_ROOT']
+met_ver = os.environ['met_ver']
 evs_run_mode = os.environ['evs_run_mode']
+COMINdailystats = os.environ['COMINdailystats']
 COMINmonthlystats = os.environ['COMINmonthlystats']
 COMINyearlystats = os.environ['COMINyearlystats']
 VDATEYYYY = os.environ['VDATEYYYY']
@@ -189,5 +194,179 @@ for avg_time_range in avg_time_range_list:
                         run_length_list
                     )
                     plot_ltam.make_long_term_annual_mean()
+
+# Make monthly ACC archive plots
+print(f"Making {VDATEYYYY} {VDATEmm} "
+      +"Grid-to-Grid - Geopotential Height 500-hPa ACC NH plots")
+yyyymm_acc_dir = os.path.join(DATA, VDATEYYYY+'_'+VDATEmm+'_ACC')
+yyyymm_acc_logs_dir = os.path.join(yyyymm_acc_dir, 'logs')
+yyyymm_acc_data_dir = os.path.join(yyyymm_acc_dir, 'data')
+yyyymm_acc_images_dir = os.path.join(yyyymm_acc_dir, 'images')
+if not os.path.exists(yyyymm_acc_dir):
+    os.makedirs(yyyymm_acc_dir)
+    os.makedirs(yyyymm_acc_logs_dir)
+    os.makedirs(yyyymm_acc_data_dir)
+    os.makedirs(yyyymm_acc_images_dir)
+print(f"Working in {yyyymm_acc_dir}")
+yyyymm_acc_forecast_hour_list = list(range(0, 240+24, 24))
+yyyymm_acc_valid_hour_list = [str(x).zfill(2) for x in np.arange(0,13,12)]
+yyyymm_acc_met_info_dict = {
+    'root': MET_ROOT,
+    'version': met_ver
+}
+yyyymm_acc_model_info_dict = {
+    'model1': {'name': 'gfs',
+               'plot_name': 'gfs',
+               'obs_name': 'gfs_anl'},
+    'model2': {'name': 'ecmwf',
+               'plot_name': 'ecmwf',
+               'obs_name': 'ecmwf_anl'},
+    'model3': {'name': 'cmc',
+               'plot_name': 'cmc',
+               'obs_name': 'cmc_anl'},
+    'model4': {'name': 'ukmet',
+               'plot_name': 'ukmet',
+               'obs_name': 'ukmet_anl'},
+    'model5': {'name': 'jma',
+               'plot_name': 'jma',
+               'obs_name': 'jma_anl'},
+    'model6': {'name': 'imd',
+               'plot_name': 'imd',
+               'obs_name': 'imd_anl'},
+    'model7': {'name': 'fnmoc',
+               'plot_name': 'fnmoc',
+               'obs_name': 'fnmoc_anl'},
+    'model8': {'name': 'cfs',
+               'plot_name': 'cfs',
+               'obs_name': 'gfs_anl'},
+}
+yyyymm_acc_plot_info_dict = {
+    'line_type': 'SAL1L2',
+    'grid': 'G004',
+    'stat': 'ACC',
+    'vx_mask': 'NHEM',
+    'event_equalization': 'NO',
+    'interp_method': 'NEAREST',
+    'interp_points': '1',
+    'fcst_var_name': 'HGT',
+    'fcst_var_level': 'P500',
+    'fcst_var_thresh': 'NA',
+    'obs_var_name': 'HGT',
+    'obs_var_level': 'P500',
+    'obs_var_thresh': 'NA',
+}
+yyyymm_acc_date_info_dict = {
+    'date_type': 'VALID',
+    'start_date': VDATEYYYY+VDATEmm+'01',
+    'end_date': (VDATEYYYY+VDATEmm
+                 +str(calendar.monthrange(int(VDATEYYYY), int(VDATEmm))[1])),
+    'valid_hr_inc': '24',
+    'init_hr_inc': '24',
+}
+acc_job_name = ('GeoHeight/SA1L2/ACC/'
+                +f"{yyyymm_acc_plot_info_dict['line_type']}/"
+                +f"{yyyymm_acc_plot_info_dict['vx_mask']}")
+acc_output_dir = os.path.join(
+    yyyymm_acc_dir, acc_job_name.replace('/','_')
+)
+if not os.path.exists(acc_output_dir):
+    os.makedirs(acc_output_dir)
+now = datetime.datetime.now()
+acc_logging_file = os.path.join(
+    yyyymm_acc_logs_dir,
+    'evs_'+COMPONENT+'_atmos_'+RUN+'_'+STEP+'_'
+    +acc_job_name.replace('/','_')+'_runon'
+    +now.strftime('%Y%m%d%H%M%S')+'.log'
+)
+acc_logger = get_logger(acc_logging_file)
+# Get model daily stat files and condense
+for model_num in list(yyyymm_acc_model_info_dict.keys()):
+    model = yyyymm_acc_model_info_dict[model_num]['name']
+    obs_name = yyyymm_acc_model_info_dict[model_num]['obs_name']
+    stat_model_dir = os.path.join(yyyymm_acc_data_dir, model)
+    if not os.path.exists(stat_model_dir):
+        os.makedirs(stat_model_dir)
+    date_dt = datetime.datetime.strptime(
+        yyyymm_acc_date_info_dict['start_date'], '%Y%m%d'
+    )
+    while date_dt <= datetime.datetime.strptime(
+            yyyymm_acc_date_info_dict['end_date'],'%Y%m%d'
+    ):
+        source_model_date_stat_file = os.path.join(
+            COMINdailystats, model+'.'+date_dt.strftime('%Y%m%d'),
+            'evs.stats.'+model+'.atmos.grid2grid.'
+            +'v'+date_dt.strftime('%Y%m%d')+'.stat'
+        )
+        dest_model_date_stat_file = os.path.join(
+            stat_model_dir,  model+'_grid2grid_v'+date_dt.strftime('%Y%m%d')
+            +'.stat'
+        )
+        if not os.path.exists(dest_model_date_stat_file):
+            if os.path.exists(source_model_date_stat_file):
+                acc_logger.debug("Linking "+source_model_date_stat_file+" to "
+                                 +dest_model_date_stat_file)
+                os.symlink(source_model_date_stat_file,
+                           dest_model_date_stat_file)
+            else:
+                acc_logger.warning(source_model_date_stat_file+" "
+                                    +"DOES NOT EXIST")
+        date_dt = date_dt + datetime.timedelta(days=1)
+    acc_logger.info("Condensing model .stat files for job")
+    condensed_model_stat_file = os.path.join(acc_output_dir,
+                                             model_num+'_'+model
+                                             +'.stat')
+    gda_util.condense_model_stat_files(
+        acc_logger, yyyymm_acc_data_dir, condensed_model_stat_file,
+        model, obs_name, yyyymm_acc_plot_info_dict['grid'],
+        yyyymm_acc_plot_info_dict['vx_mask'],
+        yyyymm_acc_plot_info_dict['fcst_var_name'],
+        yyyymm_acc_plot_info_dict['obs_var_name'],
+        yyyymm_acc_plot_info_dict['line_type']
+    )
+# Make time series and forecast mean plots
+import global_det_atmos_plots_time_series as gdap_ts
+import global_det_atmos_plots_lead_average as gdap_la
+for valid_hour in yyyymm_acc_valid_hour_list:
+    yyyymm_acc_date_info_dict['valid_hr_start'] = valid_hour
+    yyyymm_acc_date_info_dict['valid_hr_end'] = valid_hour
+    yyyymm_acc_date_info_dict['init_hr_start'] = valid_hour
+    yyyymm_acc_date_info_dict['init_hr_end'] = valid_hour
+    for fhr in yyyymm_acc_forecast_hour_list:
+        yyyymm_acc_date_info_dict['forecast_hour'] = str(fhr)
+        plot_ts = gdap_ts.TimeSeries(acc_logger, acc_output_dir,
+                                     acc_output_dir,
+                                     yyyymm_acc_model_info_dict,
+                                     yyyymm_acc_date_info_dict,
+                                     yyyymm_acc_plot_info_dict,
+                                     yyyymm_acc_met_info_dict,
+                                     os.path.join(FIXevs, 'logos'))
+        plot_ts.make_time_series()
+    del yyyymm_acc_date_info_dict['forecast_hour']
+    yyyymm_acc_date_info_dict['forecast_hours'] = yyyymm_acc_forecast_hour_list
+    plot_la = gdap_la.LeadAverage(acc_logger, acc_output_dir,
+                                  acc_output_dir,
+                                  yyyymm_acc_model_info_dict,
+                                  yyyymm_acc_date_info_dict,
+                                  yyyymm_acc_plot_info_dict,
+                                  yyyymm_acc_met_info_dict,
+                                  os.path.join(FIXevs, 'logos'))
+    plot_la.make_lead_average()
+# Rename images to have year month
+VDATEYYYY_VDATEmm_ndays = int(
+    (datetime.datetime.strptime(yyyymm_acc_date_info_dict['end_date'],
+                                '%Y%m%d')
+     - datetime.datetime.strptime(yyyymm_acc_date_info_dict['start_date'],
+                                  '%Y%m%d')).total_seconds()/86400
+
+) + 1
+for old_image_name in os.listdir(os.path.join(acc_output_dir, 'images')):
+    old_image = os.path.join(acc_output_dir, 'images', old_image_name)
+    new_image = os.path.join(
+        yyyymm_acc_images_dir,
+        old_image_name.replace('last'+str(VDATEYYYY_VDATEmm_ndays)+'days',
+                               VDATEYYYY+VDATEmm)
+    )
+    print(f"Copying {old_image} to {new_image}")
+    shutil.copy2(old_image, new_image)
 
 print("END: "+os.path.basename(__file__))
