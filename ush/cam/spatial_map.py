516653eb15d097a87721534f6aed6450d56e62e2
marcel caron <marcel.caron@dlogin02.dogwood.wcoss2.ncep.noaa.gov>
2023-02-16 17:23:02 +0000

    Add top-level cartopy shapefiles directory definition, referencing that definition in child scripts

--- ush/cam/spatial_map.py
+++ ush/cam/spatial_map.py
@@ -42,6 +42,7 @@ VX_MASK_LIST = check_VX_MASK_LIST(os.environ['VX_MASK_LIST']).replace(' ','').sp
 SAVE_DIR = check_SAVE_DIR(os.environ['SAVE_DIR'])
 STAT_OUTPUT_BASE_DIR = check_STAT_OUTPUT_BASE_DIR(os.environ['STAT_OUTPUT_BASE_DIR'])
 FIXevs = os.environ['FIXevs']
+cartopyDataDir = os.environ['cartopyDataDir']
 VERIF_TYPE = os.environ['VERIF_TYPE']
 
 # Define Settings
@@ -101,6 +102,6 @@ for VX_MASK in VX_MASK_LIST:
     p = cam_plots_precip_spatial_map.PrecipSpatialMap(
         logger, INPUT_DIR, OUTPUT_DIR, MODEL_INFO_DICT, 
         DATE_INFO_DICT, PLOT_INFO_DICT, MET_INFO_DICT, 
-        LOGO_DIR
+        LOGO_DIR, cartopyDataDir
     )
     p.make_precip_spatial_map()
