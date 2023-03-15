a8271cc02348713c1a67fdb8035e645fac29e5ce
Mallory Row <mallory.row@noaa.gov>
2023-03-15 14:21:44 +0000

    Update plot tar file name
    Refs: #1

--- scripts/global_det/plots/exevs_global_det_atmos_grid2obs_plots.sh
+++ scripts/global_det/plots/exevs_global_det_atmos_grid2obs_plots.sh
@@ -97,7 +97,7 @@ if [ $SENDCOM = YES ]; then
     for VERIF_TYPE_SUBDIR_PATH in $DATA/${VERIF_CASE}_${STEP}/plot_output/$RUN.${end_date}/images/*; do
         VERIF_TYPE_SUBDIR=$(echo ${VERIF_TYPE_SUBDIR_PATH##*/})
         cd $VERIF_TYPE_SUBDIR
-        large_tar_file=${DATA}/${VERIF_CASE}_${STEP}/plot_output/${RUN}.${end_date}/images/evs.plots.${COMPONENT}.${RUN}.${VERIF_CASE}_${VERIF_TYPE_SUBDIR}.last${NDAYS}days.v${PDYm1}.tar
+        large_tar_file=${DATA}/${VERIF_CASE}_${STEP}/plot_output/${RUN}.${end_date}/images/evs.plots.${COMPONENT}.${RUN}.${VERIF_CASE}_${VERIF_TYPE_SUBDIR}.last${NDAYS}days.v${end_date}.tar
         tar -cvf $large_tar_file *.tar
         cp -v $large_tar_file $COMOUT/.
     done
