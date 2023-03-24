ceee32a687e3487f023f45807ca05f9330d7b0d8
perry shafran <perry.shafran@clogin07.cactus.wcoss2.ncep.noaa.gov>
2023-03-24 20:21:29 +0000

    Adding correct plots scripts file

--- scripts/cam/plots/exevs_nam_firewxnest_grid2obs_plots.sh
+++ scripts/cam/plots/exevs_nam_firewxnest_grid2obs_plots.sh
@@ -25,9 +25,9 @@ while [ $DATE -ge $ENDDATE ]; do
 	MONTH=`cut -c 1-6 curdate`
 	HOUR=`cut -c 9-10 curdate`
 
-	if [ -e ${COMINnam}.$DAY/evs.stats.${MODELNAME}.${RUN}.${VERIF_CASE}.v${DAY}.stat ]
+	if [ -e ${COMINcam}.$DAY/evs.stats.${MODELNAME}.${RUN}.${VERIF_CASE}.v${DAY}.stat ]
 	then
-	cp ${COMINnam}.$DAY/evs.stats.${MODELNAME}.${RUN}.${VERIF_CASE}.v${DAY}.stat $STATDIR
+	cp ${COMINcam}.$DAY/evs.stats.${MODELNAME}.${RUN}.${VERIF_CASE}.v${DAY}.stat $STATDIR
         fi
 
 	sed "s/$model1/$MODELNAME/g" $STATDIR/evs.stats.${MODELNAME}.${RUN}.${VERIF_CASE}.v${DAY}.stat > $STATDIR/temp.stat
@@ -147,6 +147,11 @@ do
 
 done
 
+cd ${PLOTDIR}
+tar -cvf evs.plots.${COMPONENT}.${RUN}.${VERIF_CASE}.last31days.v${VDATE}.tar *png
+        
+mkdir -m 775 -p $COMOUTplots
+cp evs.plots.${COMPONENT}.${RUN}.${VERIF_CASE}.last31days.v${VDATE}.tar $COMOUTplots
 
 exit
 
