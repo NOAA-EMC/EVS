f2818ea9f40df98732bf8df4aef53918fbc3fd81
shannon shields <shannon.shields@clogin05.cactus.wcoss2.ncep.noaa.gov>
2023-05-04 00:34:00 +0000

    Remove Week 4 to Reduce Repeated Looping

--- scripts/subseasonal/stats/exevs_subseasonal_grid2obs_stats.sh
+++ scripts/subseasonal/stats/exevs_subseasonal_grid2obs_stats.sh
@@ -271,7 +271,7 @@ for group in reformat_data assemble_data generate_stats gather_stats; do
     elif [ "${JOB_GROUP}" = "assemble_data" ]; then
 	echo "Creating and running jobs for grid-to-obs stats: ${JOB_GROUP}"
 	export njobs=0
-	WEEKLY_LIST="Week1 Week2 Week3 Week4 Week5"
+	WEEKLY_LIST="Week1 Week2 Week3 Week5"
 	for WEEK in $WEEKLY_LIST; do
 	    export WEEK=$WEEK
 	    if [ "${WEEK}" = "Week1" ]; then
@@ -283,9 +283,6 @@ for group in reformat_data assemble_data generate_stats gather_stats; do
 	    elif [ "${WEEK}" = "Week3" ]; then
 		export CORRECT_INIT_DATE=$PDYm23
 		export CORRECT_LEAD_SEQ=336,348,360,372,384,396,408,420,432,444,456,468,480,492,504
-	    elif [ "${WEEK}" = "Week4" ]; then
-		export CORRECT_INIT_DATE=$PDYm30
-		export CORRECT_LEAD_SEQ=504,516,528,540,552,564,576,588,600,612,624,636,648,660,672
 	    elif [ "${WEEK}" = "Week5" ]; then
 		export CORRECT_INIT_DATE=$PDYm37
 		export CORRECT_LEAD_SEQ=672,684,696,708,720,732,744,756,768,780,792,804,816,828,840
@@ -298,7 +295,7 @@ for group in reformat_data assemble_data generate_stats gather_stats; do
 	done
 	DAYS6_10_LIST="Days6_10"
 	for DAYS in $DAYS6_10_LIST; do
-	    export njobs=5
+	    export njobs=4
 	    export DAYS=$DAYS
 	    if [ "${DAYS}" = "Days6_10" ]; then
 		export CORRECT_INIT_DATE=$PDYm12
@@ -311,7 +308,7 @@ for group in reformat_data assemble_data generate_stats gather_stats; do
 	done
 	WEEKS3_4_LIST="Weeks3_4"
 	for WEEKS in $WEEKS3_4_LIST; do
-	    export njobs=6
+	    export njobs=5
 	    export WEEKS=$WEEKS
 	    if [ "${WEEKS}" = "Weeks3_4" ]; then
 		export CORRECT_INIT_DATE=$PDYm30
