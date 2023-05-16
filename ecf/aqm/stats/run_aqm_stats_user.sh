3fd3394e644fe8b0e311414f9c245464bb150fd3
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2023-05-16 12:35:20 +0000

    for test run

--- ecf/aqm/stats/run_aqm_stats_user.sh
+++ ecf/aqm/stats/run_aqm_stats_user.sh
@@ -11,7 +11,8 @@ set -x
 
 BASE=`pwd`
 
-for fhr in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+## for fhr in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+for fhr in 15 
 do
    export fhr
    qsub -v cyc=$fhr ${BASE}/jevs_aqm_stats.ecf
