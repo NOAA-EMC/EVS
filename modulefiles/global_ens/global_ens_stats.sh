779a13b1dd042e9eb9f2dcc52fe60f67421377be
Steven Simon <Steven.Simon@noaa.gov>
2023-07-05 16:11:38 +0000

    Update global_ens module files with additional modules.

--- modulefiles/global_ens/global_ens_stats.sh
+++ modulefiles/global_ens/global_ens_stats.sh
@@ -7,6 +7,10 @@ module load gsl/${gsl_ver}
 module load prod_envir/${prod_envir_ver}
 module load prod_util/${prod_util_ver}
 module load libjpeg/${libjpeg_ver}
+module load libpng/${libpng_ver}
+module load zlib/${zlib_ver}
+module load jasper/${jasper_ver}
+module load udunits/${udunits_ver}
 module load grib_util/${grib_util_ver}
 module load wgrib2/${wgrib2_ver}
 module load met/${met_ver}
