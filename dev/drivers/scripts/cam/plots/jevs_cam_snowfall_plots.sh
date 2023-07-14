b4881c0ac1bf83a874d38d49c201432c5b88bc1c
marcel caron <marcel.caron@dlogin05.dogwood.wcoss2.ncep.noaa.gov>
2023-07-14 01:39:54 +0000

    export cyc in plotting driver scripts

--- dev/drivers/scripts/cam/plots/jevs_cam_snowfall_plots.sh
+++ dev/drivers/scripts/cam/plots/jevs_cam_snowfall_plots.sh
@@ -57,6 +57,7 @@ export DATA=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
 export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/stats/$COMPONENT
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/$STEP/$COMPONENT/$RUN
 export FIXevs="/lfs/h2/emc/vpppg/noscrub/emc.vpppg/verification/EVS_fix"
+export cyc=$(date -d "today" +"%H")
 
 # Job Settings and Run
 . ${HOMEevs}/jobs/cam/plots/JEVS_CAM_PLOTS
