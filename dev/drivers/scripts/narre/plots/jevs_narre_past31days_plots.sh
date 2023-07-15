035e65663f92eb748a25e2c324a574cdf8756337
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2023-07-15 22:17:22 +0000

    update plot drivers

--- dev/drivers/scripts/narre/plots/jevs_narre_past31days_plots.sh
+++ dev/drivers/scripts/narre/plots/jevs_narre_past31days_plots.sh
@@ -6,7 +6,7 @@
 #PBS -q dev
 #PBS -A VERF-DEV
 #PBS -l walltime=00:30:00
-#PBS -l place=vscatter:exclhost,select=1:ncpus=8:mem=300GB
+#PBS -l place=vscatter:exclhost,select=1:ncpus=8:mem=400GB
 #PBS -l debug=true
 
 
