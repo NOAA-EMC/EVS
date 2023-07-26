712f3538670333588e5df505a8327750b7c1b94a
marcel caron <marcel.caron@clogin01.cactus.wcoss2.ncep.noaa.gov>
2023-07-26 19:29:14 +0000

    Update driver script for mesoscale

--- dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_headline_plots.sh
+++ dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_headline_plots.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#PBS -S /bin/bash
 #PBS -N jevs_mesoscale_headline_plots
 #PBS -j oe
 #PBS -S /bin/bash
@@ -36,7 +36,7 @@ export VERIF_CASE="headline"
 export MODELNAME=${COMPONENT}
 
 # EVS Settings
-export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/temp/EVS"
+export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/EVS"
 export HOMEevs=${HOMEevs:-${PACKAGEROOT}/evs.${evs_ver}}
 export config=$HOMEevs/parm/evs_config/mesoscale/config.evs.prod.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}
 
@@ -45,52 +45,22 @@ source $HOMEevs/versions/run.ver
 
 source /usr/share/lmod/lmod/init/sh
 module reset
+source $HOMEevs/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
 export HPC_OPT=/apps/ops/prod/libs
-export MET_bin_exec="bin"
-module load cray-pals/${craypals_ver}
-module load PrgEnv-intel/${PrgEnv_intel_ver}
-module load craype/${craype_ver}
-module load intel/${intel_ver}
-module load cray-mpich/${craympich_ver}
-module load cray-pals/${craypals_ver}
-module load cfp/${cfp_ver}
-module load libjpeg/${libjpeg_ver}
-module load libpng/${libpng_ver}
-module load zlib/${zlib_ver}
-module load jasper/${jasper_ver}
-module load udunits/${udunits_ver}
-module load gsl/${gsl_ver}
-module load hdf5/${hdf5_ver}
-module load python/${python_ver}
-module load netcdf/${netcdf_ver}
-module load nco/${nco_ver}
-module load prod_util/${prod_util_ver}
-module load prod_envir/${prod_envir_ver}
-module load cdo/${cdo_ver}
-module use /apps/ops/prod/libs/modulefiles/compiler/intel/${intel_ver}
-module load grib_util/${grib_util_ver}
-module load wgrib2/${wgrib2_ver}
-module load proj/${proj_ver}
-module load geos/${geos_ver}
-module load met/${met_ver}
-module load metplus/${metplus_ver}
-export MET_PLUS_PATH="/apps/ops/prod/libs/intel/${intel_ver}/metplus/${metplus_ver}"
-export MET_PATH="/apps/ops/prod/libs/intel/${intel_ver}/met/${met_ver}"
+export MET_PLUS_PATH="/apps/ops/para/libs/intel/${intel_ver}/metplus/${metplus_ver}"
+export MET_PATH="/apps/ops/para/libs/intel/${intel_ver}/met/${met_ver}"
 export MET_CONFIG="${MET_PLUS_PATH}/parm/met_config"
 export PYTHONPATH=$HOMEevs/ush/$COMPONENT:$PYTHONPATH
 
 # Developer Settings
-export DATA=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
+export DATA=/lfs/h2/emc/ptmp/$USER/evs_test/$envir/tmp
 export COMINccpa=/lfs/h2/emc/ptmp/${USER}/EVS_out/com/$NET/$evs_ver/prep/$COMPONENT/$RUN
 export COMINmrms=/lfs/h2/emc/ptmp/${USER}/EVS_out/com/$NET/$evs_ver/prep/$COMPONENT/$RUN
 export COMINspcotlk=/lfs/h2/emc/ptmp/${USER}/EVS_out/com/$NET/$evs_ver/prep/$COMPONENT/$RUN
-#export COMIN=/lfs/h2/emc/ptmp/${USER}/EVS_out/com/$NET/$evs_ver/stats/$COMPONENT/$RUN
-export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/$evs_ver/stats
-export COMOUT=/lfs/h2/emc/ptmp/${USER}/EVS_out/com/$NET/$evs_ver/$STEP/$COMPONENT/$RUN
+export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/$evs_ver/stats/
+export COMOUT=/lfs/h2/emc/ptmp/${USER}/EVS_out/com/$NET/$evs_ver/$STEP/$COMPONENT
 export FIXevs="/lfs/h2/emc/vpppg/noscrub/emc.vpppg/verification/EVS_fix"
-#export cyc=%CYC%
 export cyc=$(date -d "today" +"%H")
 
 # Job Settings and Run
-# CHANGE TO . ${HOMEevs}/jobs/${COMPONENT}/plots/JEVS_MESOSCALE_PLOTS later
-. ${HOMEevs}/jobs/cam/plots/JEVS_CAM_PLOTS
+. ${HOMEevs}/jobs/mesoscale/plots/JEVS_MESOSCALE_PLOTS
