9eea069e7adb4ee53df05317164b383bbae1004e
Lin.Gan <lin.gan@noaa.gov>
2023-07-28 13:10:57 +0000

    Update narre j-job, module files, ex-script modified to work with ecflow workflow
    GitHub #180

--- modulefiles/narre/stats.sh
+++ modulefiles/narre/stats.sh
@@ -10,7 +10,6 @@ module load gsl/${gsl_ver}
 module load met/${met_ver}
 module load metplus/${metplus_ver}
 module load cray-pals/${craypals_ver}
-module load prod_envir/${prod_envir_ver}
 module load prod_util/${prod_util_ver}
 module load cfp/${cfp_ver}
 module load libjpeg/${libjpeg_ver}
