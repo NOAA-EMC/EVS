b748e16437f3e1117e2b523eb72bebd0a163f5f5
Shelley Melchior <53050457+ShelleyMelchior-NOAA@users.noreply.github.com>
2023-08-09 14:55:04 -0400

    feature/ve-evs-prod (#192)
    * Updating modulefiles for analyses, aqm, and cam for prod installation of
    ve/evs/1.0.
    
    * Updating modulefiles for analyses, aqm, and cam for prod installation of
    ve/evs/1.0.
    
    * Updating modulefiles for analyses, aqm, and cam for prod installation of
    ve/evs/1.0.
    
    * Updating modulefiles for analyses, aqm, and cam for prod installation of
    ve/evs/1.0.
    
    * Updating modulefiles for analyses, aqm, and cam for prod installation of
    ve/evs/1.0.

--- modulefiles/aqm/aqm_prep.sh
+++ modulefiles/aqm/aqm_prep.sh
@@ -3,9 +3,7 @@
 #
 set +x
 
-export HPC_OPT=/apps/ops/para/libs
-module use /apps/ops/para/libs/modulefiles/compiler/intel/${intel_ver}/
-module use /apps/dev/modulefiles/
+module use /apps/prod/lmodules/intel/${intel_ver}
 module load ve/evs/${ve_evs_ver}
 module load cray-mpich/${craympich_ver}
 module load cray-pals/${craypals_ver}
@@ -13,10 +11,12 @@ module load libjpeg/${libjpeg_ver}
 module load grib_util/${grib_util_ver}
 module load wgrib2/${wgrib2_ver}
 module load gsl/${gsl_ver}
-module load met/${met_ver}
-module load metplus/${metplus_ver}
 module load prod_util/${produtil_ver}
 module load prod_envir/${prodenvir_ver}
+export HPC_OPT=/apps/ops/para/libs
+module use /apps/ops/para/libs/modulefiles/compiler/intel/${intel_ver}/
+module load met/${met_ver}
+module load metplus/${metplus_ver}
 
 module list
 
