97d320a3f06747bb8bcd091f3a38f025abf61464
LoganDawson-NOAA <Logan.Dawson@noaa.gov>
2023-08-14 16:48:16 +0000

    update MRMS product for Alaska REFC vx

--- scripts/cam/prep/exevs_cam_radar_prep.sh
+++ scripts/cam/prep/exevs_cam_radar_prep.sh
@@ -67,13 +67,13 @@ for DOMAIN in ${DOMAINS}; do
    for RADAR_FIELD in ${RADAR_FIELDS}; do
 
       if [ $RADAR_FIELD = REFC ]; then
-         if [ $DOMAIN = conus ]; then
-            MRMS_PRODUCT=MergedReflectivityQCComposite_00.50
-	    export OBS_VAR=MergedReflectivityQCComposite
-         elif [ $DOMAIN = alaska ]; then
-            MRMS_PRODUCT=MergedReflectivityQComposite_00.50
-	    export OBS_VAR=MergedReflectivityQComposite
-         fi
+         MRMS_PRODUCT=MergedReflectivityQCComposite_00.50
+	 export OBS_VAR=MergedReflectivityQCComposite
+      #  if [ $DOMAIN = conus ]; then
+      #  elif [ $DOMAIN = alaska ]; then
+      #     MRMS_PRODUCT=MergedReflectivityQComposite_00.50
+      #     export OBS_VAR=MergedReflectivityQComposite
+      #  fi
       elif [ $RADAR_FIELD = REFD1 ]; then
          MRMS_PRODUCT=SeamlessHSR_00.00
       elif [ $RADAR_FIELD = RETOP ]; then
