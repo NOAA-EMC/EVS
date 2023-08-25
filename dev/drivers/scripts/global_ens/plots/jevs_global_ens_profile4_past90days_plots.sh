6f7bfadf72b14319723ffc5eac66090b33675caf
Steven Simon <Steven.Simon@noaa.gov>
2023-08-25 20:55:42 +0000

    Restore memory settings to exclhost 128 nodes for global_ens profile4 plots drivers.

--- dev/drivers/scripts/global_ens/plots/jevs_global_ens_profile4_past90days_plots.sh
+++ dev/drivers/scripts/global_ens/plots/jevs_global_ens_profile4_past90days_plots.sh
@@ -4,7 +4,7 @@
 #PBS -q dev
 #PBS -A VERF-DEV
 #PBS -l walltime=01:30:00
-#PBS -l place=vscatter,select=5:ncpus=64:mpiprocs=64:mem=500GB
+#PBS -l place=vscatter:exclhost,select=5:ncpus=128:mem=500GB
 #PBS -l debug=true
 
 
