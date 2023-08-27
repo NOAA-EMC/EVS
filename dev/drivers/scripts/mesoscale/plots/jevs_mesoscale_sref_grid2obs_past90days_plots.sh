6518de78b914956e84e64042f0339d8c80b2f4ff
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2023-08-27 18:53:59 +0000

    export FIXevs in the plot drivers

--- dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_sref_grid2obs_past90days_plots.sh
+++ dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_sref_grid2obs_past90days_plots.sh
@@ -46,5 +46,6 @@ export COMOUT=/lfs/h2/emc/ptmp/${USER}/$NET/$evs_ver
 export DATA=/lfs/h2/emc/stmp/${USER}/evs/tmpnwprd
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
+export FIXevs=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/verification/EVS_fix
 
 ${HOMEevs}/jobs/${COMPONENT}/${STEP}/JEVS_MESOSCALE_PLOTS
