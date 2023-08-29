ad368fcf2f1e093324ce6bff0a76754c360202d5
marcel caron <marcel.caron@clogin07.cactus.wcoss2.ncep.noaa.gov>
2023-08-29 14:24:21 +0000

    Correct DATA definition

--- dev/drivers/scripts/cam/plots/jevs_cam_snowfall_plots.sh
+++ dev/drivers/scripts/cam/plots/jevs_cam_snowfall_plots.sh
@@ -52,7 +52,7 @@ export MET_CONFIG="${MET_PLUS_PATH}/parm/met_config"
 export PYTHONPATH=$HOMEevs/ush/$COMPONENT:$PYTHONPATH
 
 # Developer Settings
-export DATA=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp/${jobid}
+export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
 export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/stats/$COMPONENT
 export cyc=$(date -d "today" +"%H")
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/$STEP/$COMPONENT
