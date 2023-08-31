b9190df3346c8010ffbd05369ef6b8eaf30030e5
Mallory Row <mallory.row@noaa.gov>
2023-08-31 17:36:18 +0000

    Adjust resources
    Refs: #8

--- dev/drivers/scripts/nfcens/stats/jevs_nfcens_wave_grid2obs_stats.sh
+++ dev/drivers/scripts/nfcens/stats/jevs_nfcens_wave_grid2obs_stats.sh
@@ -3,8 +3,8 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=01:00:00
-#PBS -l place=vscatter,select=1:ncpus=128:mem=100G
+#PBS -l walltime=00:10:00
+#PBS -l place=vscatter,select=1:ncpus=36:mem=10G
 #PBS -l debug=true
 #PBS -V
 
--- scripts/nfcens/stats/exevs_nfcens_wave_grid2obs_stats.sh
+++ scripts/nfcens/stats/exevs_nfcens_wave_grid2obs_stats.sh
@@ -121,7 +121,7 @@ done
 if [[ -s ${DATA}/jobs/run_all_${MODELNAME}_${RUN}_g2o_poe.sh ]]; then
     if [ ${run_mpi} = 'yes' ] ; then
         export LD_LIBRARY_PATH=/apps/dev/pmi-fix:$LD_LIBRARY_PATH
-        mpiexec -np 36 --cpu-bind verbose,core --depth 3 cfp ${DATA}/jobs/run_all_${MODELNAME}_${RUN}_g2o_poe.sh
+        mpiexec -np 36 -ppn 36 --cpu-bind verbose,core cfp ${DATA}/jobs/run_all_${MODELNAME}_${RUN}_g2o_poe.sh
     else
         echo "not running mpiexec"
         sh ${DATA}/jobs/run_all_${MODELNAME}_${RUN}_g2o_poe.sh
