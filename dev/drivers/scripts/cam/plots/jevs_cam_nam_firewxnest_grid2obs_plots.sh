759ca873e72a0e3e0dd868ed0cc053c30689b948
perry shafran <perry.shafran@dlogin03.dogwood.wcoss2.ncep.noaa.gov>
2023-09-01 18:04:30 +0000

    Making updates for firewx plots

--- dev/drivers/scripts/cam/plots/jevs_cam_nam_firewxnest_grid2obs_plots.sh
+++ dev/drivers/scripts/cam/plots/jevs_cam_nam_firewxnest_grid2obs_plots.sh
@@ -4,13 +4,13 @@
 #PBS -S /bin/bash
 #PBS -q "dev"
 #PBS -A VERF-DEV
-#PBS -l walltime=02:00:00
+#PBS -l walltime=00:10:00
 #PBS -l select=1:ncpus=1:mem=2GB
 #PBS -l debug=true
 
 export model=evs
 
-export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/EVS
+export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/EVS_firewx_restart/EVS
 
 source $HOMEevs/versions/run.ver
 
