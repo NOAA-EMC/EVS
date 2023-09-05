c37bf8c9e53400ed5f74415929dc53d18d7648ba
marcel caron <marcel.caron@dlogin08.dogwood.wcoss2.ncep.noaa.gov>
2023-09-05 13:06:26 +0000

    Use USER env var instead of emc.vpppg in COMIN defs

--- dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_snowfall_plots.sh
+++ dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_snowfall_plots.sh
@@ -52,7 +52,7 @@ export PYTHONPATH=$HOMEevs/ush/$COMPONENT:$PYTHONPATH
 
 # Developer Settings
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
-export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/$evs_ver/stats/
+export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/stats/
 export COMOUT=/lfs/h2/emc/ptmp/${USER}/$NET/$evs_ver/$STEP/$COMPONENT/
 export FIXevs="/lfs/h2/emc/vpppg/noscrub/emc.vpppg/verification/EVS_fix"
 export cyc=$(date -d "today" +"%H")
