6afa24161519faeacaa7dc5700ff1c3c8c058a03
perry shafran <perry.shafran@dlogin03.dogwood.wcoss2.ncep.noaa.gov>
2023-09-05 15:31:53 +0000

    Resolving conflicts with mesoscale PR

--- dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_grid2obs_plots.sh
+++ dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_grid2obs_plots.sh
@@ -1,5 +1,5 @@
 #PBS -S /bin/bash
-#PBS -N jevs_mesoscale_headline_plots
+#PBS -N jevs_mesoscale_grid2obs_plots
 #PBS -j oe
 #PBS -S /bin/bash
 #PBS -q dev
@@ -11,16 +11,17 @@
 
 set -x
 export model=evs
+module reset
 export machine=WCOSS2
 
 # ECF Settings
 export RUN_ENVIR=nco
 export SENDECF=YES
 export SENDCOM=YES
-export KEEPDATA=NO
+export KEEPDATA=YES
 export SENDDBN=YES
 export SENDDBN_NTC=
-export job=${PBS_JOBNAME:-jevs_mesoscale_headline_plots}
+export job=${PBS_JOBNAME:-jevs_mesoscale_grid2obs_plots}
 export jobid=$job.${PBS_JOBID:-$$}
 export SITE=$(cat /etc/cluster_name)
 export USE_CFP=YES
@@ -31,7 +32,7 @@ export NET="evs"
 export STEP="plots"
 export COMPONENT="mesoscale"
 export RUN="atmos"
-export VERIF_CASE="headline"
+export VERIF_CASE="grid2obs"
 export MODELNAME=${COMPONENT}
 
 # EVS Settings
@@ -44,9 +45,8 @@ source $HOMEevs/versions/run.ver
 
 source /usr/share/lmod/lmod/init/sh
 module reset
-module load prod_envir/${prod_envir_ver}
 source $HOMEevs/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
-export HPC_OPT=/apps/ops/prod/libs
+export HPC_OPT=/apps/ops/para/libs
 export MET_PLUS_PATH="/apps/ops/para/libs/intel/${intel_ver}/metplus/${metplus_ver}"
 export MET_PATH="/apps/ops/para/libs/intel/${intel_ver}/met/${met_ver}"
 export MET_CONFIG="${MET_PLUS_PATH}/parm/met_config"
@@ -54,7 +54,7 @@ export PYTHONPATH=$HOMEevs/ush/$COMPONENT:$PYTHONPATH
 
 # Developer Settings
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
-export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/stats
+export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/stats/
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/$STEP/$COMPONENT
 export FIXevs="/lfs/h2/emc/vpppg/noscrub/emc.vpppg/verification/EVS_fix"
 export cyc=$(date -d "today" +"%H")
