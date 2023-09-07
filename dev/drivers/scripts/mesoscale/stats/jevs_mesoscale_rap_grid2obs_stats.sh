567668239f2088529ebcdda597d32bff36d18f67
Roshan Shrestha <roshan.shrestha@noaa.gov>
2023-09-07 18:47:26 +0000

    EE2 review edits included

--- dev/drivers/scripts/mesoscale/stats/jevs_mesoscale_rap_grid2obs_stats.sh
+++ dev/drivers/scripts/mesoscale/stats/jevs_mesoscale_rap_grid2obs_stats.sh
@@ -5,7 +5,7 @@
 #PBS -q "dev"
 #PBS -A VERF-DEV
 #PBS -l walltime=4:59:00
-#PBS -l place=vscatter:exclhost,select=3:ncpus=128:ompthreads=1:mem=128GB
+#PBS -l place=vscatter:exclhost,select=3:ncpus=128:ompthreads=1
 #PBS -l debug=true
 #PBS -V
 
