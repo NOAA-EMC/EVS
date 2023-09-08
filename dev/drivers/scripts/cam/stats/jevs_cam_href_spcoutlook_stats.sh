1234f1d2118433d97081518c03e335c52977a87e
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2023-09-08 18:49:29 +0000

    update COMINspcotlk in driver and in J-job

--- dev/drivers/scripts/cam/stats/jevs_cam_href_spcoutlook_stats.sh
+++ dev/drivers/scripts/cam/stats/jevs_cam_href_spcoutlook_stats.sh
@@ -41,7 +41,7 @@ export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
 export jobid=$job.${PBS_JOBID:-$$}
 
 #Define the spc outlook reqions mask file path
-export COMINspcotlk=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/evs/v1.0/prep/cam/spc_otlk
+export COMINspcotlk=/lfs/h2/emc/vpppg/noscrub/${USER}/evs/v1.0/prep/cam
 
 
 export maillist='geoffrey.manikin@noaa.gov,binbin.zhou@noaa.gov'
