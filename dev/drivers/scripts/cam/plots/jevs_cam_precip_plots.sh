0f4a09298bb4bfdb63cbe6e614e65f432c8fd2be
marcel caron <marcel.caron@dlogin05.dogwood.wcoss2.ncep.noaa.gov>
2023-09-12 04:13:39 +0000

    Remove FIXevs from plots driver scripts

--- dev/drivers/scripts/cam/plots/jevs_cam_precip_plots.sh
+++ dev/drivers/scripts/cam/plots/jevs_cam_precip_plots.sh
@@ -58,7 +58,6 @@ export COMINspcotlk=/lfs/h2/emc/ptmp/${USER}/EVS_out/com/$NET/$evs_ver/prep/$COM
 export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/stats/$COMPONENT
 export cyc=$(date -d "today" +"%H")
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/$STEP/$COMPONENT
-export FIXevs="/lfs/h2/emc/vpppg/noscrub/emc.vpppg/verification/EVS_fix"
 
 # Job Settings and Run
 . ${HOMEevs}/jobs/cam/plots/JEVS_CAM_PLOTS
