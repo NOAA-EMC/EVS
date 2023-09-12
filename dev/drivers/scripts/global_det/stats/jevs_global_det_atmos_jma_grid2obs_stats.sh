61f26a0c74d2eafff0f7f76d9f3f53c79f4e80fd
Mallory Row <mallory.row@noaa.gov>
2023-09-12 11:43:17 +0000

    Remove FIXevs from drivers
    Refs: #1

--- dev/drivers/scripts/global_det/stats/jevs_global_det_atmos_jma_grid2obs_stats.sh
+++ dev/drivers/scripts/global_det/stats/jevs_global_det_atmos_jma_grid2obs_stats.sh
@@ -42,7 +42,6 @@ export RUN=atmos
 export VERIF_CASE=grid2obs
 export MODELNAME=jma
 
-export FIXevs=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/verification/EVS_fix
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
 export TMPDIR=$DATAROOT
 export COMROOT=/lfs/h2/emc/vpppg/noscrub/$USER
