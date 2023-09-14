cb490fb65c97cb8b350e02a45888b841b588c10e
Roshan Shrestha <roshan.shrestha@noaa.gov>
2023-09-14 20:46:52 +0000

    Keepdata reset

--- dev/drivers/scripts/mesoscale/stats/jevs_mesoscale_nam_grid2obs_stats.sh
+++ dev/drivers/scripts/mesoscale/stats/jevs_mesoscale_nam_grid2obs_stats.sh
@@ -18,7 +18,7 @@ set -x
   export RUN_ENVIR=emc
   export SENDECF=YES
   export SENDCOM=YES
-  export KEEPDATA=YES
+  export KEEPDATA=NO
   export SENDDBN=YES
   export SENDDBN_NTC=
   export job=${PBS_JOBNAME:-jevs_mesoscale_grid2obs_stats}
