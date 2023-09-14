5f314ec3ce2369a4eb5af3a7e5947c14c12da866
perry shafran <perry.shafran@clogin07.cactus.wcoss2.ncep.noaa.gov>
2023-09-14 15:37:49 +0000

    Removing commented out line

--- dev/drivers/scripts/rtofs/prep/jevs_rtofs_prep.sh
+++ dev/drivers/scripts/rtofs/prep/jevs_rtofs_prep.sh
@@ -35,7 +35,6 @@ export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/${evs_ver}
 export COMINrtofs=/lfs/h1/ops/$envir/com/$COMPONENT/${rtofs_ver}
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/${evs_ver}
 export COMOUTprep=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/${evs_ver}/$STEP/$COMPONENT
-#export DATA=/lfs/h2/emc/ptmp/$USER/$NET/${evs_ver}
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
 
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
