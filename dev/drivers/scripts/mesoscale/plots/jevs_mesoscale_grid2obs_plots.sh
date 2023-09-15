aab8e5cc28b67f57ba845f5b260c1dd97099cc78
marcel caron <marcel.caron@clogin05.cactus.wcoss2.ncep.noaa.gov>
2023-09-15 14:22:10 +0000

    Remove HPC_OPT

--- dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_grid2obs_plots.sh
+++ dev/drivers/scripts/mesoscale/plots/jevs_mesoscale_grid2obs_plots.sh
@@ -46,7 +46,6 @@ source $HOMEevs/versions/run.ver
 source /usr/share/lmod/lmod/init/sh
 module reset
 source $HOMEevs/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
-export HPC_OPT=/apps/ops/prod/libs
 export MET_PLUS_PATH="/apps/ops/prod/libs/intel/${intel_ver}/metplus/${metplus_ver}"
 export MET_PATH="/apps/ops/prod/libs/intel/${intel_ver}/met/${met_ver}"
 export MET_CONFIG="${MET_PLUS_PATH}/parm/met_config"
