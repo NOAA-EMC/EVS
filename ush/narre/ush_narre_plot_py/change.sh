d04a819695a25c86af6ddb4e28db170f649768a8
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2023-09-15 17:50:04 +0000

    delete change.sh in /ush

--- ush/narre/ush_narre_plot_py/change.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for file in lead_average.py  performance_diagram.py roc_curve.py stat_by_level.py threshold_average.py time_series.py valid_hour_average.py ; do
-  sed -e "s!b=True!visible=True!g" $file > a
-  mv a $file
-done
