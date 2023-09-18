9a83b122c8c65d32c1e42dcc469ccbf9ccaa0542
marcel caron <marcel.caron@clogin09.cactus.wcoss2.ncep.noaa.gov>
2023-09-18 21:30:41 +0000

    Add COMROOT dev variable

--- dev/drivers/scripts/cam/stats/jevs_cam_hrrr_precip_stats.sh
+++ dev/drivers/scripts/cam/stats/jevs_cam_hrrr_precip_stats.sh
@@ -52,6 +52,7 @@ export PYTHONPATH=$HOMEevs/ush/$COMPONENT:$PYTHONPATH
 
 # Developer Settings
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
+export COMROOT=/lfs/h2/emc/vpppg/noscrub/${USER}
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/$STEP/$COMPONENT
 export cyc=$(date -d "today" +"%H")
 export maillist="alicia.bentley@noaa.gov,marcel.caron@noaa.gov"
