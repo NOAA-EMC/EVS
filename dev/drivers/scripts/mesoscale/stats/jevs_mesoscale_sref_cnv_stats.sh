ab8bd3b4682b35bc2ed8023d1c41660ef2e8a2ee
Shelley Melchior <Shelley.Melchior@noaa.gov>
2023-09-21 15:58:21 +0000

    Adding COMIN to driver

--- dev/drivers/scripts/mesoscale/stats/jevs_mesoscale_sref_cnv_stats.sh
+++ dev/drivers/scripts/mesoscale/stats/jevs_mesoscale_sref_cnv_stats.sh
@@ -34,6 +34,7 @@ export cyc=00
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver
 export DATAROOT=/lfs/h2/emc/ptmp/$USER/evs_test/$envir/tmp
 export COMINsrefmean=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver
+export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
 
