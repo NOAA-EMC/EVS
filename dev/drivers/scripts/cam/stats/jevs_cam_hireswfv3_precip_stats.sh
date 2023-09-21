63a04ca7532d4b784757a2a1484d595fc26bf01a
Shelley Melchior <Shelley.Melchior@noaa.gov>
2023-09-21 18:24:49 +0000

    Correcting typo in DATAROOT definition.

--- dev/drivers/scripts/cam/stats/jevs_cam_hireswfv3_precip_stats.sh
+++ dev/drivers/scripts/cam/stats/jevs_cam_hireswfv3_precip_stats.sh
@@ -52,7 +52,7 @@ export PYTHONPATH=$HOMEevs/ush/$COMPONENT:$PYTHONPATH
 
 # Developer Settings
 export envir=prod
-export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$venvir/tmp
+export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
 export COMROOT=/lfs/h2/emc/vpppg/noscrub/${USER}
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/$STEP/$COMPONENT
 export cyc=$(date -d "today" +"%H")
