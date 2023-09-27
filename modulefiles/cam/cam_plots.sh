54f7976099d4636081482bc6da65a988b993412b
marcel caron <marcel.caron@clogin02.cactus.wcoss2.ncep.noaa.gov>
2023-09-27 19:24:53 +0000

    Change intel module load

--- modulefiles/cam/cam_plots.sh
+++ modulefiles/cam/cam_plots.sh
@@ -3,7 +3,8 @@
 
 set +x
 
-module use /apps/prod/lmodules/intel/${intel_ver}
+module load PrgEnv-intel/${PrgEnvintel_ver}
+module load intel/${intel_ver}
 module load ve/evs/${ve_evs_ver}
 module load cray-mpich/${craympich_ver}
 module load cray-pals/${craypals_ver}
