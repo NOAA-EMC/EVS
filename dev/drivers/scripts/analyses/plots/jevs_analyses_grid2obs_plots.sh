11ba8660a1f37e7724dc5a83000d9dabc15a671c
perry shafran <perry.shafran@clogin06.cactus.wcoss2.ncep.noaa.gov>
2023-09-28 20:53:57 +0000

    Adding KEEPDATA back into driver script

--- dev/drivers/scripts/analyses/plots/jevs_analyses_grid2obs_plots.sh
+++ dev/drivers/scripts/analyses/plots/jevs_analyses_grid2obs_plots.sh
@@ -28,6 +28,7 @@ module load prod_envir/${prod_envir_ver}
 
 export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
+export KEEPDATA=YES
 
 export NET=evs
 export STEP=plots
