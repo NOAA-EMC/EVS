8f9308d10a4eb14d727286c02b91df4fcf7ce705
yali mao <yali.mao@clogin07.cactus.wcoss2.ncep.noaa.gov>
2023-09-29 16:33:59 +0000

    Remove FIXevs from plot driver

--- dev/drivers/scripts/wafs/plots/jevs_wafs_atmos_plots.sh
+++ dev/drivers/scripts/wafs/plots/jevs_wafs_atmos_plots.sh
@@ -42,8 +42,6 @@ export VERIF_CASE=grid2grid
 
 export COMIN=${COMIN:-/lfs/h2/emc/vpppg/noscrub/$USER/evs/$evs_ver}
 
-export FIXevs=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/verification/EVS_fix
-
 export USH_DIR=$HOMEevs/ush/$COMPONENT
 export DAYS_LIST=${DAYS_LIST:-"90 31"}
 
