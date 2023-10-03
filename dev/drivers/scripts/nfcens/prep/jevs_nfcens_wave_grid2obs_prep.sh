1dcd041faeb2dfa3696dd07e777ab8727dac5e78
Samira Ardani <samira.ardani@noaa.gov>
2023-10-03 20:50:59 +0000

    KEEPDATA changed to "YES"

--- dev/drivers/scripts/nfcens/prep/jevs_nfcens_wave_grid2obs_prep.sh
+++ dev/drivers/scripts/nfcens/prep/jevs_nfcens_wave_grid2obs_prep.sh
@@ -40,7 +40,7 @@ export envir=prod
 export SENDCOM=${SENDCOM:-YES}
 export SENDECF=${SENDECF:-YES}
 export SENDDBN=${SENDDBN:-YES}
-export KEEPDATA=${KEEPDATA:-NO}
+export KEEPDATA=${KEEPDATA:-YES}
 
 export maillist='alicia.bentley@noaa.gov,samira.ardani@noaa.gov'
 
