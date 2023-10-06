d39040947464df7d4e5207fce91f77e53246a96e
marcel caron <marcel.caron@clogin05.cactus.wcoss2.ncep.noaa.gov>
2023-10-06 20:20:52 +0000

    Remove cyc=00 from CAM det drivers

--- dev/drivers/scripts/stats/cam/jevs_cam_hireswarw_grid2obs_stats.sh
+++ dev/drivers/scripts/stats/cam/jevs_cam_hireswarw_grid2obs_stats.sh
@@ -52,8 +52,7 @@ export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
 export COMROOT=/lfs/h2/emc/vpppg/noscrub/${USER}
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver/$STEP/$COMPONENT
-#export cyc=$(date -d "today" +"%H")
-export cyc=00
+export cyc=$(date -d "today" +"%H")
 export maillist="marcel.caron@noaa.gov"
 
 # Job Settings and Run
