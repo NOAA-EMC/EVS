f882faee1090bbf88f7e11bfda40ad39e3580403
Mallory Row <mallory.row@noaa.gov>
2023-10-10 16:01:20 +0000

    Rename input, temporary, and output variables for prep
    Refs: #1

--- ush/global_det/global_det_wave_prep_trim_ndbc_files.py
+++ ush/global_det/global_det_wave_prep_trim_ndbc_files.py
@@ -34,38 +34,38 @@ ndbc_header2 = ("#yr  mo dy hr mn degT m/s  m/s     m   sec   sec degT   "
                 +"hPa  degC  degC  degC  nmi  hPa    ft\n")
 
 # Trim down files for single date
-for DCOMINndbc_file in glob.glob(os.path.join(DCOMINndbc,
+for ndbc_input_file in glob.glob(os.path.join(DCOMINndbc,
                                              f"{INITDATEp1_dt:%Y%m%d}",
                                              'validation_data', 'marine',
                                              'buoy', '*.txt')):
-    DATAndbc_file = os.path.join(DATA,
+    ndbc_tmp_file = os.path.join(DATA,
                                  f"ndbc_trimmed_{INITDATE_dt:%Y%m%d}_"
-                                 +f"{DCOMINndbc_file.rpartition('/')[2]}")
-    COMOUTndbc_file = os.path.join(f"{COMOUT}.{INITDATE_dt:%Y%m%d}",
+                                 +f"{ndbc_input_file.rpartition('/')[2]}")
+    ndbc_output_file = os.path.join(f"{COMOUT}.{INITDATE_dt:%Y%m%d}",
                                    'ndbc',
-                                   f"{DCOMINndbc_file.rpartition('/')[2]}")
-    if not os.path.exists(COMOUTndbc_file):
-        print(f"Trimming {DCOMINndbc_file} for {INITDATE_dt:%Y%m%d}")
-        DCOMINndbc_file_df = pd.read_csv(
-            DCOMINndbc_file, sep=" ", skiprows=2, skipinitialspace=True,
+                                   f"{ndbc_input_file.rpartition('/')[2]}")
+    if not os.path.exists(ndbc_output_file):
+        print(f"Trimming {ndbc_input_file} for {INITDATE_dt:%Y%m%d}")
+        ndbc_input_file_df = pd.read_csv(
+            ndbc_input_file, sep=" ", skiprows=2, skipinitialspace=True,
             keep_default_na=False, dtype='str', header=None,
             names=ndbc_header1[1:].split()
         )
-        trimmed_DCOMINndbc_file_df = DCOMINndbc_file_df[
-            (DCOMINndbc_file_df['YY'] == f"{INITDATE_dt:%Y}") \
-             & (DCOMINndbc_file_df['MM'] == f"{INITDATE_dt:%m}") \
-             & (DCOMINndbc_file_df['DD'] == f"{INITDATE_dt:%d}")
+        trimmed_ndbc_input_file_df = ndbc_input_file_df[
+            (ndbc_input_file_df['YY'] == f"{INITDATE_dt:%Y}") \
+             & (ndbc_input_file_df['MM'] == f"{INITDATE_dt:%m}") \
+             & (ndbc_input_file_df['DD'] == f"{INITDATE_dt:%d}")
         ]
-        DATAndbc_file_data = open(DATAndbc_file, 'w')
-        DATAndbc_file_data.write(ndbc_header1)
-        DATAndbc_file_data.write(ndbc_header2)
-        DATAndbc_file_data.close()
-        trimmed_DCOMINndbc_file_df.to_csv(
-            DATAndbc_file, header=None, index=None, sep=' ', mode='a'
+        ndbc_tmp_file_data = open(ndbc_tmp_file, 'w')
+        ndbc_tmp_file_data.write(ndbc_header1)
+        ndbc_tmp_file_data.write(ndbc_header2)
+        ndbc_tmp_file_data.close()
+        trimmed_ndbc_input_file_df.to_csv(
+            ndbc_tmp_file, header=None, index=None, sep=' ', mode='a'
         )
         if SENDCOM == 'YES':
-            if os.path.getsize(DATAndbc_file) > 0:
-                print(f"Copying {DATAndbc_file} to {COMOUTndbc_file}")
-                shutil.copy2(DATAndbc_file, COMOUTndbc_file)
+            if os.path.getsize(ndbc_tmp_file) > 0:
+                print(f"Copying {ndbc_tmp_file} to {ndbc_output_file}")
+                shutil.copy2(ndbc_tmp_file, ndbc_output_file)
             else:
-                print("WARNING: {DATAndbc_file} empty, 0 sized")
+                print("WARNING: {ndbc_tmp_file} empty, 0 sized")
