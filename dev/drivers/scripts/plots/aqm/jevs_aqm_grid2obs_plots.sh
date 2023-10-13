b6c7dcd9787fe2439692020dcafc6b192dda3639
Your Name <you@example.com>
2023-10-13 17:07:39 +0000

    Refining EVSIN

--- dev/drivers/scripts/plots/aqm/jevs_aqm_grid2obs_plots.sh
+++ dev/drivers/scripts/plots/aqm/jevs_aqm_grid2obs_plots.sh
@@ -55,7 +55,6 @@ export jobid=$job.${PBS_JOBID:-$$}
 #
 
 export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver}
-export EVSINaqm=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver}/stats/aqm/aqm
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver}
 
 export COMOUT=/lfs/h2/emc/ptmp/$USER/$NET/$evs_ver
