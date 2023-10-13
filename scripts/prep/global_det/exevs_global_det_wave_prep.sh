039dabb3e3177ac8493eb66fd520fb7c5999621b
Mallory Row <mallory.row@noaa.gov>
2023-10-13 14:31:24 +0000

    Rename cycle/cycles to ihr/init_hours
    Refs: #3

--- scripts/prep/global_det/exevs_global_det_wave_prep.sh
+++ scripts/prep/global_det/exevs_global_det_wave_prep.sh
@@ -22,21 +22,21 @@ for MODEL in $MODELNAME; do
     echo ' '
     # Copy the GFS 0.25 degree wave forecast files
     if [ $MODEL == "gfs" ]; then
-        cycles='00 06 12 18'
+        init_hours='00 06 12 18'
         lead_hours='000 006 012 018 024 030 036 042 048 054 060 066 072 078
             084 090 096 102 108 114 120 126 132 138 144 150 156 162
             168 174 180 186 192 198 204 210 216 222 228 234 240 246
             252 258 264 270 276 282 288 294 300 306 312 318 324 330
             336 342 348 354 360 366 372 378 384'
-        for cyc in ${cycles} ; do
+        for ihr in ${init_hours} ; do
             for hr in ${lead_hours} ; do
-                COMINfilename="${COMINgfs}/${cyc}/wave/gridded/gfswave.t${cyc}z.global.0p25.f${hr}.grib2"
-                DATAfilename="${DATA}/gfswave.${INITDATE}.t${cyc}z.global.0p25.f${hr}.grib2"
-                COMOUTfilename="$COMOUT.${INITDATE}/${MODEL}/gfswave.${INITDATE}.t${cyc}z.global.0p25.f${hr}.grib2"
+                COMINfilename="${COMINgfs}/${ihr}/wave/gridded/gfswave.t${ihr}z.global.0p25.f${hr}.grib2"
+                DATAfilename="${DATA}/gfswave.${INITDATE}.t${ihr}z.global.0p25.f${hr}.grib2"
+                COMOUTfilename="$COMOUT.${INITDATE}/${MODEL}/gfswave.${INITDATE}.t${ihr}z.global.0p25.f${hr}.grib2"
                 if [ ! -s $COMINfilename ] ; then
                     if [ $SENDMAIL = YES ] ; then
                         export subject="F${hr} GFS Forecast Data Missing for EVS ${COMPONENT}"
-                        echo "Warning: No GFS forecast was available for ${INITDATE}${cyc}f${hr}" > mailmsg
+                        echo "Warning: No GFS forecast was available for ${INITDATE}${ihr}f${hr}" > mailmsg
                         echo "Missing file is ${COMINfilename}" >> mailmsg
                         echo "Job ID: $jobid" >> mailmsg
                         cat mailmsg | mail -s "$subject" $maillist
