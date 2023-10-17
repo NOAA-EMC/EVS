3cf63c8a8021e3f259e069f5f39547f303412ea7
Roshan Shrestha <roshan.shrestha@noaa.gov>
2023-10-17 19:20:15 +0000

    EVSINsrefmean removed from drivers

--- dev/drivers/scripts/stats/mesoscale/jevs_mesoscale_sref_grid2obs_stats.sh
+++ dev/drivers/scripts/stats/mesoscale/jevs_mesoscale_sref_grid2obs_stats.sh
@@ -35,7 +35,6 @@ export SENDCOM=YES
 export cyc=00
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
-export EVSINsrefmean=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver
 export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
