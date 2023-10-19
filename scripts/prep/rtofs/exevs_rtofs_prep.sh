20519cd919ae3127ffae54fac373ae028957e807
Mallory Row <mallory.row@noaa.gov>
2023-10-19 08:31:34 -0400

    feature/rtofs_cat_logs (#305)
    * Add "cat" of METplus and plotting log files
    
    Refs: #14
    
    * Improve the cat'ing of the METplus and plotting log files
    
    Refs: #14

--- scripts/prep/rtofs/exevs_rtofs_prep.sh
+++ scripts/prep/rtofs/exevs_rtofs_prep.sh
@@ -192,6 +192,16 @@ else
   fi
 fi
 
+# Cat the METplus log files
+log_dir=$DATA/logs
+log_file_count=$(find $log_dir -type f |wc -l)
+if [[ $log_file_count -ne 0 ]]; then
+    for log_file in $log_dir/*; do
+        echo "Start: $log_file"
+        cat $log_file
+        echo "End: $log_file"
+    done
+fi
 
 ##########################
 #  create the masks
