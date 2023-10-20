b12ec86e3228c511307c02b4eef63a24775d78a3
Your Name <you@example.com>
2023-10-20 14:50:42 +0000

    Adding 3-digit evs_ver to aqm scripts

--- dev/drivers/scripts/stats/aqm/jevs_aqm_grid2obs_stats.sh
+++ dev/drivers/scripts/stats/aqm/jevs_aqm_grid2obs_stats.sh
@@ -23,6 +23,10 @@ export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/EVS
 ############################################################
 
 source $HOMEevs/versions/run.ver
+
+export evs_ver=v1.0.0
+evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
+
 module reset
 module load prod_envir/${prod_envir_ver}
 
@@ -49,22 +53,22 @@ export SENDMAIL=YES
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
 
-export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver}
-export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver}
+export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/${NET}/${evs_ver_2d}
+export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver_2d}
 ########################################################################
 
-export maillist=${maillist:-'ho-chun.huang@noaa.gov,alicia.bentley@noaa.gov'}
+#export maillist=${maillist:-'ho-chun.huang@noaa.gov,alicia.bentley@noaa.gov'}
 
-if [ -z "$maillist" ]; then
+#if [ -z "$maillist" ]; then
 
    echo "maillist variable is not defined. Exiting without continuing."
 
-else
+#else
 
    # CALL executable job script here
    $HOMEevs/jobs/JEVS_AQM_STATS
 
-fi
+#fi
 
 ######################################################################
 ## Purpose: This job will generate the grid2obs statistics for the AQM
