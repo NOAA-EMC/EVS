94486efa82724d55f016e82e772c91e307fb4806
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2023-10-23 20:13:30 +0000

    Remove re-defined COMROOT from spatial map scripts

--- dev/drivers/scripts/plots/global_ens/jevs_global_ens_atmos_gefs_precip_spatial_plots.sh
+++ dev/drivers/scripts/plots/global_ens/jevs_global_ens_atmos_gefs_precip_spatial_plots.sh
@@ -32,7 +32,6 @@ export met_v=${met_ver:0:4}
 
 export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver
 export COMOUT=/lfs/h2/emc/ptmp/${USER}/$NET/$evs_ver
-export COMROOT=/lfs/h2/emc/vpppg/noscrub/${USER}/com
 export SENDMAIL=YES
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
