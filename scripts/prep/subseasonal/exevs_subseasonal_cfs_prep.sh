b5180478fe4e109fb3a75becdd1127ab7eef0e30
ShannonShields-NOAA <66634320+ShannonShields-NOAA@users.noreply.github.com>
2023-11-01 15:52:42 -0400

    Bugfix/Subseasonal Final EE2 (#328)
    * Remove evs_ver in create_METplus_subseasonal_output_dirs.py and add prep data restrictions
    
    * Remove date in J-jobs
    
    * Change cp to cpreq in stats and plots scripts
    
    * Remove old code in plots ush scripts
    
    * Add export err=0; err_chk
    
    * Remove jlogfile
    
    * Remove msg in J-jobs and move evs_config sourcing to /scripts
    
    ---------
    
    Co-authored-by: shannon shields <shannon.shields@dlogin06.dogwood.wcoss2.ncep.noaa.gov>
    Co-authored-by: shannon shields <shannon.shields@dlogin03.dogwood.wcoss2.ncep.noaa.gov>
    Co-authored-by: shannon shields <shannon.shields@dlogin07.dogwood.wcoss2.ncep.noaa.gov>

--- scripts/prep/subseasonal/exevs_subseasonal_cfs_prep.sh
+++ scripts/prep/subseasonal/exevs_subseasonal_cfs_prep.sh
@@ -3,7 +3,6 @@
 # Author(s)/Contact(s): Shannon Shields
 # Abstract: This script is run by JEVS_SUBSEASONAL_PREP in jobs/.
 #           This script retrieves data for subseasonal CFS verification.
-# History Log:
 
 set -x
 
@@ -12,6 +11,9 @@ echo
 echo "===== RUNNING EVS SUBSEASONAL CFS PREP  ====="
 export STEP="prep"
 
+# Source config
+source $config
+export err=$?; err_chk
 
 # Set up directories
 mkdir -p $STEP
@@ -19,30 +21,18 @@ cd $STEP
 
 # Check user's configuration file
 python $USHevs/subseasonal/check_subseasonal_config_cfs_prep.py
-status=$?
-[[ $status -ne 0 ]] && exit $status
-[[ $status -eq 0 ]] && echo "Successfully ran check_subseasonal_config_cfs_prep.py"
-echo
+export err=$?; err_chk
 
 # Set up environment variables for initialization, valid, and forecast hours and source them
 python $USHevs/subseasonal/set_init_valid_fhr_subseasonal_prep_info.py
-status=$?
-[[ $status -ne 0 ]] && exit $status
-[[ $status -eq 0 ]] && echo "Successfully ran set_init_valid_fhr_subseasonal_prep_info.py"
-echo
+export err=$?; err_chk
 . $DATA/$STEP/python_gen_env_vars.sh
-status=$?
-[[ $status -ne 0 ]] && exit $status
-[[ $status -eq 0 ]] && echo "Successfully sourced python_gen_env_vars.sh"
-echo
+export err=$?; err_chk
 
 # Retrieve needed data files and set up model information
 mkdir -p data
 python $USHevs/subseasonal/get_cfs_subseasonal_data_files_prep.py
-status=$?
-[[ $status -ne 0 ]] && exit $status
-[[ $status -eq 0 ]] && echo "Successfully ran get_cfs_subseasonal_data_files_prep.py"
-echo
+export err=$?; err_chk
 
 # Send for missing files
 if [ $SENDMAIL = YES ] ; then
