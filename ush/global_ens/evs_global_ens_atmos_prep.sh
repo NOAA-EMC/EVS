25dac731606c50fbd23e96c98af0596a9345e1b5
Mallory Row <mallory.row@noaa.gov>
2023-11-03 14:12:45 +0000

    Clean up empty lines

--- ush/global_ens/evs_global_ens_atmos_prep.sh
+++ ush/global_ens/evs_global_ens_atmos_prep.sh
@@ -8,16 +8,11 @@ set -x
 >run_get_all_gens_sst24h_poe.sh
 
 for model in gefs cmce ecme ; do 
-
   if [ $model = gefs ] ; then
-
    if [ $get_gefs = yes ] ; then	   
-	    
      for ihour in 00 06 12 18 ; do
        for fhr_range in range1 range2 range3 range4 range5 range6 range7 range8 range9 range10 range11 range12 range13 range14 range15 range16; do	     
-
 	>get_data_${model}_${ihour}_${fhr_range}.sh
-
 	if [ $fhr_range = range1 ] ; then
 	  fhr_beg=00
           fhr_end=24
@@ -33,96 +28,71 @@ for model in gefs cmce ecme ; do
 	elif [ $fhr_range = range5 ] ; then
           fhr_beg=102
           fhr_end=120
-
         elif [ $fhr_range = range6 ] ; then
           fhr_beg=126
           fhr_end=144
-
         elif [ $fhr_range = range7 ] ; then
           fhr_beg=150
           fhr_end=168
-
         elif [ $fhr_range = range8 ] ; then
           fhr_beg=174
           fhr_end=192
-
         elif [ $fhr_range = range9 ] ; then
           fhr_beg=198
           fhr_end=216
-
         elif [ $fhr_range = range10 ] ; then
           fhr_beg=222
           fhr_end=240
-
         elif [ $fhr_range = range11 ] ; then
           fhr_beg=246
           fhr_end=264
-
         elif [ $fhr_range = range12 ] ; then
           fhr_beg=270
           fhr_end=288
-
         elif [ $fhr_range = range13 ] ; then
           fhr_beg=294
           fhr_end=312
-
         elif [ $fhr_range = range14 ] ; then
           fhr_beg=318
           fhr_end=336
-
         elif [ $fhr_range = range15 ] ; then
           fhr_beg=342
           fhr_end=360
-
         elif [ $fhr_range = range16 ] ; then
           fhr_beg=366
           fhr_end=384
-
         fi 
-
         echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh $model $ihour $fhr_beg $fhr_end" >> get_data_${model}_${ihour}_${fhr_range}.sh
-
 	chmod +x get_data_${model}_${ihour}_${fhr_range}.sh
         echo "${DATA}/get_data_${model}_${ihour}_${fhr_range}.sh" >> run_get_all_gens_atmos_poe.sh
-
       done	 
      done
-   
      fi
 
      if [ $get_gefs_apcp06h = yes ] ; then
        for ihour in 00 06 12 18 ; do 
-
          >get_data_${model}_${ihour}_apcp06h.sh
- 
          echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh ${model}_apcp06h $ihour 0 384" >> get_data_${model}_${ihour}_apcp06h.sh
          chmod +x get_data_${model}_${ihour}_apcp06h.sh
          echo "${DATA}/get_data_${model}_${ihour}_apcp06h.sh" >> run_get_all_gens_atmos_poe.sh
-
        done
      fi
 
      if [ $get_gefs_apcp24h = yes ] ; then
        for ihour in 00 12 ; do
-
          >get_data_${model}_${ihour}_apcp24h.sh
-
          echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh ${model}_apcp24h $ihour 0 384" >> get_data_${model}_${ihour}_apcp24h.sh
          chmod +x get_data_${model}_${ihour}_apcp24h.sh
          echo "${DATA}/get_data_${model}_${ihour}_apcp24h.sh" >> run_get_all_gens_apcp24h_poe.sh
-
        done	
      fi			
 
      if [ $get_gefs_snow24h = yes ] ; then
        for ihour in 00 12 ; do
-
         >get_data_${model}_${ihour}_snow24h.sh
-
         echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh ${model}_snow24h $ihour 0 384" >> get_data_${model}_${ihour}_snow24h.sh
         chmod +x get_data_${model}_${ihour}_snow24h.sh
         echo "${DATA}/get_data_${model}_${ihour}_snow24h.sh" >> run_get_all_gens_snow24h_poe.sh
-
        done
      fi
 
@@ -143,53 +113,38 @@ for model in gefs cmce ecme ; do
      fi 
 
    elif [ $model = cmce ] ; then  
-
      if [ $get_cmce = yes ] ; then 
-
       for ihour in 00 12 ; do
-
        for fhr_range in range1 range2 range3 range4 range5 range6 range7 range8 ; do
-
 	>get_data_${model}_${ihour}_${fhr_range}.sh
-
 	 if [ $fhr_range = range1 ] ; then
            fhr_beg=00
 	   fhr_end=48
          elif [ $fhr_range = range2 ] ; then
 	   fhr_beg=60
 	   fhr_end=96
-
         elif [ $fhr_range = range3 ] ; then
           fhr_beg=108
           fhr_end=144
-
         elif [ $fhr_range = range4 ] ; then
           fhr_beg=156
           fhr_end=192
-
         elif [ $fhr_range = range5 ] ; then
           fhr_beg=204
           fhr_end=240
-
         elif [ $fhr_range = range6 ] ; then
           fhr_beg=252
           fhr_end=288
-
         elif [ $fhr_range = range7 ] ; then
           fhr_beg=300
           fhr_end=336
-
         elif [ $fhr_range = range8 ] ; then
           fhr_beg=348
           fhr_end=384
-	   	
          fi
-
          echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh $model $ihour $fhr_beg $fhr_end" >> get_data_${model}_${ihour}_${fhr_range}.sh
-
          chmod +x get_data_${model}_${ihour}_${fhr_range}.sh
          echo "${DATA}/get_data_${model}_${ihour}_${fhr_range}.sh" >> run_get_all_gens_atmos_poe.sh
-
        done
       done
 
@@ -197,44 +152,33 @@ for model in gefs cmce ecme ; do
 
      if [ $get_cmce_apcp06h = yes ] ; then
         for ihour in 00 12 ; do
-
           >get_data_${model}_${ihour}_apcp06h.sh
-
           echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh ${model}_apcp06h $ihour 0 384" >> get_data_${model}_${ihour}_apcp06h.sh
           chmod +x get_data_${model}_${ihour}_apcp06h.sh
           echo "${DATA}/get_data_${model}_${ihour}_apcp06h.sh" >> run_get_all_gens_atmos_poe.sh
-
         done
      fi 
 
      if [ $get_cmce_apcp24h = yes ] ; then
         for ihour in 00 12 ; do
-
 	  >get_data_${model}_${ihour}_apcp24h.sh
-
           echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh ${model}_apcp24h $ihour 0 384" >> get_data_${model}_${ihour}_apcp24h.sh
           chmod +x get_data_${model}_${ihour}_apcp24h.sh
           echo "${DATA}/get_data_${model}_${ihour}_apcp24h.sh" >> run_get_all_gens_apcp24h_poe.sh
-
         done
      fi      
 
 
      if [ $get_cmce_snow24h = yes ] ; then
 	for ihour in 00 12 ; do
-
           >get_data_${model}_${ihour}_snow24h.sh
-
           echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh ${model}_snow24h $ihour 0 384" >> get_data_${model}_${ihour}_snow24h.sh
           chmod +x get_data_${model}_${ihour}_snow24h.sh
           echo "${DATA}/get_data_${model}_${ihour}_snow24h.sh" >> run_get_all_gens_snow24h_poe.sh
-
         done
      fi
 
-
    elif [ $model = ecme ] ; then
-
      if [ $get_ecme = yes ] ; then
        >get_data_${model}_atmos.sh
        echo "$USHevs/global_ens/evs_get_gens_atmos_data.sh $model " >> get_data_${model}_atmos.sh 
@@ -257,11 +201,8 @@ for model in gefs cmce ecme ; do
        echo "${DATA}/get_data_${model}_snow24h.sh" >> run_get_all_gens_snow24h_poe.sh
     fi
 
-
    else
-
     echo "WARNING: $model =>  wrong model"
-
    fi
 
 done #end of model
