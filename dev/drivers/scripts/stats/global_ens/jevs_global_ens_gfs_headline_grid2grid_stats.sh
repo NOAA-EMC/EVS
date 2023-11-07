ec11821018232f8251f3d7425e84c6750891b01f
Mallory Row <mallory.row@noaa.gov>
2023-11-07 15:18:59 +0000

    Adjust headline stats jobs resources

--- dev/drivers/scripts/stats/global_ens/jevs_global_ens_gfs_headline_grid2grid_stats.sh
+++ dev/drivers/scripts/stats/global_ens/jevs_global_ens_gfs_headline_grid2grid_stats.sh
@@ -3,8 +3,8 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=01:00:00
-#PBS -l place=vscatter,select=1:ncpus=1:mem=100GB
+#PBS -l walltime=00:15:00
+#PBS -l place=vscatter,select=1:ncpus=1:mem=5GB
 #PBS -l debug=true
 
 set -x
