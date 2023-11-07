2c186fa82be8c17c0826b8e439d39e55dac6cbaa
marcel caron <marcel.caron@dlogin04.dogwood.wcoss2.ncep.noaa.gov>
2023-11-07 22:22:36 +0000

    Copy spatial maps to restart directory

--- ush/cam/cam_plots_precip_spatial_map.py
+++ ush/cam/cam_plots_precip_spatial_map.py
@@ -18,6 +18,7 @@ import logging
 import sys
 import datetime
 import subprocess
+import shutil
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
@@ -29,14 +30,15 @@ class PrecipSpatialMap:
     Create a precipitation spatial map graphic
     """
 
-    def __init__(self, logger, input_dir, output_dir, model_info_dict,
-                 date_info_dict, plot_info_dict, met_info_dict, logo_dir, 
-                 cartopy_data_dir):
+    def __init__(self, logger, input_dir, output_dir, restart_dir, 
+                 model_info_dict, date_info_dict, plot_info_dict, 
+                 met_info_dict, logo_dir, cartopy_data_dir):
         """! Initalize PrecipSpatialMap class
              Args:
                  logger           - logger object
                  input_dir        - path to input directory (string)
                  output_dir       - path to output directory (string)
+                 restart_dir      - path to restart directory (string)
                  model_info_dict  - model infomation dictionary (strings)
                  plot_info_dict   - plot information dictionary (strings)
                  date_info_dict   - date information dictionary (strings)
@@ -49,6 +51,7 @@ class PrecipSpatialMap:
         self.logger = logger
         self.input_dir = input_dir
         self.output_dir = output_dir
+        self.restart_dir = restart_dir
         self.model_info_dict = model_info_dict
         self.date_info_dict = date_info_dict
         self.plot_info_dict = plot_info_dict
@@ -64,6 +67,7 @@ class PrecipSpatialMap:
         self.logger.info(f"Creating preciptation spatial map...")
         self.logger.debug(f"Input directory: {self.input_dir}")
         self.logger.debug(f"Output directory: {self.output_dir}")
+        self.logger.debug(f"Restart directory: {self.restart_dir}")
         self.logger.debug(f"Model information dictionary: "
                           +f"{self.model_info_dict}")
         self.logger.debug(f"Date information dictionary: "
@@ -393,6 +397,18 @@ class PrecipSpatialMap:
                 plt.savefig(image_name)
                 plt.clf()
                 plt.close('all')
+                #Copy to restart directory
+                if restart_dir:
+                    self.logger.info("Copying image to restart directory: "+restart_dir)
+                    shutil.copy2(
+                        image_name, 
+                        os.path.join(
+                            restart_dir,
+                            'precip',
+                            'na',
+                            os.path.basename(image_name)
+                        )
+                    )
                 # Convert png to gif, if possible
                 check_convert = subprocess.run(
                     ['which', 'convert'], stdout=subprocess.DEVNULL,
@@ -405,6 +421,18 @@ class PrecipSpatialMap:
                         ['convert', image_name,
                          image_name.replace('.png', '.gif')]
                     )
+                    #Copy to restart directory
+                    if restart_dir:
+                        self.logger.info("Copying converted image to restart directory: "+restart_dir)
+                        shutil.copy2(
+                            image_name.replace('.png', '.gif'), 
+                            os.path.join(
+                                restart_dir,
+                                'precip',
+                                'na',
+                                os.path.basename(image_name.replace('.png', '.gif'))
+                            )
+                        )
                 else:
                     self.logger.warning("convert executable not in PATH, "
                                         "not creating gif of image")
@@ -413,6 +441,7 @@ def main():
     # Need settings
     INPUT_DIR = os.environ['HOME']
     OUTPUT_DIR = os.environ['HOME']
+    RESTART_DIR = os.environ['RESTART_DIR']
     LOGO_DIR = os.environ['HOME'],
     CARTOPY_DIR = os.environ['cartopyDataDir']
     MODEL_INFO_DICT = {
@@ -478,9 +507,9 @@ def main():
     logger_info = f"Log file: {job_logging_file}"
     print(logger_info)
     logger.info(logger_info)
-    p = PrecipSpatialMap(logger, INPUT_DIR, OUTPUT_DIR, MODEL_INFO_DICT,
-                         DATE_INFO_DICT, PLOT_INFO_DICT, MET_INFO_DICT,
-                         LOGO_DIR, cartopyDataDir)
+    p = PrecipSpatialMap(logger, INPUT_DIR, OUTPUT_DIR, RESTART_DIR, 
+                         MODEL_INFO_DICT, DATE_INFO_DICT, PLOT_INFO_DICT, 
+                         MET_INFO_DICT, LOGO_DIR, cartopyDataDir)
     p.make_precip_spatial_map()
 
 if __name__ == "__main__":
