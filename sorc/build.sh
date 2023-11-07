67ea2b605f95b2df03e5d5f71afdcb4bc2539dbc
Shelley Melchior <Shelley.Melchior@noaa.gov>
2023-11-07 13:55:45 +0000

    Adding Rahul's changes to sorc/

--- sorc/build.sh
+++ sorc/build.sh
@@ -1,15 +1,18 @@
-#!/bin/sh
-set -x
+#!/bin/bash
+set -eux
 
 # Location of PWD and package source directory.
 readonly pkg_root=$(cd "$(dirname "$(readlink -f -n "${BASH_SOURCE[0]}" )" )/.." && pwd -P)
 
 # User options
-export BUILD_CLEAN=${BUILD_CLEAN:-YES}
+BUILD_DEBUG=${BUILD_DEBUG:-NO}
+BUILD_CLEAN=${BUILD_CLEAN:-YES}
+BUILD_VERBOSE=${BUILD_VERBOSE:-YES}
 export EXECevs=${EXECevs:-${pkg_root}/exec}
 
 # Load modules
-source $pkg_root/versions/build.ver
+source ${pkg_root}/versions/build.ver
+set +x
 module reset
 module load PrgEnv-intel/${PrgEnvintel_ver}
 module load intel/${intel_ver}
@@ -17,7 +20,6 @@ module load craype/${craype_ver}
 module load cray-mpich/${craympich_ver}
 module load cray-pals/${craypals_ver}
 module load bacio/${bacio_ver}
-module load w3nco/${w3nco_ver}
 module load w3emc/${w3emc_ver}
 module load ip/${ip_ver}
 module load sp/${sp_ver}
@@ -26,9 +28,7 @@ module load jasper/${jasper_ver}
 module load libpng/${libpng_ver}
 module load zlib/${zlib_ver}
 module list
-w3nco_lib4_name=$(echo ${W3NCO_LIB4##*/})
-export W3NCOLIB=$(echo $w3nco_lib4_name | sed -e "s/lib//g" | sed -e "s/\.a//g")
-export LIBDIRW3NCO=$(echo $W3NCO_LIB4 | sed -e "s/\/${w3nco_lib4_name}//g")
+set -x
 w3emc_lib4_name=$(echo ${W3EMC_LIB4##*/})
 export W3EMCLIB=$(echo $w3emc_lib4_name | sed -e "s/lib//g" | sed -e "s/\.a//g")
 export LIBDIRW3EMC=$(echo $W3EMC_LIB4 | sed -e "s/\/${w3emc_lib4_name}//g")
@@ -36,42 +36,37 @@ bacio_lib4_name=$(echo ${BACIO_LIB4##*/})
 export BACIOLIB=$(echo $bacio_lib4_name | sed -e "s/lib//g" | sed -e "s/\.a//g")
 export LIBDIRBACIO=$(echo $BACIO_LIB4 | sed -e "s/\/${bacio_lib4_name}//g")
 
+# Define compiler and default compiler flags
+export FC=ftn
+export FFLAGS="-g -traceback"
+
 # Make EXECevs
-if [ ! -d $EXECevs ]; then
-    echo "Creating $EXECevs"
-    mkdir -p $EXECevs
+if [ ! -d ${EXECevs} ]; then
+    echo "Creating ${EXECevs}"
+    mkdir -p ${EXECevs}
 else
-    if [ $BUILD_CLEAN = YES ]; then
+    if [ ${BUILD_CLEAN} = "YES" ]; then
         echo "Doing clean build for $EXECevs"
-        rm -r $pkg_root/exec
-        mkdir $pkg_root/exec
+        rm -rf ${pkg_root}/exec
+        mkdir -p ${pkg_root}/exec
     else
-        echo "Not doing clean build...$EXECevs exists"
+        echo "Not doing clean build...${EXECevs} exists"
         exit
     fi
 fi
 
 # Build
-cd $pkg_root/sorc/ecm_gfs_look_alike_new.fd 
-make
-make install
+for code in ecm_gfs_look_alike_new jma_merge ukm_hires_merge pcpconform sref_precip evs_g2g_adjustCMC; do
 
-cd $pkg_root/sorc/jma_merge.fd
-make
-make install
+  cd "${pkg_root}/sorc/${code}.fd"
+  if [ ${BUILD_CLEAN} = "YES" ]; then
+      make VERBOSE=${BUILD_VERBOSE} clean
+  fi
+  if [ ${BUILD_DEBUG} = "YES" ]; then
+      make VERBOSE=${BUILD_VERBOSE} debug
+  else
+      make VERBOSE=${BUILD_VERBOSE}
+  fi
+  make VERBOSE=${BUILD_VERBOSE} install
 
-cd $pkg_root/sorc/ukm_hires_merge.fd
-make
-make install
-
-cd $pkg_root/sorc/pcpconform.fd
-make
-make install
-
-cd $pkg_root/sorc/sref_precip.fd
-make
-make install
-
-cd $pkg_root/sorc/evs_g2g_adjustCMC.fd
-make
-make install
+done
