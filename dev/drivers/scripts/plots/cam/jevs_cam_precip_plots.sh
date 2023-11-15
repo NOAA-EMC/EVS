4a1c789b1bc1c840e3fdb8918370cf0113d0d7af
marcel caron <marcel.caron@clogin01.cactus.wcoss2.ncep.noaa.gov>
2023-11-15 06:28:21 +0000

    Reduce memory load + adjust resource allocation

--- dev/drivers/scripts/plots/cam/jevs_cam_precip_plots.sh
+++ dev/drivers/scripts/plots/cam/jevs_cam_precip_plots.sh
@@ -23,7 +23,7 @@ export job=${PBS_JOBNAME:-jevs_cam_precip_plots}
 export jobid=$job.${PBS_JOBID:-$$}
 export SITE=$(cat /etc/cluster_name)
 export USE_CFP=YES
-export nproc=256
+export nproc=512
 
 # General Verification Settings
 export NET="evs"
