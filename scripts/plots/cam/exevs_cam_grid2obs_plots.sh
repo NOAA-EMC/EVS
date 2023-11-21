58bdcbdef210f893310dbae922edbcd7329046db
Roshan Shrestha <roshan.shrestha@noaa.gov>
2023-11-21 18:00:06 +0000

    plot tar for CAM fixed

--- scripts/plots/cam/exevs_cam_grid2obs_plots.sh
+++ scripts/plots/cam/exevs_cam_grid2obs_plots.sh
@@ -87,12 +87,13 @@ if [ -d $log_dir ]; then
     fi
 fi
 
-# Copy files to desired location
-#all commands to copy output files into the correct EVS COMOUT directory
+# Tar and Copy output files to EVS COMOUT directory
+   cd ${DATA}/${VERIF_CASE}
+   find ./* -name "*.png" -type f -print | tar -cvf ${DATA}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar -T -
+
 if [ $SENDCOM = YES ]; then
-    find ${DATA}/${VERIF_CASE}/out/*/*/*.png -type f -print | tar -cvf ${DATA}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar -T -
-    cpreq ${DATA}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar ${COMOUTplots}/.
-    if [ $SENDDBN = YES ]; then
-        $DBNROOT/bin/dbn_alert MODEL EVS_RZDM $job ${COMOUTplots}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar
-    fi
+    cpreq -v ${DATA}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar ${COMOUTplots}/.
+fi
+if [ $SENDDBN = YES ]; then
+     $DBNROOT/bin/dbn_alert MODEL EVS_RZDM $job ${COMOUTplots}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar
 fi
