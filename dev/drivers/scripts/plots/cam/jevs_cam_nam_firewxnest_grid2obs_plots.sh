12c76b1f6cacb516c176b84656c354d4bba03859
Perry Shafran <Perry.Shafran@noaa.gov>
2023-12-18 20:45:57 +0000

    Returning to production setup

--- dev/drivers/scripts/plots/cam/jevs_cam_nam_firewxnest_grid2obs_plots.sh
+++ dev/drivers/scripts/plots/cam/jevs_cam_nam_firewxnest_grid2obs_plots.sh
@@ -9,7 +9,7 @@
 
 export model=evs
 
-export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/EVS_nco/EVS
+export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/EVS
 
 source $HOMEevs/versions/run.ver
 
@@ -47,10 +47,7 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
 
-export config=$HOMEevs/parm/evs_config/cam/config.evs.cam_nam_firewxnest.prod
-##source $config
-
-export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/$evs_ver_2d
+export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
 export COMOUT=/lfs/h2/emc/ptmp/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
 
 # CALL executable job script here
