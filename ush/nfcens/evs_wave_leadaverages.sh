49db32d4935f933314967eb36967ab40a1d0ff8f
SamiraArdani-NOAA <samira.ardani@noaa.gov>
2023-12-19 23:01:09 +0000

    Bugfix for nfcens.

--- ush/nfcens/evs_wave_leadaverages.sh
+++ ush/nfcens/evs_wave_leadaverages.sh
@@ -23,10 +23,7 @@ ptype='lead_average'
 export GRID2OBS_CONF="${PARMevs}/metplus_config/${STEP}/${COMPONENT}/${RUN}_${VERIF_CASE}"
 
 cd ${DATA}
-if [ ! -s ${DATA}/sfcshp ]; then
-	mkdir ${DATA}/sfcshp
-fi
-
+mkdir -p ${DATA}/sfcshp
 touch plot_all_${MODELNAME}_${RUN}_g2o_plots.sh
 
 # write the commands
