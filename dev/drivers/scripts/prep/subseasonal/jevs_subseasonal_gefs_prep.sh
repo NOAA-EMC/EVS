29e000e2fb5985d3258700bbdbe9a07d50efee1f
ShannonShields-NOAA <66634320+ShannonShields-NOAA@users.noreply.github.com>
2023-12-29 15:12:12 -0500

    Bugfix/subseasonal increase memory for gefs prep (#399)
    * Increase memory for gefs prep job
    
    * Increase memory for gefs prep job

--- dev/drivers/scripts/prep/subseasonal/jevs_subseasonal_gefs_prep.sh
+++ dev/drivers/scripts/prep/subseasonal/jevs_subseasonal_gefs_prep.sh
@@ -4,7 +4,7 @@
 #PBS -q "dev"
 #PBS -A VERF-DEV
 #PBS -l walltime=01:30:00
-#PBS -l place=vscatter,select=1:ncpus=1:ompthreads=1:mem=120GB
+#PBS -l place=vscatter,select=1:ncpus=1:ompthreads=1:mem=150GB
 #PBS -l debug=true
 #PBS -V
 
