38bf8619ae93a2661ac81faf4303a34f56190504
Steven Simon <Steven.Simon@noaa.gov>
2024-01-25 23:43:16 +0000

    Make path components strings.

--- ush/global_ens/global_ens_prep_ghrsst_obs.py
+++ ush/global_ens/global_ens_prep_ghrsst_obs.py
@@ -29,8 +29,8 @@ vdaym1 = os.environ['vdaym1']
 print(vdaym1)
 
 # Input definitions
-daily_source_file=os.path.join(DCOMINghrsst,vdaym1,validation_data,marine,ghrsst,vdaym1+'_OSPO_L4_GHRSST.nc')
-daily_dest_file = os.path.join(COMOUTgefs, ghrsst.t00z.nc)
+daily_source_file=os.path.join(DCOMINghrsst,vdaym1,'validation_data','marine','ghrsst',vdaym1+'_OSPO_L4_GHRSST.nc')
+daily_dest_file = os.path.join(COMOUTgefs, 'ghrsst.t00z.nc')
 
 # Temporary file name
 daily_prepped_file = os.path.join(os.getcwd(), 'atmos.'+daily_source_file.rpartition('/')[2])
