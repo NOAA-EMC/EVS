d987ef18604f52ed759bc55c4990667003af42ff
perry shafran <perry.shafran@clogin07.cactus.wcoss2.ncep.noaa.gov>
2024-01-29 15:02:52 +0000

    Modifying href resources to match mpiexec

--- dev/drivers/scripts/plots/cam/jevs_cam_snowfall_plots.sh
+++ dev/drivers/scripts/plots/cam/jevs_cam_snowfall_plots.sh
@@ -22,7 +22,9 @@ export job=${PBS_JOBNAME:-jevs_cam_snowfall_plots}
 export jobid=$job.${PBS_JOBID:-$$}
 export SITE=$(cat /etc/cluster_name)
 export USE_CFP=YES
-export nproc=256
+export nproc=512
+export ncpu=128
+export VDATE=20240121
 
 # General Verification Settings
 export NET="evs"
@@ -33,7 +35,7 @@ export VERIF_CASE="snowfall"
 export MODELNAME=${COMPONENT}
 
 # EVS Settings
-export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/EVS"
+export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/EVS_resources/EVS"
 export HOMEevs=${HOMEevs:-${PACKAGEROOT}/evs.${evs_ver}}
 export config=$HOMEevs/parm/evs_config/cam/config.evs.prod.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}
 
@@ -48,7 +50,7 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 # Developer Settings
 export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
-export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
+export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/$evs_ver_2d
 export vhr=${vhr:-${vhr}}
 export COMOUT=/lfs/h2/emc/ptmp/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
 
