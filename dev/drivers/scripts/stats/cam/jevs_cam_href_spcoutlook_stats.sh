05138aead169b10ec7d251d2beda7a80d6087d91
perry shafran <perry.shafran@clogin03.cactus.wcoss2.ncep.noaa.gov>
2024-01-30 21:04:55 +0000

    Removing testing directories from some scripts

--- dev/drivers/scripts/stats/cam/jevs_cam_href_spcoutlook_stats.sh
+++ dev/drivers/scripts/stats/cam/jevs_cam_href_spcoutlook_stats.sh
@@ -11,7 +11,7 @@ set -x
 
 export OMP_NUM_THREADS=1
 
-export HOMEevs=/lfs/h2/emc/vpppg/noscrub/${USER}/EVS_resources/EVS
+export HOMEevs=/lfs/h2/emc/vpppg/noscrub/${USER}/EVS
 source $HOMEevs/versions/run.ver
 
 export NET=evs
@@ -33,7 +33,7 @@ export vhr=00
 export run_mpi=yes
 export gather=yes
 
-export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/$evs_ver_2d
+export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export envir=prod
--- dev/drivers/scripts/stats/global_ens/jevs_global_ens_cmce_atmos_precip_stats.sh
+++ dev/drivers/scripts/stats/global_ens/jevs_global_ens_cmce_atmos_precip_stats.sh
@@ -11,7 +11,7 @@ set -x
 
 export OMP_NUM_THREADS=1
 
-export HOMEevs=/lfs/h2/emc/vpppg/noscrub/${USER}/EVS_resources/EVS
+export HOMEevs=/lfs/h2/emc/vpppg/noscrub/${USER}/EVS
 source $HOMEevs/versions/run.ver
 
 export envir=prod
@@ -32,7 +32,7 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 export KEEPDATA=YES
 
 export vhr=00
-export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/$evs_ver_2d
+export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver_2d
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
