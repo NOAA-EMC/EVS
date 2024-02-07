13c3a161a37e1b1b4cbd87c65ba2b43a196e4c8b
yali mao <yali.mao@dlogin08.dogwood.wcoss2.ncep.noaa.gov>
2024-02-07 22:11:22 +0000

    Fix false warnings of missing forecasts at T+3 z

--- ush/wafs/evs_wafs_atmos_stats_preparedata.sh
+++ ush/wafs/evs_wafs_atmos_stats_preparedata.sh
@@ -28,14 +28,20 @@ for ff in $FHOURS ; do
 	    sourcefile=$DCOMINuk/$day/wgrbbul/ukmet_wafs/EGRR_WAFS_0p25_icing_unblended_${day}_${ccfcst}z_t${ff}.grib2
 	fi
     elif [ $CENTER = "us" ] ; then
+	mod=$(( 10#$ccfcst % 6 ))
+	[ $mod -eq 3 ] && continue
 	if [ $RESOLUTION = "0P25" ] ; then
 	    sourcefile=$COMINgfs/gfs.$day/$ccfcst/atmos/gfs.t${ccfcst}z.wafs_0p25_unblended.f${ff}.grib2
 	fi
     elif [ $CENTER = "blend" ] ; then
+	mod=$(( 10#$ccfcst % 6 ))
+	[ $mod -eq 3 ] && continue
         if [ $RESOLUTION = "0P25" ] ; then
 	    sourcefile=$COMINgfs/gfs.$day/$ccfcst/atmos/WAFS_0p25_blended_${day}${ccfcst}f${ff}.grib2
 	fi
     elif [ $CENTER = "gfs" ] ; then
+	mod=$(( 10#$ccfcst % 6 ))
+	[ $mod -eq 3 ] && continue
 	if [ $RESOLUTION = "1P25" ] ; then
 	    sourcefile=$COMINgfs/gfs.$day/$ccfcst/atmos/gfs.t${ccfcst}z.wafs_grb45f${ff}.grib2
 	fi
