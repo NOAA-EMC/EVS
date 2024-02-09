2a0484bb0fe968277087b526ab4adc6605a2c616
marcel caron <marcel.caron@dlogin06.dogwood.wcoss2.ncep.noaa.gov>
2024-02-09 17:06:52 +0000

    Fix jevs_cam_precip_plots WARNINGs Mute group name alerts (2)

--- ush/cam/cam_plots_precip_spatial_map.py
+++ ush/cam/cam_plots_precip_spatial_map.py
@@ -188,8 +188,9 @@ class PrecipSpatialMap:
                         make_plot = True
                     else:
                         make_plot = False
-                        self.logger.warning(f"{model_num_file} does not exist, "
-                                        +"not making plot")
+                        if "group" not in str(model_num_name):
+                            self.logger.warning(f"{model_num_file} does not exist, "
+                                                +"not making plot")
                 else:
                     make_plot = False
             if make_plot:
