c7b413ada7ebbc008da2f4374fe1e342bad7ece0
SamiraArdani-NOAA <samira.ardani@noaa.gov>
2024-02-15 23:01:25 +0000

    Files were added.

---
+++ scripts/prep/nwps/exeve_nwps_wave_grid2obs_prep.sh
@@ -0,0 +1,181 @@
+#!/bin/bash
+###############################################################################
+# Name of Script: exevs_glwu_fcst_prep.sh
+# Purpose of Script: To pre-process glwu forecast data into the same spatial
+#    and temporal scales as validation data.
+# Author: Samira Ardani (samira.ardani@noaa.gov)
+###############################################################################
+
+set -x
+
+##############################
+## grid2obs GLWU Model Prep 
+###############################
+
+cd $DATA
+echo "Starting grid2obs_prep for ${MODELNAME}_${RUN}"
+
+echo ' '
+echo ' *************************************'
+echo " *** ${MODELNAME}-${RUN} grid2obs prep ***"
+echo ' *************************************'
+echo ' '
+echo "Starting at : `date`"
+echo '-------------'
+echo ' '
+
+
+
+# n024 is nowcast = f000 forecast
+mkdir -p $COMOUTprep/glwu.$VDATE/$RUN
+mkdir -p $DATA/glwu.$VDATE/$RUN
+
+################################################################################
+# Create today's GLWU individual fcst grib2 files and add them to the archive
+################################################################################
+mkdir -p ${DATA}/glwu_nc
+mkdir -p ${DATA}/glwu_grib2
+
+echo 'Copying GLWU wave grib2 and nc files'
+
+HHs='00 06 12 18'
+leads='000 024 048 072 096 120 144 168 192'
+
+mtype='glwu glwu_lc grlc_2p5km grlc_2p5km_lc grlc_2p5km_lc_sr grlc_2p5km_sr grlr_500m grlr_500m_lc'
+
+
+if [[ ${mtype} == "glwu" || ${mtype} == "glwu_lc" ]];then
+  for HH in ${HHs} ; do
+     for lead in ${leads} ; do
+	filename="glwu.${mtype}.t${HH}z.nc"
+	newname="glwu.${mtype}.${INITDATE}.t${HH}z.nc"
+	if [ ! -s glwu.${INITDATE}/${filename} ]; then
+	   export subject="F${lead} GLWU Forecast Data Missing for EVS ${COMPONENT}"
+	   echo "Warning: No GLWU forecast was available for ${INITDATE}${HH}f${lead}" > mailmsg
+	   echo "Missing file is glwu.${INITDATE}/${filename}" >> mailmsg
+	   echo "Job ID: $jobid" >> mailmsg
+	   cat mailmsg | mail -s "$subject" $MAILTO
+        else
+	   if [ ! -s ${ARCglwu}/${newname} ]; then
+	      cp -v glwu.${INITDATE}/${filename} $DATA/glwu_nc/${newname}/
+	      if [ $SENDCOM = YES ]; then
+		 cp -v $DATA/glwu_nc/ ${ARCglwu}/glwu_nc/${newname}/
+	      fi
+	   fi
+	fi
+   done
+  done
+else
+  for HH in ${HHs} ; do
+     for lead in ${leads} ; do	   
+	filename="glwu.${mtype}.t${HH}z.grib2"
+	if [ ! -s glwu.${INITDATE}/${filename} ]; then
+	    export subject="F${hr} GLWU Forecast Data Missing for EVS ${COMPONENT}"
+	    echo "Warning: No GLWU forecast was available for ${INITDATE}${HH}f${lead}" > mailmsg
+	    echo "Missing file is glwu.${INITDATE}/${filename}" >> mailmsg
+	    echo "Job ID: $jobid" >> mailmsg
+	    cat mailmsg | mail -s "$subject" $MAILTO
+        else
+	    if [ ! -s ${ARCglwu}/${newname} ]; then
+	       cp -v glwu.${INITDATE}/${filename} $DATA/glwu_grib2/${newname}/
+	       if [ $SENDCOM = YES ]; then
+		  cp -v $DATA/glwu_grib2/ ${ARCglwu}/glwu_grib2/${newname}/
+	       fi
+	    fi
+	fi
+   done
+  done
+fi
+
+############################################################
+# convert NDBC *.txt files into a netcdf file using ASCII2NC
+############################################################
+
+export RUN=ndbc
+mkdir -p $COMOUTprep/glwu.$VDATE/$RUN
+mkdir -p ${DATA}/ncfiles
+export MET_NDBC_STATIONS=${FIXevs}/ndbc_stations/ndbc_stations.xml
+ndbc_txt_ncount=$(ls -l $DCOMINndbc/$VDATE/validation_data/marine/buoy/*.txt |wc -l)
+if [ $ndbc_txt_ncount -gt 0 ]; then
+   run_metplus.py -c $CONFIGevs/metplus_glwu.conf \
+     -c $CONFIGevs/grid2obs/$STEP/ASCII2NC_obsNDBC.conf
+   export err=$?; err_chk
+   if [ $SENDCOM = YES ]; then
+	   cp -v $DATA/ncfiles/ndbc.${INITDATE}.nc ${COMOUT}.${INITDATE}/${MODELNAME}/${VERIF_CASE}/.
+   fi
+else
+	if [ $SENDMAIL = YES ] ; then
+ 	export subject="NDBC Data Missing for EVS ${COMPONENT}"
+	echo "Warning: No NDBC data was available for valid date $VDATE." > mailmsg
+	echo "Missing files are located at $COMINobs/$VDATE/validation_data/marine/buoy/." >> mailmsg
+	echo "Job ID: $jobid" >> mailmsg
+	cat mailmsg | mail -s "$subject" $MAILTO
+fi
+
+
+############################################
+# get the GDAS prepbufr files for yesterday 
+############################################
+
+echo 'Copying GDAS prepbufr files'
+
+for HH in 00 06 12 18 ; do
+	export inithour=t${HH}z
+	if [ ! -s ${COMINobsproc}.${INITDATE}/${HH}/atmos/gdas.${inithour}.prepbufr ]; then
+		export subject="GDAS Prepbufr Data Missing for EVS ${COMPONENT}"
+		echo "WARNING: No GDAS Prepbufr was available for init date ${INITDATE}${HH}" > mailmsg
+		echo "WARNING: Missing file is ${COMINobsproc}.${INITDATE}/${HH}/atmos/gdas.${inithour}.prepbufr" >> mailmsg
+		echo "Job ID: $jobid" >> mailmsg
+		cat mailmsg | mail -s "$subject" $MAILTO
+	else
+		cp -v ${COMINobsproc}.${INITDATE}/${HH}/atmos/gdas.${inithour}.prepbufr ${DATA}/gdas.${INITDATE}${HH}.prepbufr
+	fi
+done
+
+
+############################################
+# run PB2NC                                 
+############################################
+
+echo 'Run pb2nc'
+
+for HH in 00 12; do
+	export HH=$HH
+	export inithour=t${HH}z
+	if [ -s ${DATA}/gdas.${INITDATE}${HH}.prepbufr ]; then
+		if [ ! -s ${COMOUT}.${INITDATE}/${MODELNAME}/${VERIF_CASE}/gdas.${INITDATE}${HH}.nc ]; then
+			run_metplus.py ${PARMevs}/metplus_config/machine.conf ${PARMevs}/metplus_config/${STEP}/${COMPONENT}/${RUN}_${VERIF_CASE}/PB2NC_wave.conf
+			export err=$?; err_chk
+			if [ $SENDCOM = YES ]; then
+				cp -v $DATA/ncfiles/gdas.${INITDATE}${HH}.nc ${COMOUT}.${INITDATE}/${MODELNAME}/${VERIF_CASE}/.
+			fi
+		fi
+		chmod 640 $DATA/ncfiles/gdas.${INITDATE}${HH}.nc
+		chgrp rstprod $DATA/ncfiles/gdas.${INITDATE}${HH}.nc	
+	fi
+done
+
+
+# Create the masks
+$HOMEevs/scripts/$STEP/$COMPONENT/glwu_regions.sh
+
+##########################################
+## Cat the prep log files
+###########################################
+log_dir=$DATA/logs
+log_file_count=$(find $log_dir -type f |wc -l)
+if [[ $log_file_count -ne 0 ]]; then
+	for log_file in $log_dir/*; do
+		echo "Start: $log_file"
+		cat $log_file
+		echo "End: $log_file"
+	done
+fi
+########################################
+
+echo ' '
+echo "Ending at : `date`"
+echo ' '
+echo " *** End of ${MODELNAME}-${RUN} grid2obs prep *** "
+
+################################ END OF SCRIPT ###########
---
+++ scripts/stats/nwps/exev_nwps_wave_grid2obs_stats.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+###############################################################################
+# Name of Script: exevs_glwu_stats.sh
+# Purpose of Script: To create stat files for GLWU forecasts verified with
+#    NDBC buoy data using MET/METplus.
+# Author: Samira Ardani (samira.ardani@noaa.gov)
+###############################################################################
+
+set -x
+
+# check if ndbc nc file exists; exit if not
+if [ ! -s $DCOMINndbc/glwu.$VDATE/$RUN/ndbc.${VDATE}.nc ] ; then
+	echo "WARNING: No NDBC data in $DCOMINndbc/${INITDATEp1}/validation_data/marine/buoy"
+	if [ $SENDMAIL = YES ] ; then
+		export subject="NDBC Data Missing for EVS ${COMPONENT}"
+		export maillist=${maillist:-'alicia.bentley@noaa.gov,samira.ardani@noaa.gov'}
+		echo "Warning: No NDBC data was available for valid date $VDATE." > mailmsg
+		cat mailmsg | mail -s "$subject" $MAILTO
+	fi
+fi
+
+
+############################
+## grid2obs wave model stats 
+#############################
+
+cd $DATA
+echo "Starting grid2obs_stats for ${MODELNAME}_${RUN}"
+
+echo ' '
+echo ' ******************************************'
+echo " *** ${MODELNAME}-${RUN} grid2obs stats ***"
+echo ' ******************************************'
+echo ' '
+echo "Starting at : `date`"
+echo '-------------'
+echo ' '
+
+mkdir -p ${DATA}/gribs
+mkdir -p ${DATA}/ncfiles
+mkdir -p ${DATA}/all_stats
+mkdir -p ${DATA}/jobs
+mkdir -p ${DATA}/logs
+mkdir -p ${DATA}/confs
+mkdir -p ${DATA}/tmp
+
+vhours='00 06 12 18'
+
+lead_hours='0 6 12 18 24 30 36 42 48 54 60 66 72 78
+            84 90 96 102 108 114 120 126 132 138 144 150 156 162
+            168 174 180 186 192 198 204 210 216 222 228 234 240 246
+            252 258 264 270 276 282 288 294 300 306 312 318 324 330
+	    336 342 348 354 360 366 372 378 384'
+
+export GRID2OBS_CONF="${PARMevs}/metplus_config/${COMPONENT}/${RUN}_${VERIF_CASE}/${STEP}"
+
+cd ${DATA}
+
+############################################
+# create point_stat files
+############################################
+echo ' '
+echo 'Creating point_stat files'
+for vhr in ${vhours} ; do
+    vhr2=$(printf "%02d" "${vhr}")
+    if [ ${vhr} = '00' ] ; then
+       wind_level_str="'{ name=\"WIND\"; level=\"(0,*,*)\"; }'"
+       htsgw_level_str="'{ name=\"HTSGW\"; level=\"(0,*,*)\"; }'"
+       perpw_level_str="'{ name=\"PERPW\"; level=\"(0,*,*)\"; }'"
+    elif [ ${vhr} = '06' ] ; then
+       wind_level_str="'{ name=\"WIND\"; level=\"(2,*,*)\"; }'"
+       htsgw_level_str="'{ name=\"HTSGW\"; level=\"(2,*,*)\"; }'"
+       perpw_level_str="'{ name=\"PERPW\"; level=\"(2,*,*)\"; }'"
+    elif [ ${vhr} = '12' ] ; then
+       wind_level_str="'{ name=\"WIND\"; level=\"(4,*,*)\"; }'"
+       htsgw_level_str="'{ name=\"HTSGW\"; level=\"(4,*,*)\"; }'"
+       perpw_level_str="'{ name=\"PERPW\"; level=\"(4,*,*)\"; }'"
+
+    elif [ ${vhr} = '18' ] ; then
+       wind_level_str="'{ name=\"WIND\"; level=\"(6,*,*)\"; }'"
+       htsgw_level_str="'{ name=\"HTSGW\"; level=\"(6,*,*)\"; }'"
+       perpw_level_str="'{ name=\"PERPW\"; level=\"(6,*,*)\"; }'"
+    fi
+    for lead in ${lead_hours} ; do
+       matchtime=$(date --date="${VDATE} ${vhr2} ${lead} hours ago" +"%Y%m%d %H")
+       match_date=$(echo ${matchtime} | awk '{print $1}')
+       match_hr=$(echo ${matchtime} | awk '{print $2}')
+       match_fhr=$(printf "%02d" "${match_hr}")
+       flead=$(printf "%03d" "${lead}")
+       flead2=$(printf "%02d" "${lead}")
+       COMINglwufilename=${COMINglwunc}/${RUN}.${VDATE}/${MODELNAME}/${VERIF_CASE}/glwu.${VDATE}${vhr2}.nc 
+       DATAglwuncfilename=${DATA}/ncfiles/glwu.${VDATE}${vhr2}.nc
+       COMINmodelfilename=$COMIN/prep/$COMPONENT/${RUN}.${match_date}/${MODELNAME}/${VERIF_CASE}/${MODELNAME}.${RUN}.${match_date}.t${match_fhr}z.nc                  
+       DATAmodelfilename=$DATA/gribs/${MODELNAME}.${RUN}.${match_date}.t${match_fhr}z.nc
+       DATAstatfilename=$DATA/all_stats/point_stat_fcst${MODNAM}_obsGLWU_NDBC_${flead2}0000L_${VDATE}_${vhr}0000V.stat
+       COMOUTstatfilename=$COMOUTsmall/point_stat_fcst${MODNAM}_obsGLWU_NDBC_${flead2}0000L_${VDATE}_${vhr}0000V.stat
+       if [[ -s $COMOUTstatfilename ]]; then
+	       cp -v $COMOUTstatfilename $DATAstatfilename
+       else
+	       if [[ ! -s $DATAglwuncfilename ]]; then
+		       if [[ -s $COMINglwuncfilename ]]; then
+			       cp -v $COMINglwuncfilename $DATAglwuncfilename
+		       else
+			       echo "DOES NOT EXIST $COMINglwuncfilename"
+		       fi
+	       fi
+	       if [[ -s $DATAglwuncfilename ]]; then
+		       if [[ ! -s $DATAmodelfilename ]]; then
+			       if [[ -s $COMINmodelfilename ]]; then
+				       cp -v $COMINmodelfilename $DATAmodelfilename
+			       else
+				       echo "DOES NOT EXIST $COMINmodelfilename"
+			       fi
+		       fi
+		       if [[ -s $DATAmodelfilename ]]; then
+			       echo "export wind_level_str=${wind_level_str}" >> ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       echo "export htsgw_level_str=${htsgw_level_str}" >> ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       echo "export perpw_level_str=${perpw_level_str}" >> ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       echo "export VHR=${vhr}" >> ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       echo "export lead=${flead}" >> ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       echo "${METPLUS_PATH}/ush/run_metplus.py ${PARMevs}/metplus_config/machine.conf ${GRID2OBS_CONF}/PointStat_fcstGEFS_obsGDAS_climoERA5_Wave_Multifield.conf" >> ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       echo "export err=\$?; err_chk" >> ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       if [ $SENDCOM = YES ]; then
+				       echo "cp -v $DATAstatfilename $COMOUTstatfilename" >> ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       fi
+			       chmod +x ${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh
+			       echo "${DATA}/jobs/run_${MODELNAME}_${RUN}_${VDATE}${vhr}_f${flead}_g2o.sh" >> ${DATA}/jobs/run_all_${MODELNAME}_${RUN}_g2o_poe.sh
+		       fi
+	       fi
+       fi
+done
+done
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+#####################																					
+# Run the command file
+#####################                                                                                                                                                                        
+if [[ -s ${DATA}/jobs/run_all_${MODELNAME}_${RUN}_g2o_poe.sh ]]; then
+    if [ ${run_mpi} = 'yes' ] ; then
+       export LD_LIBRARY_PATH=/apps/dev/pmi-fix:$LD_LIBRARY_PATH
+       mpiexec -np 36 -ppn 36 --cpu-bind verbose,core cfp ${DATA}/jobs/run_all_${MODELNAME}_${RUN}_g2o_poe.sh
+    else
+	echo "not running mpiexec"
+	sh ${DATA}/jobs/run_all_${MODELNAME}_${RUN}_g2o_poe.sh
+    fi
+fi
+
+##########################
+# Gather all the files
+#########################
+if [ $gather = yes ] ; then
+# check to see if the small stat files are there
+   nc=$(ls ${DATA}/all_stats/*stat | wc -l | awk '{print $1}')
+   if [ "${nc}" != '0' ]; then
+           echo " Found ${nc} ${DATA}/all_stats/*stat files for ${VDATE}"
+	   mkdir -p ${DATA}/stats
+           # Use StatAnalysis to gather the small stat files into one file
+           run_metplus.py ${PARMevs}/metplus_config/machine.conf ${GRID2OBS_CONF}/StatAnalysis_fcstGLWU_obsGLWU.conf
+	   if [ $SENDCOM = YES ]; then
+		   if [ -s ${DATA}/stats/evs.stats.${MODELNAME}.${RUN}.${VERIF_CASE}.v${VDATE}.stat ]; then
+			   cp -v ${DATA}/stats/evs.stats.${MODELNAME}.${RUN}.${VERIF_CASE}.v${VDATE}.stat ${COMOUTfinal}/.
+		   else
+			   echo "DOES NOT EXIST ${DATA}/stats/evs.stats.${MODELNAME}.${RUN}.${VERIF_CASE}.v${VDATE}.stat"
+		   fi
+	   fi
+   else
+	   echo "NO SMALL STAT FILES FOUND IN ${DATA}/all_stats"
+   fi
+fi
+
+##################################################################
+# Revisit part below later and keep it in case you need to use it:
+
+# run Point_Stat
+run_metplus.py -c $CONFIGevs/metplus_glwu.conf \
+-c $CONFIGevs/${VERIF_CASE}/$STEP/PointStat_fcstGLWU_obsNDBC_htsgw.conf
+
+# check if stat files exist; exit if not
+if [ ! -s $COMOUTsmall/point_stat_GLWU_NDBC_HTSGW_1440000L_${VDATE}_000000V.stat ] ; then
+   echo "Missing GLWU_NDBC_HTSGW stat files for $VDATE" 
+   exit
+fi
+
+# sum small stat files into one big file using Stat_Analysis
+mkdir -p $COMOUTfinal
+
+run_metplus.py -c $CONFIGevs/metplus_glwu.conf \
+-c $CONFIGevs/${VERIF_CASE}/$STEP/StatAnalysis_fcstGLWU_obsNDBC.conf
+
+# archive final stat file
+rsync -av $COMOUTfinal $ARCHevs
+
+#############################
+#Cat the stat log files
+#############################
+
+log_dir=$DATA/logs
+log_file_count=$(find $log_dir -type f |wc -l)
+if [[ $log_file_count -ne 0 ]]; then
+	for log_file in $log_dir/*; do
+		echo "Start: $log_file"
+		cat $log_file
+		echo "End: $log_file"
+	done
+fi
+
+echo ' '
+echo "Ending at : `date`"
+echo ' '
+echo " *** End of ${MODELNAME}-${RUN} grid2obs stat *** "
+echo ' '
+################################ END OF SCRIPT ################################
