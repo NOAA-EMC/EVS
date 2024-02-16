b6a6259988791fd23a5e369120e946b5949bedf2
SamiraArdani-NOAA <samira.ardani@noaa.gov>
2024-02-16 21:53:26 +0000

    prep STEP was modified.

--- scripts/prep/glwu/exevs_glwu_wave_grid2obs_prep.sh
+++ scripts/prep/glwu/exevs_glwu_wave_grid2obs_prep.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ###############################################################################
-# Name of Script: exevs_glwu_fcst_prep.sh
+# Name of Script: exevs_glwu_wave_grid2obs_prep.sh
 # Purpose of Script: To pre-process glwu forecast data into the same spatial
 #    and temporal scales as validation data.
 # Author: Samira Ardani (samira.ardani@noaa.gov)
@@ -24,70 +24,58 @@ echo "Starting at : `date`"
 echo '-------------'
 echo ' '
 
-
-
-# n024 is nowcast = f000 forecast
 mkdir -p $COMOUTprep/glwu.$VDATE/$RUN
 mkdir -p $DATA/glwu.$VDATE/$RUN
 
 ################################################################################
 # Create today's GLWU individual fcst grib2 files and add them to the archive
 ################################################################################
-mkdir -p ${DATA}/glwu_nc
-mkdir -p ${DATA}/glwu_grib2
+mkdir -p ${DATA}/nc
+mkdir -p ${DATA}/grib2
 
 echo 'Copying GLWU wave grib2 and nc files'
 
-HHs='00 06 12 18'
+HHs='01 07 13 19'
 leads='000 024 048 072 096 120 144'
 
-mtype='glwu glwu_lc grlc_2p5km grlc_2p5km_lc grlc_2p5km_lc_sr grlc_2p5km_sr grlr_500m grlr_500m_lc'
+mtypes='glwu glwu_lc grlc_2p5km grlc_2p5km_lc grlc_2p5km_lc_sr grlc_2p5km_sr grlr_500m grlr_500m_lc'
 
-
-if [[ ${mtype} == "glwu" || ${mtype} == "glwu_lc" ]];then
-  for HH in ${HHs} ; do
-     for lead in ${leads} ; do
-	filename="glwu.${mtype}.t${HH}z.nc"
-	newname="glwu.${mtype}.${INITDATE}.t${HH}z.nc"
-	if [ ! -s glwu.${INITDATE}/${filename} ]; then
-	   export subject="F${lead} GLWU Forecast Data Missing for EVS ${COMPONENT}"
-	   echo "Warning: No GLWU forecast was available for ${INITDATE}${HH}f${lead}" > mailmsg
-	   echo "Missing file is glwu.${INITDATE}/${filename}" >> mailmsg
-	   echo "Job ID: $jobid" >> mailmsg
-	   cat mailmsg | mail -s "$subject" $MAILTO
-        else
-	   if [ ! -s ${ARCglwu}/${newname} ]; then
-	      cp -v glwu.${INITDATE}/${filename} $DATA/glwu_nc/${newname}/
-	      if [ $SENDCOM = YES ]; then
-		 cp -v $DATA/glwu_nc/ ${ARCglwu}/glwu_nc/${newname}/
-	      fi
-	   fi
+for mtype in $mtypes; do
+	if [[ ${mtype} == "grlc_2p5km" || ${mtype} == "grlc_2p5km_lc" ]]; then
+		for HH in ${HHs} ; do
+			filename="glwu.${mtype}.t${HH}z.grib2"
+			COMINfilename="${COMINglwu}/${MODELNAME}.${INITDATE}/${filename}"
+			DATAfilename="${DATA}/grib2/${MODELNAME}.${INITDATE}.t${HH}z.grib2"
+			if [ ! -s COMINfilename ]; then
+				if [ SENDMAIL = YES ]; then
+					export subject="F${lead} GLWU Forecast Data Missing for EVS ${COMPONENT}"
+					echo "WARNING: No GLWU forecast was available for ${INITDATE}${HH}f${lead}" > mailmsg
+					echo "Missing file is glwu.${INITDATE}/${filename}" >> mailmsg
+					echo "Job ID: $jobid" >> mailmsg
+					cat mailmsg | mail -s "$subject" $MAILTO
+				fi
+			else
+				cp -v $COMINfilename $DATAfilename
+			fi
+			if [ -s $DATAfilename ]; then
+				for lead in ${leads} ; do		
+					if [ $lead = 0 ]; then
+						grib2_match=":surface:anl:"
+					else
+						grib2_match=":${fcst} hour fcst:"
+					fi
+					DATAfilename_fcst="${DATA}/grib2/${MODELNAME}.${INITDATE}.t${HH}z.f${fcst}.grib2"
+					ARCmodelfilename="${ARCmodel}/${MODELNAME}.${INITDATE}.t${HH}z.f${fcst}.grib2"
+					wgrib2 $DATAfilename -match "$grib2_match" -grib $DATAfilename_fcst > /dev/null
+					export err=$?; err_chk
+					if [ $SENDCOM = YES ]; then
+						cp -v $DATAfilename_fcst ${ARCmodelfilename}/.
+					fi
+				done
+			fi
+		done
 	fi
-   done
-  done
-else
-  for HH in ${HHs} ; do
-     for lead in ${leads} ; do	   
-	filename="glwu.${mtype}.t${HH}z.grib2"
-	if [ ! -s glwu.${INITDATE}/${filename} ]; then
-		if [ SENDMAIL  YES ]; then
-	    		export subject="F${hr} GLWU Forecast Data Missing for EVS ${COMPONENT}"
-	    		echo "Warning: No GLWU forecast was available for ${INITDATE}${HH}f${lead}" > mailmsg
-	    		echo "Missing file is glwu.${INITDATE}/${filename}" >> mailmsg
-	    		echo "Job ID: $jobid" >> mailmsg
-	    		cat mailmsg | mail -s "$subject" $MAILTO
-		fi
-        else
-	    if [ ! -s ${ARCglwu}/${newname} ]; then
-	       cp -v glwu.${INITDATE}/${filename} $DATA/glwu_grib2/${newname}/
-	       if [ $SENDCOM = YES ]; then
-		  cp -v $DATA/glwu_grib2/ ${ARCglwu}/glwu_grib2/${newname}/
-	       fi
-	    fi
-	fi
-   done
-  done
-fi
+done
 
 ############################################################
 # convert NDBC *.txt files into a netcdf file using ASCII2NC
@@ -157,10 +145,6 @@ for HH in 00 12; do
 		chgrp rstprod $DATA/ncfiles/gdas.${INITDATE}${HH}.nc	
 	fi
 done
-
-
-# Create the masks
-$HOMEevs/scripts/$STEP/$COMPONENT/glwu_regions.sh
 
 ##########################################
 ## Cat the prep log files
