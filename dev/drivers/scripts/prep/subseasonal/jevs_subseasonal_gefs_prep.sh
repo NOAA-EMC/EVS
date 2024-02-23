e9ca5143d3cadfd45d5633f3934480dc136dbe59
shannon shields <shannon.shields@dlogin01.dogwood.wcoss2.ncep.noaa.gov>
2024-02-23 16:58:30 +0000

    Increase gefs prep memory

--- dev/drivers/scripts/prep/subseasonal/jevs_subseasonal_gefs_prep.sh
+++ dev/drivers/scripts/prep/subseasonal/jevs_subseasonal_gefs_prep.sh
@@ -4,7 +4,7 @@
 #PBS -q "dev"
 #PBS -A VERF-DEV
 #PBS -l walltime=02:00:00
-#PBS -l place=vscatter,select=1:ncpus=1:ompthreads=1:mem=230GB
+#PBS -l place=vscatter,select=1:ncpus=1:ompthreads=1:mem=260GB
 #PBS -l debug=true
 #PBS -V
 
