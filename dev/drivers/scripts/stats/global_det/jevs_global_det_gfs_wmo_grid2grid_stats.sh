71a6ca30a06b26ab99b91128cac43ea82a8bc25c
Mallory Row <mallory.row@noaa.gov>
2024-02-26 20:19:55 +0000

    Adjust walltime and memory

--- dev/drivers/scripts/stats/global_det/jevs_global_det_gfs_wmo_grid2grid_stats.sh
+++ dev/drivers/scripts/stats/global_det/jevs_global_det_gfs_wmo_grid2grid_stats.sh
@@ -3,8 +3,8 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=01:00:00
-#PBS -l place=vscatter:exclhost,select=1:ncpus=40:ompthreads=1:mem=25GB
+#PBS -l walltime=00:15:00
+#PBS -l place=vscatter:exclhost,select=1:ncpus=40:ompthreads=1:mem=50GB
 #PBS -l debug=true
 #PBS -V
 
