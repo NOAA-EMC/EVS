567748ce8f65165457c6edb1305174603f36ca43
Samira Ardani <samira.ardani@noaa.gov>
2024-03-22 17:43:14 +0000

    Modifications were applied to run prep step.

--- scripts/prep/glwu/exevs_glwu_wave_grid2obs_prep.sh
+++ scripts/prep/glwu/exevs_glwu_wave_grid2obs_prep.sh
@@ -24,8 +24,10 @@ echo "Starting at : `date`"
 echo '-------------'
 echo ' '
 
+###############################################################################
 # Create today's GLWU individual fcst grib2 files and add them to the archive
 ################################################################################
+
 mkdir -p ${DATA}/nc
 mkdir -p ${DATA}/gribs
 #mkdir -p $COMOUTprep/glwu.${INITDATE}/$RUN
@@ -33,20 +35,20 @@ mkdir -p ${DATA}/gribs
 echo 'Copying GLWU wave grib2 and nc files'
 
 HHs='01 07 13 19'
-leads='000 024 048 072 096 120 144'
+fcsts='0 24 48 72 96 120 144'
 
-mtypes='glwu glwu_lc grlc_2p5km grlc_2p5km_lc grlc_2p5km_lc_sr grlc_2p5km_sr grlr_500m grlr_500m_lc'
+#mtypes='glwu glwu_lc grlc_2p5km grlc_2p5km_lc grlc_2p5km_lc_sr grlc_2p5km_sr grlr_500m grlr_500m_lc'
 
 for mtype in glwu glwu_lc grlc_2p5km grlc_2p5km_lc grlc_2p5km_lc_sr grlc_2p5km_sr grlr_500m grlr_500m_lc ; do
-	if [ ${mtype} = grlc_2p5km ] || [ ${mtype} = grlc_2p5km_lc ]; then
+	if [ "${mtype}" == "grlc_2p5km" ] || [ "${mtype}" == "grlc_2p5km_lc" ]; then
 		for HH in ${HHs} ; do
-			filename="glwu.${mtype}.t${HH}z.grib2"
+			filename="${MODELNAME}.${mtype}.t${HH}z.grib2"
 			COMINfilename="${COMINglwu}/${MODELNAME}.${INITDATE}/${filename}"
-			DATAfilename="${DATA}/gribs/${MODELNAME}.${INITDATE}.${filename}.t${HH}z.grib2"
-			if [ ! -s COMINfilename ]; then
+			DATAfilename="${DATA}/gribs/${MODELNAME}.${mtype}.${INITDATE}.t${HH}z.grib2"
+			if [ ! -s ${COMINfilename} ]; then
 				if [ SENDMAIL = YES ]; then
-					export subject="F${lead} GLWU Forecast Data Missing for EVS ${COMPONENT}"
-					echo "WARNING: No GLWU forecast was available for ${INITDATE}${HH}f${lead}" > mailmsg
+					export subject="GLWU Forecast Data Missing for EVS ${COMPONENT}"
+					echo "WARNING: No GLWU forecast was available for ${INITDATE}${HH}" > mailmsg
 					echo "Missing file is glwu.${INITDATE}/${filename}" >> mailmsg
 					echo "Job ID: $jobid" >> mailmsg
 					cat mailmsg | mail -s "$subject" $MAILTO
@@ -55,18 +57,19 @@ for mtype in glwu glwu_lc grlc_2p5km grlc_2p5km_lc grlc_2p5km_lc_sr grlc_2p5km_s
 				cp -v $COMINfilename $DATAfilename
 			fi
 			if [ -s $DATAfilename ]; then
-				for lead in ${leads} ; do		
-					if [ $lead = 0 ]; then
+				for fcst in ${fcsts} ; do
+					FCST=$(printf "%03d" "$fcst")		
+					if [ ${fcst} = 0 ]; then
 						grib2_match=":surface:anl:"
 					else
 						grib2_match=":${fcst} hour fcst:"
 					fi
-					DATAfilename_fcst="${DATA}/gribs/${MODELNAME}.${INITDATE}.${filename}.t${HH}z.f${fcst}.grib2"
-					ARCmodelfilename="${ARCmodel}/${MODELNAME}.${INITDATE}.${filename}.t${HH}z.f${fcst}.grib2"
+					DATAfilename_fcst="${DATA}/gribs/${MODELNAME}.${mtype}.${INITDATE}.t${HH}z.f${FCST}.grib2"
+					ARCmodelfilename="${ARCmodel}/${MODELNAME}.${mtype}.${INITDATE}.t${HH}z.f${FCST}.grib2"
 					wgrib2 $DATAfilename -match "$grib2_match" -grib $DATAfilename_fcst > /dev/null
 					export err=$?; err_chk
 					if [ $SENDCOM = YES ]; then
-						cp -v $DATAfilename_fcst ${ARCmodelfilename}/.
+						cp -v $DATAfilename_fcst ${ARCmodelfilename}
 					fi
 				done
 			fi
@@ -79,7 +82,7 @@ done
 ############################################################
 
 export RUN=ndbc
-mkdir -p $COMOUTprep/glwu.${INITDATE}/$RUN
+#mkdir -p $COMOUTprep/glwu.${INITDATE}/$RUN
 mkdir -p ${DATA}/ncfiles
 export MET_NDBC_STATIONS=${FIXevs}/ndbc_stations/ndbc_stations.xml
 ndbc_txt_ncount=$(ls -l $DCOMINndbc/$VDATE/validation_data/marine/buoy/*.txt |wc -l)
