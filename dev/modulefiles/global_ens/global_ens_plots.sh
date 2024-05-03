513a1ce01dd920e9dc2d7711fe950dc5f3a770a7
PerryShafran-NOAA <62255233+PerryShafran-NOAA@users.noreply.github.com>
2024-05-03 09:36:06 -0400

    Updating run.ver and modulefiles for met/metplus beta3 versions (#468)
    * Upgrading run.ver and modulefiles to run with met/metplus betas
    
    * Update JEVS_SUBSEASONAL_STATS - removing additional space
    
    * Updating seasonal modulefiles for beta3 versions of MET/METplus.
    
    ---------
    
    Co-authored-by: Shelley Melchior <Shelley.Melchior@noaa.gov>

--- dev/modulefiles/global_ens/global_ens_plots.sh
+++ dev/modulefiles/global_ens/global_ens_plots.sh
@@ -1,4 +1,7 @@
 set -x
+module use /apps/ops/para/libs/modulefiles/compiler/intel/${intel_ver}
+export HPC_OPT=/apps/ops/para/libs
+module use /apps/dev/modulefiles
 module load PrgEnv-intel/${PrgEnvintel_ver}
 module load intel/${intel_ver}
 module load ve/evs/${ve_evs_ver}
