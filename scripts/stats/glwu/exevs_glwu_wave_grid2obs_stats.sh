d56feb362ee442d6048e03100747928e82a6643b
Samira Ardani <samira.ardani@noaa.gov>
2024-05-07 20:45:04 +0000

    stats script and parm were modified.

--- scripts/stats/glwu/exevs_glwu_wave_grid2obs_stats.sh
+++ scripts/stats/glwu/exevs_glwu_wave_grid2obs_stats.sh
@@ -40,7 +40,7 @@ vhours='01 07 13 19'
 lead_hours='0 6 12 18 24 30 36 42 48 54 60 66 72 78
             84 90 96 102 108 114 120 126 132 138 144'
 
-export GRID2OBS_CONF="${PARMevs}/metplus_config/${COMPONENT}/${RUN}_${VERIF_CASE}/${STEP}"
+export GRID2OBS_CONF="${PARMevs}/metplus_config/${STEP}/${COMPONENT}/${RUN}_${VERIF_CASE}"
 
 cd ${DATA}
 
