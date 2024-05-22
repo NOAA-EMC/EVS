d1f1f1266d41cb0cc91e6f85e39325f1e62e2820
Samira Ardani <samira.ardani@noaa.gov>
2024-05-22 22:20:17 +0000

    Added more edits to grid2obs stats script.

--- scripts/stats/rtofs/exevs_rtofs_grid2obs_stats.sh
+++ scripts/stats/rtofs/exevs_rtofs_grid2obs_stats.sh
@@ -15,9 +15,6 @@ mkdir -p $STATSDIR
 # PointStat for Argo:
 ########################################
 
-export VARS="temp psal"
-export RUNupper=$(echo $RUN | tr '[a-z]' '[A-Z]')
-
 # get the months for the climo files:
 #     for day < 15, use the month before + valid month
 #     for day >= 15, use valid month + the month after
@@ -41,50 +38,48 @@ else
    export EM=$NM
 fi
 
-# run Point_Stat
-for levl in 0 50 125 200 400 700 1000 1400; do
-  export LVL=$levl
+if [ $RUN = argo ]; then
+	export VARS="temp psal"
+	export RUNupper=$(echo $RUN | tr '[a-z]' '[A-Z]')
+	EVSINfilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/argo.$VDATE.nc
+	COMINicefilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/rtofs_glo_2ds_f000_ice.$RUN.nc
+	
+	for levl in 0 50 125 200 400 700 1000 1400; do
+		export LVL=$levl
 
-  if [ $levl -eq 0 ] ; then
-    export ZRANGE=0-4
-  fi
+  		if [ $levl -eq 0 ] ; then
+    			export ZRANGE=0-4
+  		fi
 
-  if [ $levl -eq 50 ] ; then
-    export ZRANGE=48-52
-  fi
+		if [ $levl -eq 50 ] ; then
+    			export ZRANGE=48-52
+  		fi
 
-  if [ $levl -eq 125 ] ; then
-    export ZRANGE=123-127
-  fi
+  		if [ $levl -eq 125 ] ; then
+    			export ZRANGE=123-127
+  		fi
 
-  if [ $levl -eq 200 ] ; then
-    export ZRANGE=198-202
-  fi
+  		if [ $levl -eq 200 ] ; then
+    			export ZRANGE=198-202
+  		fi
 
-  if [ $levl -eq 400 ] ; then
-    export ZRANGE=398-402
-  fi
+  		if [ $levl -eq 400 ] ; then
+    			export ZRANGE=398-402
+  		fi
 
-  if [ $levl -eq 700 ] ; then
-    export ZRANGE=698-702
-  fi
+		if [ $levl -eq 700 ] ; then
+    			export ZRANGE=698-702
+ 		fi
 
-  if [ $levl -eq 1000 ] ; then
-    export ZRANGE=997-1003
-  fi
+  		if [ $levl -eq 1000 ] ; then
+    			export ZRANGE=997-1003
+  		fi
 
-  if [ $levl -eq 1400 ] ; then
-    export ZRANGE=1397-1403
-  fi
-
-for Rcase in argo ndbc; do
-      	if [ $Rcase = argo ]; then
-		export VARS="temp psal"
-		export RUNupper=$(echo $Rcase | tr '[a-z]' '[A-Z]')
-		export RUN=${Rcase}
-		EVSINfilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/argo.$VDATE.nc
-		COMINicefilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/rtofs_glo_2ds_f000_ice.$RUN.nc
-  		if [ -s $EVSINargofilename ] ; then
+  		if [ $levl -eq 1400 ] ; then
+    			export ZRANGE=1397-1403
+  		fi
+  
+  		if [ -s $EVSINfilename ] ; then
     			if [ -s $COMINicefilename ] ; then
       				for fday in 0 1 2 3 4 5 6 7 8; do
         				fhr=$(($fday * 24))
@@ -93,25 +88,25 @@ for Rcase in argo ndbc; do
 					match_date=$(date --date="${VDATE} ${fhr} hours ago" +"%Y%m%d")
 					COMINrtofsfilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/rtofs_glo_3dz_f${fhr3}_daily_3ztio.$RUN.nc
 					if [ -s $COMINrtofsfilename ] ; then
-          				for vari in ${VARS}; do
-            					export VAR=$vari
-            					mkdir -p $STATSDIR/$RUN.$VDATE/$VAR
-            					if [ -s $COMOUTsmall/$VAR/point_stat_RTOFS_${RUNupper}_${VAR}_Z${levl}_${fhr2}0000L_${VDATE}_000000V.stat ]; then
-              						cp -v $COMOUTsmall/$VAR/point_stat_RTOFS_${RUNupper}_${VAR}_Z${levl}_${fhr2}0000L_${VDATE}_000000V.stat $STATSDIR/$RUN.$VDATE/$VAR/.
-           					else
-              						run_metplus.py -c ${PARMevs}/metplus_config/machine.conf \
-              						-c $CONFIGevs/$STEP/$COMPONENT/${VERIF_CASE}/PointStat_fcstRTOFS_obs${RUNupper}_climoWOA23_$VAR.conf
-              						export err=$?; err_chk
-              						if [ $SENDCOM = "YES" ]; then
-                  						mkdir -p $COMOUTsmall/$VAR
-		  						if [ -s $STATSDIR/$RUN.$VDATE/$VAR/point_stat_RTOFS_${RUNupper}_${VAR}_Z${levl}_${fhr2}0000L_${VDATE}_000000V.stat ] ; then
-                  							cp -v $STATSDIR/$RUN.$VDATE/$VAR/point_stat_RTOFS_${RUNupper}_${VAR}_Z${levl}_${fhr2}0000L_${VDATE}_000000V.stat $COMOUTsmall/$VAR/.
+          					for vari in ${VARS}; do
+            						export VAR=$vari
+            						mkdir -p $STATSDIR/$RUN.$VDATE/$VAR
+            						if [ -s $COMOUTsmall/$VAR/point_stat_RTOFS_${RUNupper}_${VAR}_Z${levl}_${fhr2}0000L_${VDATE}_000000V.stat ]; then
+              							cp -v $COMOUTsmall/$VAR/point_stat_RTOFS_${RUNupper}_${VAR}_Z${levl}_${fhr2}0000L_${VDATE}_000000V.stat $STATSDIR/$RUN.$VDATE/$VAR/.
+           						else
+              							run_metplus.py -c ${PARMevs}/metplus_config/machine.conf \
+              							-c $CONFIGevs/$STEP/$COMPONENT/${VERIF_CASE}/PointStat_fcstRTOFS_obs${RUNupper}_climoWOA23_$VAR.conf
+              							export err=$?; err_chk
+              							if [ $SENDCOM = "YES" ]; then
+                  							mkdir -p $COMOUTsmall/$VAR
+		  							if [ -s $STATSDIR/$RUN.$VDATE/$VAR/point_stat_RTOFS_${RUNupper}_${VAR}_Z${levl}_${fhr2}0000L_${VDATE}_000000V.stat ] ; then
+                  								cp -v $STATSDIR/$RUN.$VDATE/$VAR/point_stat_RTOFS_${RUNupper}_${VAR}_Z${levl}_${fhr2}0000L_${VDATE}_000000V.stat $COMOUTsmall/$VAR/.
+									fi
 								fi
 							fi
-						fi
-					done
-				else
-					echo "WARNING: Missing RTOFS f${fhr3} 3dz file for $VDATE: ${COMINrtofsfilename}"
+						done
+					else
+						echo "WARNING: Missing RTOFS f${fhr3} 3dz file for $VDATE: ${COMINrtofsfilename}"
 					fi
 				done
 			else
@@ -120,50 +115,51 @@ for Rcase in argo ndbc; do
 		else
 			echo "WARNING: Missing ARGO data file for $VDATE: $EVSINfilename"
 		fi
-	elif [ Rcase=ndbc ]; then
-		export VARS="sst"
-		export RUNupper="NDBC_STANDARD"
-		EVSINfilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/ndbc.${VDATE}.nc
-		COMINicefilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/rtofs_glo_2ds_f000_ice.$RUN.nc
-		if [ -s $EVSINfilename ] ; then
-			   if [ -s $COMINicefilename ] ; then
-				   for fday in 0 1 2 3 4 5 6 7 8; do
-					   fhr=$(($fday * 24))
-					   fhr2=$(printf "%02d" "${fhr}")
-					   export fhr3=$(printf "%03d" "${fhr}")
-					   match_date=$(date --date="${VDATE} ${fhr} hours ago" +"%Y%m%d")
-					   COMINrtofsfilename=$COMIN/prep/$COMPONENT/rtofs.${match_date}/$RUN/rtofs_glo_2ds_f${fhr3}_prog.$RUN.nc
-					   if [ -s $COMINrtofsfilename ] ; then
-						   for vari in ${VARS}; do
-							   export VAR=$vari
-							   export VARupper=$(echo $VAR | tr '[a-z]' '[A-Z]')
-							   mkdir -p $STATSDIR/$RUN.$VDATE/$VAR
-							   if [ -s $COMOUTsmall/$VAR/point_stat_RTOFS_NDBC_${VARupper}_${fhr2}0000L_${VDATE}_000000V.stat ]; then
-								   cp -v $COMOUTsmall/$VAR/point_stat_RTOFS_NDBC_${VARupper}_${fhr2}0000L_${VDATE}_000000V.stat $STATSDIR/$RUN.$VDATE/$VAR/.
-							   else
-								   run_metplus.py -c ${PARMevs}/metplus_config/machine.conf \
-								   -c $CONFIGevs/$STEP/$COMPONENT/${VERIF_CASE}/PointStat_fcstRTOFS_obsNDBC_climoWOA23.conf
-								   export err=$?; err_chk
-								   if [ $SENDCOM = "YES" ]; then
-									   mkdir -p $COMOUTsmall/$VAR
-									   if [ -s $STATSDIR/$RUN.$VDATE/$VAR/point_stat_RTOFS_NDBC_${VARupper}_${fhr2}0000L_${VDATE}_000000V.stat ] ; then
-										   cp -v $STATSDIR/$RUN.$VDATE/$VAR/point_stat_RTOFS_NDBC_${VARupper}_${fhr2}0000L_${VDATE}_000000V.stat $COMOUTsmall/$VAR/.
-									   fi
-								   fi
-							   fi
-						   done
-					   else
-						   echo "WARNING: Missing RTOFS f${fhr3} prog file for $VDATE: $COMINrtofsfilename"
-					   fi
-				   done
-			   else
-				   echo "WARNING: Missing RTOFS f000 ice file for $VDATE: $COMINicefilename"
-			   fi
-		   else
-			   echo "WARNING: Missing NDBC data file for $VDATE: $EVSINfilename"
+	done
+
+elif [ $RUN = ndbc ]; then
+	export VARS="sst"
+	export RUNupper="NDBC_STANDARD"
+	EVSINfilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/ndbc.${VDATE}.nc
+	COMINicefilename=$COMIN/prep/$COMPONENT/rtofs.$VDATE/$RUN/rtofs_glo_2ds_f000_ice.$RUN.nc
+	if [ -s $EVSINfilename ] ; then
+		if [ -s $COMINicefilename ] ; then
+			for fday in 0 1 2 3 4 5 6 7 8; do
+				fhr=$(($fday * 24))
+				fhr2=$(printf "%02d" "${fhr}")
+				export fhr3=$(printf "%03d" "${fhr}")
+				match_date=$(date --date="${VDATE} ${fhr} hours ago" +"%Y%m%d")
+				COMINrtofsfilename=$COMIN/prep/$COMPONENT/rtofs.${match_date}/$RUN/rtofs_glo_2ds_f${fhr3}_prog.$RUN.nc
+				if [ -s $COMINrtofsfilename ] ; then
+					for vari in ${VARS}; do
+						export VAR=$vari
+						export VARupper=$(echo $VAR | tr '[a-z]' '[A-Z]')
+						mkdir -p $STATSDIR/$RUN.$VDATE/$VAR
+						if [ -s $COMOUTsmall/$VAR/point_stat_RTOFS_NDBC_${VARupper}_${fhr2}0000L_${VDATE}_000000V.stat ]; then
+							cp -v $COMOUTsmall/$VAR/point_stat_RTOFS_NDBC_${VARupper}_${fhr2}0000L_${VDATE}_000000V.stat $STATSDIR/$RUN.$VDATE/$VAR/.
+						else
+							run_metplus.py -c ${PARMevs}/metplus_config/machine.conf \
+							-c $CONFIGevs/$STEP/$COMPONENT/${VERIF_CASE}/PointStat_fcstRTOFS_obsNDBC_climoWOA23.conf
+							export err=$?; err_chk
+							if [ $SENDCOM = "YES" ]; then
+								mkdir -p $COMOUTsmall/$VAR
+								if [ -s $STATSDIR/$RUN.$VDATE/$VAR/point_stat_RTOFS_NDBC_${VARupper}_${fhr2}0000L_${VDATE}_000000V.stat ] ; then
+									cp -v $STATSDIR/$RUN.$VDATE/$VAR/point_stat_RTOFS_NDBC_${VARupper}_${fhr2}0000L_${VDATE}_000000V.stat $COMOUTsmall/$VAR/.
+								fi
+							fi
+						fi
+					done
+				else
+					echo "WARNING: Missing RTOFS f${fhr3} prog file for $VDATE: $COMINrtofsfilename"
+				fi
+			done
+		else
+			echo "WARNING: Missing RTOFS f000 ice file for $VDATE: $COMINicefilename"
 		fi
+	else
+		echo "WARNING: Missing NDBC data file for $VDATE: $EVSINfilename"
 	fi
-done
+fi
 
 ######################################
 # check if stat files exist
