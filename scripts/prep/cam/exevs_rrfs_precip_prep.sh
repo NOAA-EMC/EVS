b8081712e4f90704cc0ecad29baa4687eac99ada
marcel caron <marcel.caron@clogin08.cactus.wcoss2.ncep.noaa.gov>
2024-06-12 17:55:35 +0000

    Add EVS scripts for rrfs prep jobs

---
+++ scripts/prep/cam/exevs_rrfs_precip_prep.sh
@@ -0,0 +1,94 @@
+#!/bin/bash -e
+
+# =============================================================================
+#
+# NAME: exevs_rrfs_precip_prep.sh
+# CONTRIBUTOR(S): Marcel Caron, marcel.caron@noaa.gov, NOAA/NWS/NCEP/EMC-VPPPGB
+# PURPOSE: Handle all components of an EVS RRFS Precipitation - Prep job
+# DEPENDENCIES: $HOMEevs/jobs/JEVS_CAM_PREP 
+#
+# =============================================================================
+
+set -x
+
+# Loop through RRFS Precipitation configs
+export machine=${machine:-"WCOSS2"}
+export PYTHONPATH=$USHevs/$COMPONENT:$PYTHONPATH
+export njob=1
+for NEST in "conus" "ak"; do
+    export NEST=$NEST
+    for ACC in "24"; do
+        export ACC=$ACC
+        source $config
+ 
+        # Check User's Configuration Settings
+        python $USHevs/cam/cam_check_settings.py
+        export err=$?; err_chk
+ 
+        # Create Output Directories
+        python $USHevs/cam/cam_create_output_dirs.py
+        export err=$?; err_chk
+ 
+        # Create Job Script 
+        python $USHevs/cam/cam_prep_precip_create_job_script.py
+        export err=$?; err_chk
+        export njob=$((njob+1))
+    done
+done
+
+# Create POE Job Scripts
+if [ $USE_CFP = YES ]; then
+    python $USHevs/cam/cam_prep_precip_create_poe_job_scripts.py
+    export err=$?; err_chk
+fi
+
+# Run all RRFS precip/prep jobs
+chmod u+x ${DATA}/${VERIF_CASE}/${STEP}/prep_job_scripts/*
+ncount_job=$(ls -l ${DATA}/${VERIF_CASE}/${STEP}/prep_job_scripts/job* |wc -l)
+nc=1
+if [ $USE_CFP = YES ]; then
+    ncount_poe=$(ls -l ${DATA}/${VERIF_CASE}/${STEP}/prep_job_scripts/poe* |wc -l)
+    while [ $nc -le $ncount_poe ]; do
+        poe_script=${DATA}/${VERIF_CASE}/${STEP}/prep_job_scripts/poe_jobs${nc}
+        chmod 775 $poe_script
+        export MP_PGMMODEL=mpmd
+        export MP_CMDFILE=${poe_script}
+        if [ $machine = WCOSS2 ]; then
+            launcher="mpiexec -np $nproc -ppn $nproc --cpu-bind verbose,depth cfp"
+        elif [$machine = HERA -o $machine = ORION -o $machine = S4 -o $machine = JET ]; then
+            export SLURM_KILL_BAD_EXIT=0
+            launcher="srun --export=ALL --multi-prog"
+        else
+            err_exit "Cannot submit jobs to scheduler on this machine.  Set USE_CFP=NO and retry."
+        fi
+        $launcher $MP_CMDFILE
+        nc=$((nc+1))
+    done
+else
+    while [ $nc -le $ncount_job ]; do
+        sh +x ${DATA}/${VERIF_CASE}/${STEP}/prep_job_scripts/job${nc}
+        export err=$?; err_chk
+        nc=$((nc+1))
+    done
+fi
+
+for NEST in "conus" "ak"; do
+    export NEST=$NEST
+    source $config
+    # Copy files to desired location
+    #all commands to copy output files into the correct EVS COMOUT directory
+    if [ $SENDCOM = YES ]; then
+        for OBS_DIR_PATH in $DATA/$VERIF_CASE/data/$VERIF_TYPE/*; do
+            OBS_DIR=$(echo ${OBS_DIR_PATH##*/})
+            mkdir -p $COMOUT/$OBS_DIR
+            if [ ! -z "$(ls -A $OBS_DIR_PATH)" ]; then
+                for FILE in $OBS_DIR_PATH/*; do
+                    if [ -s "$FILE" ]; then
+                        cp -v $FILE $COMOUT/$OBS_DIR/.
+                    fi
+                done
+            fi
+        done
+    fi
+
+done
---
+++ scripts/prep/cam/exevs_rrfs_severe_prep.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+###############################################################################
+# Name of Script: exevs_rrfs_severe_prep.sh
+# Contact(s):     Logan C. Dawson (logan.dawson@noaa.gov)
+# Purpose of Script: This script preprocesses RRFS control UH data for 
+#                    CAM severe verification.
+# History Log:
+# 3/2023: Initial script assembled by Logan Dawson 
+###############################################################################
+
+
+set +x
+
+echo 
+echo " ENTERING SUB SCRIPT $0 "
+echo
+
+set -x
+
+
+############################################################
+# Define surrogate severe settings
+############################################################
+
+export machine=${machine:-"WCOSS2"}
+export VERIF_GRID=G211
+export VERIF_GRID_DX=81.271
+export GAUSS_RAD=120
+
+
+############################################################
+# Set some model-specific environment variables 
+############################################################
+
+export MODEL_INPUT_DIR=${COMINrrfs}
+
+export MXUPHL25_THRESH1=160.0
+
+if [ $vhr -eq 00 ];then
+   nloop=2
+   fhr_beg1=12
+   fhr_end1=36
+   fhr_beg2=36
+   fhr_end2=60
+
+elif [ $vhr -eq 06 ]; then
+   nloop=2
+   fhr_beg1=6
+   fhr_end1=30
+   fhr_beg2=30
+   fhr_end2=54
+
+elif [ $vhr -eq 12 ]; then
+   nloop=2
+   fhr_beg1=0
+   fhr_end1=24
+   fhr_beg2=24
+   fhr_end2=48
+
+elif [ $vhr -eq 18 ]; then
+   nloop=1
+   fhr_beg1=18
+   fhr_end1=42
+
+else
+   err_exit "Current cyc is unsupported"
+
+fi
+
+export nloop
+export fhr_beg1
+export fhr_end1
+export fhr_beg2
+export fhr_end2
+
+
+############################################################
+# Write poescript for each domain and use case
+############################################################
+
+njob=0
+
+# Create output directory for GridStat (and EnsembleStat) runs
+mkdir -p $DATA/gen_ens_prod
+mkdir -p $DATA/pcp_combine
+mkdir -p $DATA/sspf
+
+MEMNUM="ctl"
+echo "${USHevs}/${COMPONENT}/evs_rrfs_severe_prep.sh $MEMNUM $njob" >> $DATA/poescript
+njob=$((njob+1))
+
+
+for member in {1..5}; do
+   MEMNUM="mem000${member}"
+   echo "${USHevs}/${COMPONENT}/evs_rrfs_severe_prep.sh $MEMNUM $njob" >> $DATA/poescript
+   njob=$((njob+1))
+done
+
+
+###################################################################
+# Run the command file
+###################################################################
+
+chmod 775 $DATA/poescript
+
+export MP_PGMMODEL=mpmd
+export MP_CMDFILE=${DATA}/poescript
+
+if [ $USE_CFP = YES ]; then
+
+   echo "running cfp"
+   mpiexec -np $nproc --cpu-bind verbose,core cfp ${MP_CMDFILE}
+   export err=$?; err_chk
+   echo "done running cfp"
+
+else
+
+   echo "not running cfp"
+   ${MP_CMDFILE}
+   export err=$?; err_chk
+
+fi
+
+
+###################################################################
+# Copy hourly output to $COMOUT
+###################################################################
+
+output_dirs="pcp_combine sspf"
+
+if [ $SENDCOM = YES ]; then
+   mkdir -p $COMOUT/${modsys}.${IDATE}
+   for output_dir in ${output_dirs}; do
+      if [ "$(ls -A $DATA/$output_dir)" ]; then
+         for FILE in $DATA/${output_dir}/${modsys}.${IDATE}/*; do
+            if [ -s "$FILE" ]; then
+               cp -v $FILE $COMOUT/${modsys}.${IDATE}
+            fi
+         done
+      fi
+   done
+fi
+
+# Cat the METplus log files
+log_dir=$DATA/logs
+if [ -d $log_dir ]; then
+    log_file_count=$(find $log_dir -type f | wc -l)
+    if [[ $log_file_count -ne 0 ]]; then
+        log_files=("$log_dir"/*)
+        for log_file in "${log_files[@]}"; do
+            if [ -f "$log_file" ]; then
+                echo "Start: $log_file"
+                cat "$log_file"
+                echo "End: $log_file"
+            fi
+        done
+    fi
+fi
+
+exit
