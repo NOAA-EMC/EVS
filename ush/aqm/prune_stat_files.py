3d191383c0198f77c0771fb41795dd15487c14b2
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-06-26 14:28:08 +0000

    vpppg overall v2.0 request changes #1

--- ush/aqm/prune_stat_files.py
+++ ush/aqm/prune_stat_files.py
@@ -100,7 +100,7 @@ def prune_data(data_dir, prune_dir, tmp_dir, output_base_template, valid_range,
          )
       # Prune the MET .stat files and write to new file
       for met_stat_file in met_stat_files:
-         ps = subprocess.Popen('grep -R "'+model+'" '+met_stat_file+filter_cmd,
+         ps = subprocess.run('grep -R "'+model+'" '+met_stat_file+filter_cmd,
                                shell=True, stdout=subprocess.PIPE,
                                stderr=subprocess.STDOUT, encoding='UTF-8')
          grep_output = ps.communicate()[0]
