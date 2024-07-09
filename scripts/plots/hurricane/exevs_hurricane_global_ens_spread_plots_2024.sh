a02f9d4f25ccfe45ea947b150560529cf3d1b01c
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2024-07-09 13:53:45 +0000

    corrected filenames, successful run for AL022024 spread

--- scripts/plots/hurricane/exevs_hurricane_global_ens_spread_plots_2024.sh
+++ scripts/plots/hurricane/exevs_hurricane_global_ens_spread_plots_2024.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -x
-export PS4=' + exevs_hurricane_global_ens_spread_plots.sh.2024 line $LINENO: '
+export PS4=' + exevs_hurricane_global_ens_spread_plots_2024.sh line $LINENO: '
 
 export stormYear=${YYYY}
-export basinlist="al ep wp"
-export numlist="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 \
-	        21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"
+export basinlist="al"
+#export basinlist="al ep wp"
+export numlist="02"
+#export numlist="01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 \
+#	        21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40"
 
 for bas in $basinlist; do
 ### bas do loop start
