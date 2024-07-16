131138b90a9fc7badc806aa18d7b1c9af05f50fc
Samira Ardani <samira.ardani@noaa.gov>
2024-07-16 19:34:56 +0000

    Updated the code to use gen_vx_mask for ice masking.

--- ush/glwu/glwu_prep_regions.sh
+++ ush/glwu/glwu_prep_regions.sh
@@ -9,62 +9,80 @@
 set -x
 
 # create ice mask: ice grids are defined as grids with ice coverage >= 0.15
-#     from RTOFS nowcast (i.e., f000 forecast)
+#     from GLWU nowcast (i.e., f000 forecast)
 # use non-ice grids to calculate stats
 
 # Lake Champlain:
-gen_vx_mask \
-$COMINglwu/glwu.$VDATE/$RUN/glwu.glwu_lc.t00z.nc \
-$COMINglwu/glwu.$VDATE/$RUN/glwu.glwu_lc.t00z.nc \
-$DATA/glwu.$VDATE/$RUN/ice_mask.nc \
--type data -mask_field 'name="ice_coverage"; level="(0,*,*)";' -thresh lt0.15 -name ice_mask_lc
-
-if [ $SENDCOM = "YES" ]; then
- cp $DATA/glwu.$VDATE/$RUN/ice_mask_lc.nc $COMINglwu/glwu.$VDATE/$RUN
+if [ -s $COMINglwu/glwu.$VDATE/glwu.glwu_lc.t00z.nc ]; then
+	gen_vx_mask \
+	$COMINglwu/glwu.$VDATE/glwu.glwu_lc.t00z.nc \
+	$COMINglwu/glwu.$VDATE/glwu.glwu_lc.t00z.nc \
+	$DATA/masks/ice_mask.nc \
+	-type data -mask_field 'name="ice_coverage"; level="(0,*,*)";' -thresh lt0.15 -name ice_mask_lc
+	export err=$?; err_chk
+	if [ $SENDCOM = "YES" ]; then
+		if [ -s $DATA/masks/ice_mask_lc.nc ]; then
+			cp $DATA/masks/ice_mask_lc.nc $ARCmodel/
+		fi
+	fi
 fi
-
 # Other lakes:
-gen_vx_mask \
-$COMINglwu/glwu.$VDATE/$RUN/glwu.glwu.t00z.nc \
-$COMINglwu/glwu.$VDATE/$RUN/glwu.glwu.t00z.nc \
-$DATA/glwu.$VDATE/$RUN/ice_mask.nc \
--type data -mask_field 'name="ice_coverage"; level="(0,*,*)";' -thresh lt0.15 -name ice_mask
 
-if [ $SENDCOM = "YES" ]; then
- cp $DATA/glwu.$VDATE/$RUN/ice_mask.nc $COMINglwu/glwu.$VDATE/$RUN
+if [ -s $COMINglwu/glwu.$VDATE/glwu.glwu.t00z.nc ]; then
+	gen_vx_mask \
+	$COMINglwu/glwu.$VDATE/glwu.glwu.t00z.nc \
+	$COMINglwu/glwu.$VDATE/glwu.glwu.t00z.nc \
+	$DATA/masks/ice_mask.nc \
+	-type data -mask_field 'name="ice_coverage"; level="(0,*,*)";' -thresh lt0.15 -name ice_mask
+	export err=$?; err_chk
+	if [ $SENDCOM = "YES" ]; then
+		if [ -s $DATA/masks/ice_mask.nc ]; then
+			cp $DATA/masks/ice_mask.nc $ARCmodel/
+		fi
+	fi
 fi
-
 
 # Lake Champlain:
-gen_vx_mask \
+#gen_vx_mask \
 # create subregions using ice mask
 #   Other lakes
-gen_vx_mask \
-$DATA/glwu.$VDATE/$RUN/ice_mask_lc.nc \
-$DATA/glwu.$VDATE/$RUN/ice_mask.nc \
-$DATA/glwu.$VDATE/$RUN/mask.nc \
--type lat -thresh 'ge-80 && le90' -intersection -name LC
+#gen_vx_mask \
+#$DATA/masks/ice_mask_lc.nc \
+#$DATA/masks/ice_mask.nc \
+#$DATA/masks/mask.nc \
+#-type lat -thresh 'ge-80 && le90' -intersection -name LC
 
-if [ $SENDCOM = "YES" ]; then
- cp $DATA/glwu.$VDATE/$RUN/mask.nc $COMINglwu/glwu.$VDATE/$RUN
-fi
+#if [ $SENDCOM = "YES" ]; then
+# cp $DATA/masks/mask.nc $ARCmodel
+#fi
 
 #   Other lakes:
-gen_vx_mask \
-$DATA/glwu.$VDATE/$RUN/ice_mask.nc \
-$DATA/glwu.$VDATE/$RUN/ice_mask.nc \
-$DATA/glwu.$VDATE/$RUN/greatlakes.lat.nc \
--type lat -thresh 'ge0 && le60' -intersection
-
-if [ $SENDCOM = "YES" ]; then
- cp $DATA/glwu.$VDATE/$RUN/greatlakes.lat.nc $COMINglwu/glwu.$VDATE/$RUN
+if [ -s $ARCmodel/ice_mask.nc ]; then
+	gen_vx_mask \
+	$DATA/masks/ice_mask.nc \
+	$DATA/masks/ice_mask.nc \
+	$DATA/masks/greatlakes.lat.nc \
+	-type lat -thresh 'ge40.8 && le49' -intersection
+	export err=$?; err_chk
+	if [ $SENDCOM = "YES" ]; then
+		if [ -s $DATA/masks/greatlakes.lat.nc ]; then
+ 			cp $DATA/masks/greatlakes.lat.nc $ARCmodel
+		fi
+	fi
 fi
+if [ -s $ARCmodel/greatlakes.lat.nc ]; then
+	gen_vx_mask \
+	$DATA/masks/greatlakes.lat.nc \
+	$DATA/masks/greatlakes.lat.nc \
+	$DATA/masks/mask.greatlakes.nc \
+	-type lon -thresh 'ge-93 && le-75' -intersection -name GL
+	export err=$?; err_chk
 
-gen_vx_mask \
-$DATA/rtofs.$VDATE/$RUN/greatlakes.lat.nc \
-$DATA/rtofs.$VDATE/$RUN/greatlakes.lat.nc \
-$DATA/rtofs.$VDATE/$RUN/mask.greatlakes.nc \
--type lon -thresh 'ge-98 && le10' -intersection -name GL
-
+	if [ $SENDCOM = "YES" ]; then
+		if [ -s $DATA/masks/mask.greatlakes.nc ]; then
+			cp $DATA/masks/greatlakes.nc $ARCmodel
+		fi
+	fi
+fi
 
 ################################ END OF SCRIPT ################################
