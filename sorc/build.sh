78d0a58fb425c16f56ed07de3548070e087d5e7b
Shelley Melchior <Shelley.Melchior@noaa.gov>
2024-07-22 19:45:47 +0000

    Updates to test the para installation of w3emc updates on cactus

--- sorc/build.sh
+++ sorc/build.sh
@@ -23,13 +23,11 @@ module load bacio/${bacio_ver}
 #module load w3emc/${w3emc_ver}
 module load ip/${ip_ver}
 module load sp/${sp_ver}
-#module load g2/${g2_ver}
+module load g2/${g2_ver}
 module load jasper/${jasper_ver}
 module load libpng/${libpng_ver}
 module load zlib/${zlib_ver}
-#module use /lfs/h2/emc/eib/save/Hang.Lei/forgdit/nco_wcoss2/install/modulefiles/compiler/intel/19.1.3.304
 module use /apps/ops/para/libs/modulefiles/compiler/intel/19.1.3.304 
-module load g2/${g2_ver}
 module load w3emc/${w3emc_ver}
 module list
 set -x
