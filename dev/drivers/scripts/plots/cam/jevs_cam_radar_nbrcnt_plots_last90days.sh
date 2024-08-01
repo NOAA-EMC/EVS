eed69f7595711c47b27664dcdb65374dcfafc9f2
marcel caron <marcel.caron@clogin06.cactus.wcoss2.ncep.noaa.gov>
2024-08-01 19:08:56 +0000

    Separate plots jobs by EVAL_PERIOD and LINE_TYPE

--- dev/drivers/scripts/plots/cam/jevs_cam_radar_nbrcnt_plots_last90days.sh
+++ dev/drivers/scripts/plots/cam/jevs_cam_radar_nbrcnt_plots_last90days.sh
@@ -1,9 +1,9 @@
-#PBS -N jevs_cam_severe_plots_00
+#PBS -N jevs_cam_radar_nbrcnt_plots_last90days_00
 #PBS -j oe
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=0:45:00
+#PBS -l walltime=1:50:00
 #PBS -l place=vscatter:exclhost,select=1:ncpus=64
 #PBS -l debug=true
 #PBS -V
@@ -40,7 +40,7 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
 export KEEPDATA=YES
-export VERIF_CASE=severe
+export VERIF_CASE=radar
 export MODELNAME=${COMPONENT}
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${LINE_TYPE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
@@ -50,8 +50,8 @@ export nproc=64
 ############################################################
 
 export vhr=${vhr:-${vhr}}
-export EVAL_PERIOD=${EVAL_PERIOD:-${EVAL_PERIOD}}
-export LINE_TYPE=${LINE_TYPE:-${LINE_TYPE}}
+export EVAL_PERIOD=${EVAL_PERIOD:-last90days}
+export LINE_TYPE=${LINE_TYPE:-nbrcnt}
 
 export SENDMAIL=${SENDMAIL:-YES}
 export SENDCOM=${SENDCOM:-YES}
@@ -74,7 +74,7 @@ fi
 
 
 ######################################################################
-# Purpose: This job generates severe verification graphics
+# Purpose: This job generates radar verification graphics
 #          for the CAM component (deterministic and ensemble CAMs)
 ######################################################################
 
