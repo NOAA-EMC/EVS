40df1521037d0517773699de9aefd0058f7cd05b
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-08-08 13:10:08 +0000

    PR request changes 7

--- dev/drivers/scripts/prep/global_ens/jevs_global_ens_chem_grid2obs_prep.sh
+++ dev/drivers/scripts/prep/global_ens/jevs_global_ens_chem_grid2obs_prep.sh
@@ -56,7 +56,7 @@ export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/${evs_ver_2d}
 mkdir -p ${COMIN}
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/${evs_ver_2d}
 
-export DATAROOT=/lfs/h2/emc/ptmp/${USER}/${NET}/${evs_ver_2d}/${STEP}
+export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/${envir}/tmp
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${RUN}_${VERIF_CASE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
 
