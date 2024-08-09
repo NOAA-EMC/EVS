75842e5c461d7836815ab39608c8eda60ba0a6d5
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-08-09 15:06:49 +0000

    PR request changes 13

--- ush/global_ens/global_ens_chem_plots_production_tof120.py
+++ ush/global_ens/global_ens_chem_plots_production_tof120.py
@@ -7,7 +7,6 @@ Abstract: This is the driver script for creating plots.
           forecast hour 120.
 Run By: individual plotting job scripts generated through
         ush/global_ens/global_ens_chem_plots_grid2obs_create_job_scripts.py
-        and ush/global_ens/global_ens_chem_plots_grid2grid_create_job_scripts.py
 '''
 
 import os
