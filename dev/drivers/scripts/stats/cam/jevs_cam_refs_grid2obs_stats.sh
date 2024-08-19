d63193b9c5d46ad4a206f7bb35559d181e217805
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2024-08-19 19:53:28 +0000

    Add driver and ecf scripts

--- dev/drivers/scripts/stats/cam/jevs_cam_refs_grid2obs_stats.sh
+++ dev/drivers/scripts/stats/cam/jevs_cam_refs_grid2obs_stats.sh
@@ -1,13 +1,13 @@
-#PBS -N jevs_cam_refs_spcoutlook_stats
+#PBS -N jevs_cam_refs_grid2obs_stats
 #PBS -j oe
 #PBS -q dev
 #PBS -S /bin/bash
 #PBS -A VERF-DEV
-#PBS -l walltime=01:00:00
-#PBS -l place=vscatter,select=1:ncpus=2:mem=100GB
+#PBS -l walltime=04:30:00
+#PBS -l place=vscatter:exclhost,select=1:ncpus=72:mem=500GB
 #PBS -l debug=true
 
-set -x
+set -x 
 
 export OMP_NUM_THREADS=1
 
@@ -18,7 +18,7 @@ export NET=evs
 export STEP=stats
 export COMPONENT=cam
 export RUN=atmos
-export VERIF_CASE=spcoutlook
+export VERIF_CASE=grid2obs
 export MODELNAME=refs
 export KEEPDATA=YES
 export SENDMAIL=YES
@@ -33,23 +33,23 @@ export vhr=00
 export run_mpi=yes
 export gather=yes
 
+
 export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
-export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
+export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
-export EVSINspcotlk=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/evs/v2.0/prep/cam
 export COMINrefs=/lfs/h2/emc/ptmp/emc.lam/para/com/refs/v1.0
+
 
 export MAILTO='alicia.bentley@noaa.gov,binbin.zhou@noaa.gov'
 if [ -z "$MAILTO" ]; then
 
-  echo "MAILTO variable is not defined. Exiting without continuing."
+   echo "MAILTO variable is not defined. Exiting without continuing."
 
 else
 
  ${HOMEevs}/jobs/JEVS_CAM_STATS
 
 fi
-
