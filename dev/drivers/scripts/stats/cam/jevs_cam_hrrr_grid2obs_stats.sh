fa894903dcc2bbf8397fc88c06bbb2c0ac46d712
Mallory Row <mallory.row@noaa.gov>
2024-08-20 11:36:18 +0000

    Update PBS account to EVS-DEV

--- dev/drivers/scripts/stats/cam/jevs_cam_hrrr_grid2obs_stats.sh
+++ dev/drivers/scripts/stats/cam/jevs_cam_hrrr_grid2obs_stats.sh
@@ -4,7 +4,6 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A EVS-DEV
-#PBS -A VERF-DEV
 #PBS -l walltime=02:20:00
 #PBS -l place=vscatter:exclhost,select=1:ncpus=128:ompthreads=1:mem=128GB
 #PBS -l debug=true
