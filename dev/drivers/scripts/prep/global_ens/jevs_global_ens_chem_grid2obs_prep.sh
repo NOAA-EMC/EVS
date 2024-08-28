088259469cff3d7116d6f3a12bf2c7a66509153d
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-08-28 18:00:34 +0000

    PR change #4

--- dev/drivers/scripts/prep/global_ens/jevs_global_ens_chem_grid2obs_prep.sh
+++ dev/drivers/scripts/prep/global_ens/jevs_global_ens_chem_grid2obs_prep.sh
@@ -50,7 +50,7 @@ export MODELNAME=${MODELNAME:-gefs}
 export modsys=${modsys:-gefs}
 export mod_ver=${mod_ver:-${gefs_ver}}
 
-export INITDATE=$(date --date="3 days ago" +%Y%m%d)
+export INITDATE=${INITDATE:-$(date --date="3 days ago" +%Y%m%d)}
 echo "INITDATE=${INITDATE}"
 
 export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/${evs_ver_2d}
