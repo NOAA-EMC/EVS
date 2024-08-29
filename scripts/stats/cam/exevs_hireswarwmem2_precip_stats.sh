9f92f7a2fbdadf7bcc823811542df2c8652c18b3
Marcel Caron <74978213+MarcelCaron-NOAA@users.noreply.github.com>
2024-08-29 07:47:33 -0400

    Bugfix/cam fixes 1 (#529)
    * increase time_offset_warning window
    
    * Remove log file concatenation
    
    * Remove cpreq
    
    * Remove outdated header info (e.g. previous code managers)
    
    * Split grid2obs plots into two jobs
    
    * Split precip plots into two jobs
    
    * Minor adjustments after splitting plots jobs
    
    * Add default EVAL_PERIOD value
    
    * Set KEEPDATA to YES
    
    * Use lowercase version of EVAL_PERIOD to match other settings
    
    * Correct job purpose info
    
    ---------
    
    Co-authored-by: marcel caron <marcel.caron@clogin09.cactus.wcoss2.ncep.noaa.gov>
    Co-authored-by: marcel caron <marcel.caron@clogin07.cactus.wcoss2.ncep.noaa.gov>
    Co-authored-by: marcel caron <marcel.caron@clogin05.cactus.wcoss2.ncep.noaa.gov>
    Co-authored-by: marcel caron <marcel.caron@clogin06.cactus.wcoss2.ncep.noaa.gov>

--- scripts/stats/cam/exevs_hireswarwmem2_precip_stats.sh
+++ scripts/stats/cam/exevs_hireswarwmem2_precip_stats.sh
@@ -370,38 +370,3 @@ if [ "$vhr" -ge "$last_cyc" ]; then
         fi
     fi
 fi
-
-# Cat the METplus log files
-log_dirs="$DATA/$VERIF_CASE/METplus_output/*/out/logs"
-for log_dir in $log_dirs; do
-    if [ -d $log_dir ]; then
-        log_file_count=$(find $log_dir -type f | wc -l)
-        if [[ $log_file_count -ne 0 ]]; then
-            log_files=("$log_dir"/*)
-            for log_file in "${log_files[@]}"; do
-                if [ -f "$log_file" ]; then
-                    echo "Start: $log_file"
-                    cat "$log_file"
-                    echo "End: $log_file"
-                fi
-            done
-        fi
-    fi
-done
-log_dir="$DATA/$VERIF_CASE/METplus_output/out/logs"
-if [ -d $log_dir ]; then
-    log_file_count=$(find $log_dir -type f | wc -l)
-    if [[ $log_file_count -ne 0 ]]; then
-        log_files=("$log_dir"/*)
-        for log_file in "${log_files[@]}"; do
-            if [ -f "$log_file" ]; then
-                echo "Start: $log_file"
-                cat "$log_file"
-                echo "End: $log_file"
-            fi
-        done
-    fi
-fi
-
-
-
