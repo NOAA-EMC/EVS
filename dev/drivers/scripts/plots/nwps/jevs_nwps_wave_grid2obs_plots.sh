8d0e01dd94aa65ae71ba751515ac0b073758242c
Samira Ardani <samira.ardani@noaa.gov>
2024-10-15 18:31:01 +0000

    OBTYPE was modified.

--- dev/drivers/scripts/plots/nwps/jevs_nwps_wave_grid2obs_plots.sh
+++ dev/drivers/scripts/plots/nwps/jevs_nwps_wave_grid2obs_plots.sh
@@ -12,7 +12,7 @@ set -x
 export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/EVS
 
 export MODELNAME=nwps
-export OBTYPE=NDBC
+export OBTYPE=NDBC_STANDARD
 export NET=evs_beta5
 export COMPONENT=nwps
 export STEP=plots
