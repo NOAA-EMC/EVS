b8687deeb6666725f13912ee391ba552a2875c2f
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2024-10-17 12:47:06 +0000

    add checking aeronet record columns

--- ush/global_ens/check_aeronet_aod_lev15.py
+++ ush/global_ens/check_aeronet_aod_lev15.py
@@ -2,36 +2,39 @@
 ###############################################################################
 # Name of Script: 
 # Contact(s):     Ho-Chun Huang (ho-chun.huang@noaa.gov)
-# Purpose of Script: Read AERONET AOD file and remove bad records with inconsistent columns
-
+# Purpose of Script: Read AERONET AOD file and remove bad records with
+#                    inconsistent columns number as header
+#
 # History Log:
-#   9/2024: Script copied from METplus repository
 ###############################################################################
 
 import os
 import sys
-## import datetime
 
 print(f'Python Script: {sys.argv[0]}')
-# input file specified on the command line
+# input and output file specified on the command line
 
 if len(sys.argv) < 2:
     script_name = os.path.basename(sys.argv[0])
-    print(f"FATAL ERROR: {script_name} -> Must specify exactly one input file.")
+    print(f"FATAL ERROR: {script_name} -> Must specify input and output files.")
     sys.exit(1)
 
 # Read the input file as the first argument
 input_file = os.path.expandvars(sys.argv[1])
-print(f'Input File: {input_file}')
+print(f'Input Original Aeronet File:  {input_file}')
+
+# Read the Output file as the second argument
+output_file = os.path.expandvars(sys.argv[2])
+print(f'Output screened Aeronet File: {output_file}')
 
 if not os.path.exists(input_file):
-    print(f"Can not fid input file {input_file}")
-    sys.exit(2)
+    print(f"DEBUG :: Can not find input Aeronet file - {input_file}")
+    print(f"DEBUG :: Check the existence of input file before calling {sys.argv[0]}")
+    sys.exit()
 
-output_file=os.path.join(os.environ['DATA'],"post_aeronet_lev15.csv")
-print(output_file)
-wfile=open(output_file,'w')
 rfile=open(input_file, 'r')
+wfile=open(output_file,'w')
+
 count=0
 flag_data=False
 for line in rfile:
@@ -53,4 +56,6 @@ for line in rfile:
         num_var=len(var)
         if num_var == num_hdr:
             wfile.write(line+"\n")
+        else:
+            print(f"DEBUG :: Line {count} has different columns number {num_var} vs standard {num_hdr}")
 wfile.close()
