421273a7bd92b2e4b20cac87670f96157bdcefa2
Mallory Row <mallory.row@noaa.gov>
2024-10-18 14:18:08 +0000

    Add err_chk

--- ush/global_det/global_det_atmos_stats_grid2grid_create_job_scripts.py
+++ ush/global_det/global_det_atmos_stats_grid2grid_create_job_scripts.py
@@ -1170,6 +1170,7 @@ if JOB_GROUP in ['reformat_data', 'assemble_data', 'generate_stats']:
                                     or verif_type_job == 'WindShear':
                                 if job_env_dict['fhr_list'] != '':
                                     job.write(verif_type_job_commands_list[1])
+                                    job.write('export err=$?; err_chk\n')
                     job.close()
                 date_dt = date_dt + datetime.timedelta(hours=valid_date_inc)
 elif JOB_GROUP == 'gather_stats':
