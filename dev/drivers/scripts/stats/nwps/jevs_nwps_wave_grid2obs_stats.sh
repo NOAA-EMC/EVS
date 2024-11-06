8b6f3074337802c937805691d1736b44e91ba109
Mallory Row <mallory.row@noaa.gov>
2024-11-06 15:51:15 +0000

    Change account to EVS-DEV

--- dev/drivers/scripts/stats/nwps/jevs_nwps_wave_grid2obs_stats.sh
+++ dev/drivers/scripts/stats/nwps/jevs_nwps_wave_grid2obs_stats.sh
@@ -2,7 +2,7 @@
 #PBS -j oe
 #PBS -S /bin/bash
 #PBS -q dev
-#PBS -A VERF-DEV
+#PBS -A EVS-DEV
 #PBS -l walltime=00:15:00
 #PBS -l place=vscatter:shared,select=1:ncpus=36:mem=40GB
 #PBS -l debug=true
