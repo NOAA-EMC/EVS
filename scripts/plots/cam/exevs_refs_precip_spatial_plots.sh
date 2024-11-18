3b1553d6f1ca51086e7538be9477a5d075f6156f
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2024-11-18 23:29:27 +0000

    Match ecf and dev scripts

--- scripts/plots/cam/exevs_refs_precip_spatial_plots.sh
+++ scripts/plots/cam/exevs_refs_precip_spatial_plots.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/ksh
 #*******************************************************************************
 # Purpose: setup environment, paths, and run the refs precip spatial map plotting 
 #           python script
