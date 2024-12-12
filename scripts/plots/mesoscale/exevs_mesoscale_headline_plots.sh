600e0f9148a17f7e1e1b5c6657bcc4570962b77d
Perry Shafran <Perry.Shafran@noaa.gov>
2024-12-12 21:40:51 +0000

    Updating tar command in headline plot job

--- scripts/plots/mesoscale/exevs_mesoscale_headline_plots.sh
+++ scripts/plots/mesoscale/exevs_mesoscale_headline_plots.sh
@@ -90,7 +90,7 @@ for CHILD_DIR in ${DATA}/${VERIF_CASE}/out/workdirs/*; do
 done
 
 # Tar and Copy output files to EVS COMOUT directory
-   find ${DATA}/${VERIF_CASE}/* -name "*.png" -type f -not -path "*workdirs" -print | tar -cvf ${DATA}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar --transform='s#.*/##' -T -
+   find ${DATA}/${VERIF_CASE}/out/* -name "*.png" -type f -not -path "*workdirs*" -print | tar -cvf ${DATA}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar --transform='s#.*/##' -T -
 
 if [ $SENDCOM = YES ]; then
     cpreq -v ${DATA}/${NET}.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.v${VDATE}.tar ${COMOUTplots}/.
