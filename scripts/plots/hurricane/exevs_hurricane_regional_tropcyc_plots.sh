1b96c83ac4417917368791c52e5fa28b7878029b
Mallory Row <mallory.row@noaa.gov>
2024-12-23 18:18:54 +0000

    Remove commneted out paths with Jiayi's name

--- scripts/plots/hurricane/exevs_hurricane_regional_tropcyc_plots.sh
+++ scripts/plots/hurricane/exevs_hurricane_regional_tropcyc_plots.sh
@@ -84,7 +84,6 @@ export tc_num=${stormNumber}
 export tropcyc_model_type="regional"
 python ${USHevs}/${COMPONENT}/plot_tropcyc_lead_average.py
 
-#/lfs/h2/emc/ptmp/jiayi.peng/metTC/wp02/plot/WP_2022_MALAKAS/images
 nimgs=$(ls ${STORMroot}/plot/${tc_name}/images/* |wc -l)
 if [ $nimgs -ne 0 ]; then
   cd ${STORMroot}/plot/${tc_name}/images
