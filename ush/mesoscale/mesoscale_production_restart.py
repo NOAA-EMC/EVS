87be9652300035fb408a6d12ccf0bcf4c87c4874
perry shafran <perry.shafran@clogin06.cactus.wcoss2.ncep.noaa.gov>
2025-01-16 17:25:59 +0000

    Keeping current develop version of meoscale_production_restart.py

--- ush/mesoscale/mesoscale_production_restart.py
+++ ush/mesoscale/mesoscale_production_restart.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
-# =============================================================================
-#
-# NAME: mesoscale_production_restart.py
-# CONTRIBUTOR(S): Marcel Caron, marcel.caron@noaa.gov, NOAA/NWS/NCEP/EMC-VPPPGB
-# PURPOSE: Check the appropriate restart directory for restart files and copy
-#          the available files to the working directory
-#
-# =============================================================================
+'''
+Name: mesoscale_production_restart.py
+Contact(s): Marcel Caron
+Abstract:
+'''
 
 import os
 import glob
@@ -23,33 +20,20 @@ COMOUT = os.environ['COMOUT']
 NET = os.environ['NET']
 RUN = os.environ['RUN']
 COMPONENT = os.environ['COMPONENT']
-STEP = os.environ['STEP']
 VERIF_CASE = os.environ['VERIF_CASE']
+STEP = os.environ['STEP']
 
 # Copy files for restart
-if STEP == 'stats':
-    VERIF_CASE = os.environ['VERIF_CASE']
-    RESTART_DIR = os.environ['RESTART_DIR']
-    working_dir = os.path.join(DATA, VERIF_CASE)
-    completed_jobs_file = os.path.join(RESTART_DIR, 'completed_jobs.txt')
-    if os.path.exists(RESTART_DIR):
-        if (os.path.exists(completed_jobs_file) 
-                and os.stat(completed_jobs_file).st_size != 0):
-            print(f"Copying restart directory {RESTART_DIR} "
-                  +f"into working directory {working_dir}")
-            cutil.run_shell_command(
-                ['cp', '-rpv', RESTART_DIR, working_dir]
-            )
-elif STEP == 'plots':
+if STEP == 'plots':
     COMOUTplots = os.environ['COMOUTplots']
     RESTART_DIR = os.environ['RESTART_DIR']
-    SAVE_DIR = os.environ['SAVE_DIR']
-    completed_jobs_file = os.path.join(RESTART_DIR, f'completed_jobs.txt')
+    COMPLETED_JOBS_FILE = os.environ['COMPLETED_JOBS_FILE']
+    working_dir = os.path.join(DATA, VERIF_CASE, 'out')
+    completed_jobs_file = os.path.join(RESTART_DIR, COMPLETED_JOBS_FILE)
     if os.path.exists(completed_jobs_file):
         if os.stat(completed_jobs_file).st_size != 0:
             cutil.run_shell_command(
-                ['cp', '-rpv', os.path.join(RESTART_DIR,'*'), SAVE_DIR]
+                ['cp', '-rpv', os.path.join(RESTART_DIR,'*'), working_dir]
             )
-
 
 print("END: "+os.path.basename(__file__))
