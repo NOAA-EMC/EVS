cd8d70905a2e49a3e45f8c19b636cd1241ea54a1
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2025-03-14 19:34:04 +0000

    TC gen 2022 runs

--- dev/drivers/scripts/stats/hurricane/jevs_hurricane_global_det_tcgen_stats.sh
+++ dev/drivers/scripts/stats/hurricane/jevs_hurricane_global_det_tcgen_stats.sh
@@ -33,14 +33,20 @@ module load prod_envir/${prod_envir_ver}
 source ${HOMEevs}/dev/modulefiles/${COMPONENT}/${COMPONENT}_${STEP}.sh
 
 #Set PDY to override setpdy.sh called in the j-jobs
-export PDY=20241231
+export PDY=20221231
 
 #Define the directories of your TC genesis data and A/Bdeck files
-export COMINgenesis=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/TCgen
-export COMINadeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/adeck
-export COMINbdeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/bdeck
-export COMINadeckJTWC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/adeck
-export COMINbdeckJTWC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/bdeck
+#export COMINgenesis=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/TCgen
+#export COMINadeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/adeck
+#export COMINbdeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/bdeck
+#export COMINadeckJTWC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/adeck
+#export COMINbdeckJTWC=/lfs/h2/emc/vpppg/noscrub/$USER/evs_tc_2024/bdeck
+
+export COMINgenesis=/lfs/h2/emc/vpppg/noscrub/$USER/Data/Year2022/TCgen
+export COMINadeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/Data/Year2022/adeck
+export COMINbdeckNHC=/lfs/h2/emc/vpppg/noscrub/$USER/Data/Year2022/bdeck
+export COMINadeckJTWC=/lfs/h2/emc/vpppg/noscrub/$USER/Data/Year2022/adeck
+export COMINbdeckJTWC=/lfs/h2/emc/vpppg/noscrub/$USER/Data/Year2022/bdeck
 
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
