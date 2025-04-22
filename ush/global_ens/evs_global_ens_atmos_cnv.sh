9c789e28c5e1f9359571c8d04abc090383ab36bf
L. Gwen Chen <lichuan.chen@noaa.gov>
2025-04-22 20:01:07 +0000

    Clean up

--- ush/global_ens/evs_global_ens_atmos_cnv.sh
+++ ush/global_ens/evs_global_ens_atmos_cnv.sh
@@ -43,17 +43,14 @@ if [ $modnam = gefs ] ; then
      mbrs='01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30'
      validhours="00 06 12 18"
      fhrs="0 6 12 18 24 30 36 42 48 54 60 66 72 78 84"
-     #fhrs="000 006 012 018 024 030 036 042 048 054 060 066 072 078 084"
 elif [ $modnam = cmce ] ; then
      mbrs='01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20'
      validhours="00 12"
      fhrs="12 24 36 48 60 72 84"
-     #fhrs="012 024 036 048 060 072 084"
 elif [ $modnam = ecme ] ; then
      mbrs='01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50'
      validhours="00 12"
      fhrs="12 24 36 48 60 72 84"
-     #fhrs="012 024 036 048 060 072 084"
 else
      err_exit "wrong model: $modnam"
 fi
@@ -185,7 +182,6 @@ for fhr in $fhrs ; do
     #***************************************************************************
     mkdir -p $WORK/cnv/run_${modnam}_${fhr}_cnv/stat/${modnam}
     for FILE in $COMOUTsmall/restart/cnv/run_${modnam}_t*z_${fhr}_cnv/stat/${modnam}/* ; do
-#    for FILE in $WORK/cnv/run_${modnam}_t*z_${fhr}_cnv/stat/${modnam}/* ; do
         if [ -s $FILE ]; then
             cp -v $FILE $WORK/cnv/run_${modnam}_${fhr}_cnv/stat/${modnam}/.
         fi
