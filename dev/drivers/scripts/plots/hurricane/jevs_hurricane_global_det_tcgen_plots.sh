3d814767537d9935f07d8f849cce07b25b4fc407
Olivia Ostwald <Olivia.Ostwald@noaa.gov>
2025-05-13 15:18:26 +0000

    Updated walltimes

--- dev/drivers/scripts/plots/hurricane/jevs_hurricane_global_det_tcgen_plots.sh
+++ dev/drivers/scripts/plots/hurricane/jevs_hurricane_global_det_tcgen_plots.sh
@@ -5,7 +5,7 @@
 #PBS -q dev
 #PBS -l select=1:ncpus=1:mem=4GB
 ##PBS -l place=vscatter:exclhost,select=1:ncpus=128:ompthreads=1
-#PBS -l walltime=00:30:00
+#PBS -l walltime=00:15:00
 #PBS -l debug=true
 
 set -x
