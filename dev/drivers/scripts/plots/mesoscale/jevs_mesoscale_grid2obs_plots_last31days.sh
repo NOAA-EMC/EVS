17bf5d1648c36fc2ba9e07af34785c73e93c4f0b
perry shafran <perry.shafran@clogin01.cactus.wcoss2.ncep.noaa.gov>
2025-05-14 21:09:36 +0000

    More updates to resources

--- dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_grid2obs_plots_last31days.sh
+++ dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_grid2obs_plots_last31days.sh
@@ -3,7 +3,7 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=01:15:00
+#PBS -l walltime=01:25:00
 #PBS -l place=vscatter:exclhost,select=6:ncpus=128:mem=500GB
 #PBS -l debug=true
 
