8e11ee02a777065d5519bbd41faba3129c2d1aa6
perry shafran <perry.shafran@clogin08.cactus.wcoss2.ncep.noaa.gov>
2025-05-14 12:19:41 +0000

    Updating runtimes for snow

--- dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_snowfall_plots_last31days.sh
+++ dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_snowfall_plots_last31days.sh
@@ -3,8 +3,8 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=03:45:00
-#PBS -l place=vscatter:exclhost,select=12:ncpus=128:mem=500GB
+#PBS -l walltime=08:20:00
+#PBS -l place=vscatter:exclhost,select=6:ncpus=128:mem=500GB
 #PBS -l debug=true
 
 set -x
@@ -14,7 +14,7 @@ export machine=WCOSS2
 # ECF Settings
 export SENDECF=YES
 export SENDCOM=YES
-export KEEPDATA=NO
+export KEEPDATA=YES
 export SENDDBN=NO
 export SENDDBN_NTC=
 export SENDMAIL=YES
