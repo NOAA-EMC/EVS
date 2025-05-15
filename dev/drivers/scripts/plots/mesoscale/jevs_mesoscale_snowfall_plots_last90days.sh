84922c9c8cd3acdba965bfd1cd77895e78fa6b0b
perry shafran <perry.shafran@clogin06.cactus.wcoss2.ncep.noaa.gov>
2025-05-15 14:21:48 +0000

    Fix bracket

--- dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_snowfall_plots_last90days.sh
+++ dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_snowfall_plots_last90days.sh
@@ -49,7 +49,7 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 export PYTHONPATH=$HOMEevs/ush/$COMPONENT:$PYTHONPATH
 
 # Developer Settings
-export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER]/$NET/$evs_ver_2d
+export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver_2d
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
 export COMOUT=/lfs/h2/emc/ptmp/${USER}/$NET/$evs_ver_2d/$STEP/$COMPONENT
 export EVAL_PERIOD="last90days"
