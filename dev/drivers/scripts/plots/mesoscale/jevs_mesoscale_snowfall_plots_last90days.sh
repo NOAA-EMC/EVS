2668bdef0102a2efeee18f11263c93e1fcac5a05
perry shafran <perry.shafran@clogin06.cactus.wcoss2.ncep.noaa.gov>
2025-05-15 14:31:16 +0000

    Adjust snowfall 90day walltime

--- dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_snowfall_plots_last90days.sh
+++ dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_snowfall_plots_last90days.sh
@@ -3,7 +3,7 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=10:40:00
+#PBS -l walltime=13:10:00
 #PBS -l place=vscatter:exclhost,select=6:ncpus=128:mem=500GB
 #PBS -l debug=true
 
