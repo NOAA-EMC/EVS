dceaa7a5502e386240316340fedebf43f6e58ea9
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2025-05-22 15:43:27 +0000

    incremental changes 05/22/2025 2

--- scripts/prep/global_chem/exevs_global_chem_atmos_grid2obs_prep.sh
+++ scripts/prep/global_chem/exevs_global_chem_atmos_grid2obs_prep.sh
@@ -3,7 +3,7 @@
 ###  UNIX Script Documentation Block
 ###                      .
 ### Script name:         exevs_global_chem_atmos_grid2obs_prep.sh
-### Script description:  To run grid-to-obs verification on Global aerosol modeling
+### Script description:  To run grid-to-obs verification on Global Chemical modeling
 ### Original Author   :  Partha Bhattacharjee
 ###
 ###   Change Logs:
@@ -136,10 +136,10 @@ for OBTTYPE in ${obstype}; do
 done
 #
 ########################################################################
-##  Extract variables from full GEFS-aerosol output to be verified
+##  Extract variables from full Global-Chemical output to be verified
 ##    against observation and option to used already recuded
-##    GEFS-aerosol output (suitable for restrospective run)
-##  Backup GEFS-aerosol reduced output for global_ens_chem_grid2obs
+##    Global-Chemical output (suitable for restrospective run)
+##  Backup Global-Chemical reduced output for global_chem_atmos_grid2obs
 ##    stats step due to insuccficent retention time (at least 6 days)
 ########################################################################
 match_aod_1=":AOTK:"
@@ -152,22 +152,22 @@ match_pm25_3="aerosol_size <2.5e-06"
 declare -a cyc_opt=( 00 06 12 18 )
 let inc=3
 for mdl_cyc in "${cyc_opt[@]}"; do
-    com_gefs=${COMINgefs}/${MODELNAME}.${INITDATE}/${mdl_cyc}/${RUN}/pgrb2ap25
-    if [ -d ${com_gefs} ]; then
-        prep_gefs=${COMOUTprep}/${mdl_cyc}/${RUN}/pgrb2ap25
-        mkdir -p ${prep_gefs}
+    com_gc_mdl=${COMINgefs}/${MODELNAME}.${INITDATE}/${mdl_cyc}/${RUN}/pgrb2ap25
+    if [ -d ${com_gc_mdl} ]; then
+        prep_gc_mdl=${COMOUTprep}/${mdl_cyc}/${RUN}/pgrb2ap25
+        mkdir -p ${prep_gc_mdl}
         let hour_now=0
         let max_hour=120
         while [ ${hour_now} -le ${max_hour} ]; do
             fhr=`printf %3.3d ${hour_now}`
             mdl_full_grib2=${MODELNAME}.${RUN}.t${mdl_cyc}z.a2d_0p25.f${fhr}.grib2
             reduced_rec_grib2=${MODELNAME}.${RUN}.t${mdl_cyc}z.a2d_0p25.f${fhr}.reduced.grib2
-            check_full_file=${com_gefs}/${mdl_full_grib2}
-            check_reduced_file=${com_gefs}/${reduced_rec_grib2}
+            check_full_file=${com_gc_mdl}/${mdl_full_grib2}
+            check_reduced_file=${com_gc_mdl}/${reduced_rec_grib2}
             if [ -s ${check_reduced_file} ]; then
                 echo "Found file ${check_reduced_file}"
                 if [ ${SENDCOM} = "YES" ]; then
-                    cp -v ${check_reduced_file} ${prep_gefs}
+                    cp -v ${check_reduced_file} ${prep_gc_mdl}
                 fi
             elif [ -s ${check_full_file} ]; then
                 if [ -e extract_aod ]; then /bin/rm -rf extract_aod; fi
@@ -176,15 +176,15 @@ for mdl_cyc in "${cyc_opt[@]}"; do
                 wgrib2 -match "${match_pm25_1}" -match "${match_pm25_2}" -match "${match_pm25_3}" ${check_full_file} -grib extract_pm25
                 cat extract_aod extract_pm25 >> ${reduced_rec_grib2}
                 if [ ${SENDCOM} = "YES" ]; then
-                    cp -v ${reduced_rec_grib2} ${prep_gefs}
+                    cp -v ${reduced_rec_grib2} ${prep_gc_mdl}
                 fi
             else
-                echo "FCST_OUTPUT_MISSING: GEFS-aerosol forecast file ${check_full_file} is missing. The missing GEFS-aerosol forecast file will be skipped"
+                echo "FCST_OUTPUT_MISSING: Global-Chemical forecast file ${check_full_file} is missing. The missing Global-Chemical forecast file will be skipped"
             fi
             ((hour_now+=${inc}))
         done
     else
-        echo "FCST_OUTPUT_MISSING: GEFS-aerosol output directory ${com_gefs} is missing. The missing GEFS-aerosol forecast files will be skipped"
+        echo "FCST_OUTPUT_MISSING: Global-Chemical output directory ${com_gc_mdl} is missing. The missing Global-Chemical forecast files will be skipped"
     fi
 done
 #
