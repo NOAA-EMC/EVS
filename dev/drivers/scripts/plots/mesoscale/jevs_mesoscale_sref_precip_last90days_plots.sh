9671fb63d04fd947164cd272eef72980c5fb5eeb
Binbin.Zhou <Binbin.Zhou@noaa.gov>
2025-05-29 16:11:05 +0000

    Fix typo in jevs_mesoscale_sref_precip_last90days_plots.s

--- dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_sref_precip_last90days_plots.sh
+++ dev/drivers/scripts/plots/mesoscale/jevs_mesoscale_sref_precip_last90days_plots.sh
@@ -4,7 +4,7 @@
 #PBS -q dev
 #PBS -A VERF-DEV
 #PBS -l walltime=00:20:00
-#PBS -l place=vscatter,select=1:ncpus=60:Mem=150GB
+#PBS -l place=vscatter,select=1:ncpus=60:mem=150GB
 #PBS -l debug=true
 
 set -x
