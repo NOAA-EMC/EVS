0d3cf1f4a1d15a199aa0bdad2dcb71dc79939025
marcel caron <marcel.caron@dlogin05.dogwood.wcoss2.ncep.noaa.gov>
2025-05-30 19:44:15 +0000

    Update walltime

--- dev/drivers/scripts/stats/cam/jevs_cam_hireswarw_grid2obs_stats.sh
+++ dev/drivers/scripts/stats/cam/jevs_cam_hireswarw_grid2obs_stats.sh
@@ -4,7 +4,7 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=01:00:00
+#PBS -l walltime=01:10:00
 #PBS -l place=vscatter:exclhost,select=1:ncpus=128:ompthreads=1:mem=150GB
 #PBS -l debug=true
 
@@ -13,10 +13,10 @@ export model=evs
 export machine=WCOSS2
 
 # ECF Settings
-export SENDMAIL=YES
+export SENDMAIL=NO
 export SENDECF=YES
 export SENDCOM=YES
-export KEEPDATA=NO
+export KEEPDATA=YES
 export SENDDBN=NO
 export SENDDBN_NTC=
 export job=${PBS_JOBNAME:-jevs_cam_hireswarw_grid2obs_stats}
@@ -34,7 +34,7 @@ export VERIF_CASE="grid2obs"
 export MODELNAME="hireswarw"
 
 # EVS Settings
-export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/EVS"
+export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/test/EVS"
 export HOMEevs=${HOMEevs:-${PACKAGEROOT}/evs.${evs_ver}}
 export config=$HOMEevs/parm/evs_config/cam/config.evs.prod.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.${MODELNAME}
 
@@ -48,8 +48,8 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 # Developer Settings
 export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
-export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
-export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
+export COMIN=/lfs/h2/emc/vpppg/noscrub/emc.vpppg/$NET/$evs_ver_2d
+export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/test/$STEP/$COMPONENT
 export vhr=${vhr:-${vhr}}
 export MAILTO="andrew.benjamin@noaa.gov,marcel.caron@noaa.gov"
 
