5d71f770222bcca78ae8f69fb956d0d9975e17c4
Mallory Row <mallory.row@noaa.gov>
2025-05-30 18:15:18 +0000

    Adding updates made by NCO in EVS v1.0.20

--- ush/subseasonal/subseasonal_stats_grid2obs_create_weeks3_4_anomaly.py
+++ ush/subseasonal/subseasonal_stats_grid2obs_create_weeks3_4_anomaly.py
@@ -125,7 +125,7 @@ while valid_date_dt <= ENDDATE_dt and fhr <= fhr_end:
         input_file_df = pd.read_csv(input_file, sep=" ", skiprows=1,
                                     skipinitialspace=True, header=None,
                                     names=MET_MPR_column_list, 
-                                    na_filter=False, dtype=str)
+                                    na_filter=False, dtype=str, on_bad_lines='warn')
         input_file_var_level_df = input_file_df[
             (input_file_df['FCST_VAR'] == var) & (input_file_df['FCST_LEV'] == level)
         ]
