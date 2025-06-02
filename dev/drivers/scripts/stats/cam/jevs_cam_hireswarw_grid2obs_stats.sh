73a0fb51beccff0d5bdbb4de60312165d767efeb
marcel caron <marcel.caron@dlogin06.dogwood.wcoss2.ncep.noaa.gov>
2025-06-02 17:08:41 +0000

    Revert back from test EVS

--- dev/drivers/scripts/stats/cam/jevs_cam_hireswarw_grid2obs_stats.sh
+++ dev/drivers/scripts/stats/cam/jevs_cam_hireswarw_grid2obs_stats.sh
@@ -34,7 +34,7 @@ export VERIF_CASE="grid2obs"
 export MODELNAME="hireswarw"
 
 # EVS Settings
-export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/test/EVS"
+export HOMEevs="/lfs/h2/emc/vpppg/noscrub/$USER/EVS"
 export HOMEevs=${HOMEevs:-${PACKAGEROOT}/evs.${evs_ver}}
 export config=$HOMEevs/parm/evs_config/cam/config.evs.prod.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.${MODELNAME}
 
