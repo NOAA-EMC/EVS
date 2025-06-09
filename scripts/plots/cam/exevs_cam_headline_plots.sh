NOAA-EMC/EVS

commit 50ece0bfdd8441b0c224eaebf05b860cafbda57e

Bugfix/fix completed jobs logic (#743)
* Apply modifications for new completed_jobs logic + don't write empty cards

* Apply new completed_jobs logic

* Apply modifications for new completed_jobs logic + don't write empty cards (pcp + sno)

* Mods for new completed_dir + remove subdir + check job card existence

* Only write job names to poe if they exist

* Modify completed jobs logic + other related changes (plots)

* Prewrite completed_jobs_dir to DATA and COMOUT

* Change COMPLETED_JOBS_FILE to COMPLETED_JOBS_DIR

* Only write job names to poe if they exist (plots)

* Simplify DATA structure

* Simplify DATA structure (2)

* Use completed jobs directory rather than file

* Handle case when no job files are created

* Fix typo

* Change ASNOW graphics titles

* Increase walltime

* Handle case when no job files are created (2)

* Write completed job files to working directories

* Don't exclude "completed_jobs" from workdirs command

* Handle case when no job files are created (3)

* Fix find command

* Fix completed_jobs out directory

* Update walltime

* Write text so that completed_job file is not empmpty

* Revert back from test EVS

* Revert test settings back to default

* Reorder "find" command

* Correct level and var_name keywords for Alaska plotting

* Fix typo

* Suppress warning when no job scripts exist

* Revert back to ops setting

---------

Co-authored-by: marcel caron <marcel.caron@clogin02.cactus.wcoss2.ncep.noaa.gov>
Co-authored-by: marcel caron <marcel.caron@clogin07.cactus.wcoss2.ncep.noaa.gov>
Co-authored-by: marcel caron <marcel.caron@dlogin05.dogwood.wcoss2.ncep.noaa.gov>
Co-authored-by: marcel caron <marcel.caron@dlogin06.dogwood.wcoss2.ncep.noaa.gov>
Co-authored-by: marcel caron <marcel.caron@dlogin04.dogwood.wcoss2.ncep.noaa.gov>
Co-authored-by: marcel caron <marcel.caron@dlogin02.dogwood.wcoss2.ncep.noaa.gov>
Co-authored-by: marcel caron <marcel.caron@dlogin08.dogwood.wcoss2.ncep.noaa.gov>

scripts/plots/cam/exevs_cam_headline_plots.sh
@@ -75,10 +75,12 @@ else
 fi
 
 # Copy Plots Output to Main Directory
+shopt -s nullglob
 for CHILD_DIR in ${DATA}/${VERIF_CASE}/out/workdirs/*; do
     cp -ruv $CHILD_DIR/* ${DATA}/${VERIF_CASE}/out/.
     export err=$?; err_chk
 done
+shopt -u nullglob
 
 # Cat the plotting log files
 log_dir="$DATA/$VERIF_CASE/out/logs"

commit b14f9356c50054b09618802ca760a9cf17369087

Address errors caused by changes in PR #743

scripts/plots/cam/exevs_cam_headline_plots.sh
@@ -45,13 +45,13 @@ python $USHevs/cam/cam_create_child_workdirs.py
 export err=$?; err_chk
 
 # Run All CAM headline/plots Jobs
-chmod u+x ${DATA}/${VERIF_CASE}/${STEP}/plotting_job_scripts/*
-ncount_job=$(ls -l ${DATA}/${VERIF_CASE}/${STEP}/plotting_job_scripts/job* |wc -l)
+chmod u+x ${DATA}/${VERIF_CASE}/plotting_job_scripts/*
+ncount_job=$(ls -l ${DATA}/${VERIF_CASE}/plotting_job_scripts/job* 2>/dev/null |wc -l)
 nc=1
 if [ $USE_CFP = YES ]; then
-    ncount_poe=$(ls -l ${DATA}/${VERIF_CASE}/${STEP}/plotting_job_scripts/poe* |wc -l)
+    ncount_poe=$(ls -l ${DATA}/${VERIF_CASE}/plotting_job_scripts/poe* |wc -l)
     while [ $nc -le $ncount_poe ]; do
-        poe_script=${DATA}/${VERIF_CASE}/${STEP}/plotting_job_scripts/poe_jobs${nc}
+        poe_script=${DATA}/${VERIF_CASE}/plotting_job_scripts/poe_jobs${nc}
         chmod 775 $poe_script
         export MP_PGMMODEL=mpmd
         export MP_CMDFILE=${poe_script}
@@ -68,8 +68,11 @@ if [ $USE_CFP = YES ]; then
     done
 else
     while [ $nc -le $ncount_job ]; do
-        ${DATA}/${VERIF_CASE}/${STEP}/plotting_job_scripts/job${nc}
-        export err=$?; err_chk
+        job_file="${DATA}/${VERIF_CASE}/plotting_job_scripts/job${nc}"
+        if [ -f "$job_file" ]; then
+            $job_file
+            export err=$?; err_chk
+        fi
         nc=$((nc+1))
     done
 fi

ush/cam/cam_plots_headline_create_job_script.py
@@ -36,6 +36,7 @@ IMG_HEADER = os.environ['IMG_HEADER']
 PRUNE_DIR = os.environ['PRUNE_DIR']
 SAVE_DIR = os.environ['SAVE_DIR']
 RESTART_DIR = os.environ['RESTART_DIR']
+SENDCOM = os.environ['SENDCOM']
 LOG_TEMPLATE = os.environ['LOG_TEMPLATE']
 LOG_LEVEL = os.environ['LOG_LEVEL']
 STAT_OUTPUT_BASE_DIR = os.environ['STAT_OUTPUT_BASE_DIR']
@@ -64,7 +65,7 @@ INTERP_PNTS = os.environ['INTERP_PNTS']
 CONFIDENCE_INTERVALS = os.environ['CONFIDENCE_INTERVALS']
 PYTHONDONTWRITEBYTECODE = os.environ['PYTHONDONTWRITEBYTECODE']
 njob = os.environ['njob']
-COMPLETED_JOBS_FILE = os.environ['COMPLETED_JOBS_FILE']
+COMPLETED_JOBS_DIR = os.environ['COMPLETED_JOBS_DIR']
 
 # Make a dictionary of environment variables needed to run this particular job
 job_env_vars_dict = {
@@ -77,6 +78,7 @@ job_env_vars_dict = {
     'PRUNE_DIR': PRUNE_DIR,
     'SAVE_DIR': SAVE_DIR.format(njob=njob),
     'RESTART_DIR': RESTART_DIR,
+    'SENDCOM': SENDCOM,
     'STAT_OUTPUT_BASE_DIR': STAT_OUTPUT_BASE_DIR,
     'STAT_OUTPUT_BASE_TEMPLATE': STAT_OUTPUT_BASE_TEMPLATE,
     'LOG_TEMPLATE': LOG_TEMPLATE.format(njob=njob),
@@ -119,15 +121,7 @@ if STEP == 'prep':
 elif STEP == 'stats':
     pass
 elif STEP == 'plots':
-    if f'job{njob}' in cutil.get_completed_jobs(os.path.join(RESTART_DIR, COMPLETED_JOBS_FILE)):
-        job_cmd_list_iterative.append(
-            f'#jobs were restarted, and the following command has already run successfully'
-        )
-        job_cmd_list_iterative.append(
-            f'#python '
-            + f'{USH_DIR}/{PLOT_TYPE}.py'
-        )
-    else:
+    if not f'job{njob}' in cutil.get_completed_jobs(os.path.join(RESTART_DIR, COMPLETED_JOBS_DIR)):
         job_cmd_list_iterative.append(
             f'python '
             + f'{USH_DIR}/{PLOT_TYPE}.py'
@@ -135,7 +129,7 @@ elif STEP == 'plots':
         job_cmd_list_iterative.append(
             f"python -c "
             + f"'import cam_util; cam_util.mark_job_completed("
-            + f"\"{os.path.join(RESTART_DIR, COMPLETED_JOBS_FILE)}\", "
+            + f"\"{RESTART_DIR}\", \"{DATA}\", \"{VERIF_CASE}\", \"{COMPLETED_JOBS_DIR}\", "
             + f"\"job{njob}\")'"
         )
 
@@ -143,82 +137,85 @@ elif STEP == 'plots':
 indent = ''
 indent_width = 4
 iterative_first = True
-job_dir = os.path.join(DATA, VERIF_CASE, STEP, 'plotting_job_scripts')
+job_dir = os.path.join(DATA, VERIF_CASE, 'plotting_job_scripts')
 if not os.path.exists(job_dir):
     os.makedirs(job_dir)
 job_file = os.path.join(job_dir, f'job{njob}')
-print(f"Creating job script: {job_file}")
-job = open(job_file, 'w')
-job.write('#!/bin/bash\n')
-job.write('set -x \n')
-job.write('\n')
-job.write(f'export job_name=\"job{njob}\"\n')
-for name, value in job_env_vars_dict.items():
-    job.write(f'export {name}=\"{value}\"\n')
-job.write('\n')
-if not iterative_first:
-    for cmd in job_cmd_list:
-        job.write(f'{cmd}\n')
-for name_list, values in job_iterate_over_env_lists_dict.items():
-    name = name_list.replace('_LIST','')
-    items = ' '.join([f'\"{item}\"' for item in values['list_items']])
-    job.write(f'{indent}for {name} in {items}; do\n')
-    indent = indent_width*' ' + indent 
-    job.write(f'{indent}export {name}=${name}\n')
-    for var_name in values['exports']:
-        job.write(f'{indent}TARGET_{var_name}=\"{var_name}_$'+'{'+f'{name}'+'}\"\n')
-        job.write(f'{indent}export {var_name}=$'+'{!'+f'TARGET_{var_name}'+'}\n')
-        #job.write(f'{indent}export {var_name}=\"{value}\"\n')
-for name, value in job_dependent_vars.items():
-    if value["exec_value"]:
-        exec(f"{name}={value['exec_value']}")
-        job.write(
-            f'{indent}export {name}={globals()[name]}\n'
-        )
-    elif value["bash_value"]:
-        job.write(f'{indent}export {name}={value["bash_value"]}\n')
-    if (value["bash_conditional"] 
-            and (
-            value["bash_conditional_value"] 
-            or value["bash_conditional_else_value"])):
-        job.write(
-            f'{indent}if {value["bash_conditional"]};'
-            + f' then\n'
-        )
-        job.write(
-            f'{indent}{" "*indent_width}export {name}='
-            + f'{value["bash_conditional_value"]}\n'
-        )
-        if (value["bash_conditional_else_value"]):
+if not job_cmd_list and not job_cmd_list_iterative:
+    print(f"No commands to run / not creating job script: {job_file}")
+else:
+    print(f"Creating job script: {job_file}")
+    job = open(job_file, 'w')
+    job.write('#!/bin/bash\n')
+    job.write('set -x \n')
+    job.write('\n')
+    job.write(f'export job_name=\"job{njob}\"\n')
+    for name, value in job_env_vars_dict.items():
+        job.write(f'export {name}=\"{value}\"\n')
+    job.write('\n')
+    if not iterative_first:
+        for cmd in job_cmd_list:
+            job.write(f'{cmd}\n')
+    for name_list, values in job_iterate_over_env_lists_dict.items():
+        name = name_list.replace('_LIST','')
+        items = ' '.join([f'\"{item}\"' for item in values['list_items']])
+        job.write(f'{indent}for {name} in {items}; do\n')
+        indent = indent_width*' ' + indent 
+        job.write(f'{indent}export {name}=${name}\n')
+        for var_name in values['exports']:
+            job.write(f'{indent}TARGET_{var_name}=\"{var_name}_$'+'{'+f'{name}'+'}\"\n')
+            job.write(f'{indent}export {var_name}=$'+'{!'+f'TARGET_{var_name}'+'}\n')
+            #job.write(f'{indent}export {var_name}=\"{value}\"\n')
+    for name, value in job_dependent_vars.items():
+        if value["exec_value"]:
+            exec(f"{name}={value['exec_value']}")
             job.write(
-                f'{indent}else'
-                + f'\n'
+                f'{indent}export {name}={globals()[name]}\n'
+            )
+        elif value["bash_value"]:
+            job.write(f'{indent}export {name}={value["bash_value"]}\n')
+        if (value["bash_conditional"] 
+                and (
+                value["bash_conditional_value"] 
+                or value["bash_conditional_else_value"])):
+            job.write(
+                f'{indent}if {value["bash_conditional"]};'
+                + f' then\n'
             )
             job.write(
                 f'{indent}{" "*indent_width}export {name}='
-                + f'{value["bash_conditional_else_value"]}\n'
+                + f'{value["bash_conditional_value"]}\n'
             )
-        job.write(f'{indent}fi\n')
-for name, value in job_iterate_over_custom_lists_dict.items():
-    job.write(f"{indent}for {name} in {value['custom_list']}; do\n")
-    indent = indent_width*' ' + indent
-    job.write(f"{indent}export {name}=${value['export_value']}\n")
-    if value['dependent_vars']:
-        dep_names = value['dependent_vars']['names']
-        dep_values = value['dependent_vars']['values']
-        for dn, dep_name in enumerate(dep_names):
-            job.write(f"{indent}export {dep_name}={dep_values[dn]}\n")
-for cmd in job_cmd_list_iterative:
-    job.write(f'{indent}{cmd}\n')
-for name, value in job_iterate_over_custom_lists_dict.items():
-    indent = indent[indent_width:]
-    job.write(f'{indent}done\n')
-for name_list, value_list in job_iterate_over_env_lists_dict.items():
-    indent = indent[indent_width:]
-    job.write(f'{indent}done\n')
-if iterative_first:
-    for cmd in job_cmd_list:
-        job.write(f'{cmd}\n')
-job.close()
+            if (value["bash_conditional_else_value"]):
+                job.write(
+                    f'{indent}else'
+                    + f'\n'
+                )
+                job.write(
+                    f'{indent}{" "*indent_width}export {name}='
+                    + f'{value["bash_conditional_else_value"]}\n'
+                )
+            job.write(f'{indent}fi\n')
+    for name, value in job_iterate_over_custom_lists_dict.items():
+        job.write(f"{indent}for {name} in {value['custom_list']}; do\n")
+        indent = indent_width*' ' + indent
+        job.write(f"{indent}export {name}=${value['export_value']}\n")
+        if value['dependent_vars']:
+            dep_names = value['dependent_vars']['names']
+            dep_values = value['dependent_vars']['values']
+            for dn, dep_name in enumerate(dep_names):
+                job.write(f"{indent}export {dep_name}={dep_values[dn]}\n")
+    for cmd in job_cmd_list_iterative:
+        job.write(f'{indent}{cmd}\n')
+    for name, value in job_iterate_over_custom_lists_dict.items():
+        indent = indent[indent_width:]
+        job.write(f'{indent}done\n')
+    for name_list, value_list in job_iterate_over_env_lists_dict.items():
+        indent = indent[indent_width:]
+        job.write(f'{indent}done\n')
+    if iterative_first:
+        for cmd in job_cmd_list:
+            job.write(f'{cmd}\n')
+    job.close()
 
 print(f"END: {os.path.basename(__file__)}")

ush/cam/cam_plots_headline_create_poe_job_scripts.py
@@ -26,42 +26,49 @@ DATA = os.environ['DATA']
 
 # If Using CFP, create POE scripts
 if USE_CFP == 'YES':
-    job_dir = os.path.join(DATA, VERIF_CASE, STEP, 'plotting_job_scripts')
+    job_dir = os.path.join(DATA, VERIF_CASE, 'plotting_job_scripts')
     job_files = glob.glob(os.path.join(job_dir, 'job*'))
     njob_files = len(job_files)
     if njob_files == 0:
         print(f"NOTE: No job files created in {job_dir}")
-    poe_job_files = glob.glob(os.path.join(job_dir, f'poe_job*'))
-    npoe_job_files = len(poe_job_files)
-    if npoe_job_files > 0:
-        for poe_job_file in poe_job_files:
-            os.remove(poe_job_file)
+    
+    # Remove any existing poe_job* files
+    for poe_job_file in glob.glob(os.path.join(job_dir, 'poe_job*')):
+        os.remove(poe_job_file)
+
     njob, iproc, node = 1, 0, 1
     while njob <= njob_files:
         job_filename = f'job{njob}'
-        if machine in ['HERA', 'ORION', 'S4', 'JET']:
-            if iproc >= int(nproc):
-                iproc = 0
-                node+=1
+        job_path = os.path.join(job_dir, job_filename)
+
+        if not os.path.isfile(job_path):
+            njob += 1
+            continue
+
+        if iproc >= int(nproc):
+            iproc = 0
+            node += 1
+
         poe_job_file = os.path.join(job_dir, f'poe_jobs{node}')
-        poe_job = open(poe_job_file, 'a')
-        iproc+=1
-        if machine in ['HERA', 'ORION', 'S4', 'JET']:
-            poe_job.write(f'{iproc-1} {os.path.join(job_dir, job_filename)}\n')
-        else:
-            poe_job.write(f'{os.path.join(job_dir, job_filename)}\n')
-        poe_job.close()
-        njob+=1
+        with open(poe_job_file, 'a') as poe_job:
+            if machine in ['HERA', 'ORION', 'S4', 'JET']:
+                poe_job.write(f'{iproc} {job_path}\n')
+            else:
+                poe_job.write(f'{job_path}\n')
+
+        iproc += 1
+        njob += 1
+
+    # Fille remaining processors with /bin/echo commands
     poe_job_file = os.path.join(job_dir, f'poe_jobs{node}')
-    poe_job = open(poe_job_file, 'a')
-    iproc+=1
-    while iproc <= int(nproc):
-        if machine in ['HERA', 'ORION', 'S4', 'JET']:
-            poe_job.write(f'{iproc-1} /bin/echo {iproc}\n')
-        else:
-            poe_job.write(f'/bin/echo {iproc}\n')
-        iproc+=1
-    poe_job.close()
+    with open(poe_job_file, 'a') as poe_job:
+        while iproc < int(nproc):
+            if machine in ['HERA', 'ORION', 'S4', 'JET']:
+                poe_job.write(f'{iproc} /bin/echo {iproc}\n')
+            else:
+                poe_job.write(f'/bin/echo {iproc}\n')
+            iproc += 1
+
 else:
     print(f"FATAL ERROR: Cannot create POE scripts because USE_CFP is set to"
           + f" {USE_CFP}.  Please set USE_CFP=YES")