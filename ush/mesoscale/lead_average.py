1be24f04be72ff2f8077011e99ac58a284ee172a
Perry Shafran <Perry.Shafran@noaa.gov>
2025-06-12 17:13:02 +0000

    Removing commented out line for lead_average.py

--- ush/mesoscale/lead_average.py
+++ ush/mesoscale/lead_average.py
@@ -1434,7 +1434,6 @@ def plot_lead_average(df: pd.DataFrame, logger: logging.Logger,
         save_name = f'{save_header}.'+save_name
     save_subdir = os.path.join(
         save_dir, f'{str(plot_group).lower()}'  
-##        f'{str(time_period_savename).lower()}'
     )
     if not os.path.isdir(save_subdir):
         try:
@@ -1450,7 +1449,6 @@ def plot_lead_average(df: pd.DataFrame, logger: logging.Logger,
             os.path.join(
                 restart_dir, 
                 f'{str(plot_group).lower()}', 
-##                f'{str(time_period_savename).lower()}', 
                 save_name+'.png'
             )
         )
