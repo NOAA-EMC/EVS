119585816b9514bb71817f86db08bf4103f62e69
Samira Ardani <samira.ardani@noaa.gov>
2025-07-02 16:55:05 +0000

    Separated last31days and last90days plots.

--- scripts/plots/nfcens/exevs_nfcens_wave_grid2obs_plots.sh
+++ scripts/plots/nfcens/exevs_nfcens_wave_grid2obs_plots.sh
@@ -150,7 +150,9 @@ export err=$?; err_chk
 #######################
 # Gather all the files 
 #######################
-periods='LAST31DAYS LAST90DAYS'
+#periods='LAST31DAYS LAST90DAYS'
+periods=$(echo "$EVAL_PERIOD" | tr '[:lower:]' '[:upper:]')
+
 if [ $gather = yes ] ; then
   nc=$(ls ${DATA}/images/*.png | wc -l | awk '{print $1}')
   echo "Found ${nc} ${DATA}/images/*.png "
