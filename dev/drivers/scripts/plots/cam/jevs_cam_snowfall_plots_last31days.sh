bdf801f14b7c3a90b73cad5a0eb1c679d4ea69cf
alicia bentley <alicia.bentley@clogin02.cactus.wcoss2.ncep.noaa.gov>
2025-07-07 16:43:54 +0000

    Updating project codes in new cam snowfall plots scripts

--- dev/drivers/scripts/plots/cam/jevs_cam_snowfall_plots_last31days.sh
+++ dev/drivers/scripts/plots/cam/jevs_cam_snowfall_plots_last31days.sh
@@ -2,7 +2,7 @@
 #PBS -j oe
 #PBS -S /bin/bash
 #PBS -q dev
-#PBS -A EVS-DEV
+#PBS -A VERF-DEV
 #PBS -l walltime=00:35:00
 #PBS -l place=vscatter:exclhost,select=4:ncpus=128:mem=150GB
 #PBS -l debug=true
