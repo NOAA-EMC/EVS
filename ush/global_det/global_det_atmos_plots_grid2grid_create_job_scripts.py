b88e9b5941c536d62592c233a5cd9e063ab1418e
Qi Shi <qi.shi@clogin09.cactus.wcoss2.ncep.noaa.gov>
2025-08-22 21:23:46 +0000

    remove precip plots by PNS

--- ush/global_det/global_det_atmos_plots_grid2grid_create_job_scripts.py
+++ ush/global_det/global_det_atmos_plots_grid2grid_create_job_scripts.py
@@ -223,12 +223,12 @@ base_plot_jobs_info_dict = {
                                                     'levels': ['P500']}},
         'Ozone': {'vx_masks': ['GLOBAL', 'NHEM', 'SHEM', 'TROPICS'],
                   'fcst_var_dict': {'name': 'O3MR',
-                                    'levels': ['P925', 'P100', 'P70', 'P50',
-                                               'P30', 'P20', 'P10', 'P5',
+                                    'levels': ['P925', 'P70',
+                                               'P30', 'P10', 
                                                'P1']},
                   'obs_var_dict': {'name': 'O3MR',
-                                   'levels': ['P925', 'P100', 'P70', 'P50',
-                                              'P30', 'P20', 'P10', 'P5',
+                                   'levels': ['P925', 'P70',
+                                              'P30', 'P10',
                                               'P1']}},
         'PresSeaLevel': {'vx_masks': ['GLOBAL', 'NHEM', 'SHEM', 'TROPICS'],
                          'fcst_var_dict': {'name': 'PRMSL',
@@ -379,11 +379,11 @@ for means_job in list(filter_stats_jobs_dict['means'].keys()):
 filter_stats_jobs_dict['precip']['24hrCCPA']['line_types'] = ['CTC']
 filter_stats_jobs_dict['precip']['24hrCCPA']['grid'] = 'G211'
 filter_stats_jobs_dict['precip']['24hrCCPA']['fcst_var_dict']['threshs'] = [
-    'ge0.1', 'ge0.5', 'ge1', 'ge5', 'ge10', 'ge25', 'ge50', 'ge75', 'ge0.254',
+    'ge0.254',
     'ge2.54', 'ge6.35', 'ge12.7', 'ge25.4', 'ge50.8', 'ge76.2', 'ge101.6'
 ]
 filter_stats_jobs_dict['precip']['24hrCCPA']['obs_var_dict']['threshs'] = [
-    'ge0.1', 'ge0.5', 'ge1', 'ge5', 'ge10', 'ge25', 'ge50', 'ge75', 'ge0.254',
+    'ge0.254',
     'ge2.54', 'ge6.35', 'ge12.7', 'ge25.4', 'ge50.8', 'ge76.2', 'ge101.6'
 ]
 filter_stats_jobs_dict['precip']['24hrCCPA']['interps'] = ['NEAREST/1']
@@ -393,9 +393,7 @@ filter_stats_jobs_dict['precip']['24hrCCPA_Nbrhd'] = copy.deepcopy(
 filter_stats_jobs_dict['precip']['24hrCCPA_Nbrhd']['line_types'] = ['NBRCNT']
 filter_stats_jobs_dict['precip']['24hrCCPA_Nbrhd']['grid'] = 'G240'
 filter_stats_jobs_dict['precip']['24hrCCPA_Nbrhd']['interps'] = [
-    'NBRHD_SQUARE/1', 'NBRHD_SQUARE/169', 'NBRHD_SQUARE/529',
-    'NBRHD_SQUARE/1089', 'NBRHD_SQUARE/1849', 'NBRHD_SQUARE/2809',
-    'NBRHD_SQUARE/3969'
+    'NBRHD_SQUARE/1', 'NBRHD_SQUARE/169'
 ]
 #### pres_levs
 for pres_levs_job in list(filter_stats_jobs_dict['pres_levs'].keys()):
@@ -457,9 +455,7 @@ for snow_job in list(filter_stats_jobs_dict['snow'].keys()):
         'NBRCNT'
     ]
     filter_stats_jobs_dict['snow'][f"{snow_job}_Nbrhd"]['interps'] = [
-        'NBRHD_SQUARE/1', 'NBRHD_SQUARE/169', 'NBRHD_SQUARE/529',
-        'NBRHD_SQUARE/1089', 'NBRHD_SQUARE/1849', 'NBRHD_SQUARE/2809',
-        'NBRHD_SQUARE/3969'
+        'NBRHD_SQUARE/1', 'NBRHD_SQUARE/169'
     ]
 #### sst
 for sst_job in list(filter_stats_jobs_dict['sst'].keys()):
