8eab22cc7150fac475c7472592431e9216d0a61a
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2025-08-28 19:00:31 +0000

    add headline plot

---
+++ scripts/plots/global_chem/exevs_global_chem_headline_grid2obs_plots.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+###############################################################################
+# Name of Script: exevs_global_chem_headline_grid2obs_plots.sh
+# Developers: Ho-Chun Huang / Ho-Chun.Huang@noaa.gov
+#
+# Purpose of Script: This script is run for the global_chemplots step for
+#                    the headline verification. It uses EMC-developed
+#                    python scripts to do the plotting.
+#
+#   Change Logs:
+###############################################################################
+
+set -x
+
+echo "RUN MODE:${evs_run_mode}"
+
+export obs_src_name=airnow
+
+## Temporary staging area for renaming and/or updating model
+## name id in the stats files
+export STATDIR=${DATA}/stats_staging
+
+## Provides data location in ~/ush/global_chem/global_chem_plots_headline.py
+export linked_stat_base_dir=${DATA}/data/headline
+
+mkdir -p ${STATDIR} ${linked_stat_base_dir}
+
+model1=`echo ${MODELNAME} | tr a-z A-Z`
+export model1
+
+gcafs_ver_id=$( echo ${gefs_ver} | awk -F"." '{print $1}' )
+export modelid=${MODELNAME}${gcafs_ver_id}
+#
+# Define plot type, model id, and input stats file locations.
+#
+declare -a obstype_list=(airnow_pm25 aeronet_aod)
+declare -a mdl_list=(${modelid})
+declare -a mdl_idir_list=( ${COMIN}/stats/${COMPONENT} )
+declare -a plotname_list=( ${gcafs_ver_id} )
+declare -a obssrc_list=(airnow aeronet)
+
+let num_mdl=${#mdl_list[@]}
+if [ ${num_mdl} -gt 10 ]; then
+    echo "number of model to be plotted can not exceed 10"
+    exit
+else
+    export num_plot_mdl="${num_mdl}"
+    IFS=","
+    export plot_model_list="${mdl_list[*]}"
+fi
+
+let num_plotname=${#plotname_list[@]}
+if [ ${num_plotname} -gt 10 ]; then
+    echo "number of model to be plotted can not exceed 10"
+    exit
+else
+    export num_plot_name="${num_plotname}"
+    IFS=","
+    export plot_plotname_list="${plotname_list[*]}"
+fi
+
+let num_obssrc=${#obssrc_list[@]}
+if [ ${num_obssrc} -gt 10 ]; then
+    echo "number of obs_src to be plotted can not exceed 10"
+    exit
+else
+    export num_obs_src="${num_obssrc}"
+    IFS=","
+    export plot_obssrc_list="${obssrc_list[*]}"
+fi
+#
+# Bringing in all statistics files and rearranging the filename and
+# model ID according to the model name defined in `mdl_list`.
+#
+let imdl=0
+while [ ${imdl} -lt ${num_mdl} ]; do
+    biasc=$( echo ${mdl_list[${imdl}]} | awk -F"_" '{print $2}' )
+    input_plots_model_name=${mdl_list[${imdl}]}
+    idir=${mdl_idir_list[${imdl}]}
+    linked_plot_stat_dir=${linked_stat_base_dir}/${input_plots_model_name}
+    if [ ! -d ${linked_plot_stat_dir} ]; then mkdir -p ${linked_plot_stat_dir}; fi
+    for ivar in "${obstype_list[@]}"; do
+        ## get 2 additional day's stat for day 1 forecast
+        cdate=${VDATE_START}"00"
+        NOW=$( ${NDATE} -48 ${cdate} | cut -c1-8 )
+        while [ ${NOW} -le ${VDATE_END} ]; do
+            cpfile=evs.stats.${MODELNAME}.atmos.${VERIF_CASE}_${ivar}.v${NOW}.stat
+            sedfile=${input_plots_model_name}_${ivar}.v${NOW}.stat
+            if [ -s ${idir}/${MODELNAME}.${NOW}/${cpfile} ]; then
+                cpreq ${idir}/${MODELNAME}.${NOW}/${cpfile} ${STATDIR}
+                sed "s/${model1}/${input_plots_model_name}/g" ${STATDIR}/${cpfile} > ${STATDIR}/${sedfile}
+                dest_model_date_stat_file=${linked_plot_stat_dir}/${input_plots_model_name}_${ivar}_v${NOW}.stat
+                ln -s ${STATDIR}/${sedfile} ${dest_model_date_stat_file}
+            else
+                echo "DEBUG :: Input Stats ${idir}/${MODELNAME}.${NOW}/${cpfile} is missing and it will be skipped"
+            fi
+            cdate=${NOW}"00"
+            NOW=$( ${NDATE} +24 ${cdate} | cut -c1-8 )
+        done
+    done
+    ((imdl++))
+done
+
+# Create headline plots
+python ${USHevs}/global_chem/global_chem_plots_headline.py
+export err=$?; err_chk
+
+# Copy files to desired location
+if [ "${SENDCOM}" == "YES" ]; then
+    # Make and copy tar file
+    cd ${DATA}/images
+    headline_tar_name=${DATA}/evs.plots.${COMPONENT}.atmos.${RUN}.v${VDATE_END}.tar
+    tar -cvf ${headline_tar_name} *.png
+    if [ -f "${headline_tar_name}" ]; then
+        cp -v ${headline_tar_name} ${COMOUT}/.
+    fi
+fi
+
+# Cat the plotting log files
+log_dir=${DATA}/logs
+log_file_count=$(find ${log_dir} -type f |wc -l)
+if [[ ${log_file_count} -ne 0 ]]; then
+    for log_file in ${log_dir}/*; do
+        echo "Start: ${log_file}"
+        cat ${log_file}
+        echo "End: ${log_file}"
+    done
+fi
+
+if [ "${SENDDBN}" = "YES" ]; then
+    headline_tar_name=${COMOUT}/evs.plots.${COMPONENT}.atmos.${RUN}.v${VDATE_END}.tar
+    if [ -f "${headline_tar_name}" ]; then
+        ${DBNROOT}/bin/dbn_alert MODEL EVS_RZDM ${job} ${headline_tar_name}
+    fi
+fi
