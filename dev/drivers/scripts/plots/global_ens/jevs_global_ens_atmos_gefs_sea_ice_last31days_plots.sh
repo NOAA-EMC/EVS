ac5b52e6346b87a0228ecda3b6cad53f0f28f036
GwenChen-NOAA <95313292+GwenChen-NOAA@users.noreply.github.com>
2025-09-03 14:23:56 -0400

    global_ens: adjust atmos snowfall, sea ice, and sst plots (#810)
    * Adjust snowfall, sea ice, and sst plots
    
    * Change tar filename for sea ice

--- dev/drivers/scripts/plots/global_ens/jevs_global_ens_atmos_gefs_sea_ice_last31days_plots.sh
+++ dev/drivers/scripts/plots/global_ens/jevs_global_ens_atmos_gefs_sea_ice_last31days_plots.sh
@@ -1,10 +1,10 @@
-#PBS -N jevs_global_ens_atmos_gefs_sea_ice_past90days_plots
-#PBS -j oe 
+#PBS -N jevs_global_ens_atmos_gefs_sea_ice_last31days_plots
+#PBS -j oe
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
 #PBS -l walltime=00:10:00
-#PBS -l place=vscatter,select=1:ncpus=32:mem=40GB
+#PBS -l place=vscatter,select=1:ncpus=29:mem=10GB
 #PBS -l debug=true
 
 set -x
@@ -24,6 +24,7 @@ export MODELNAME=gefs
 
 module reset
 module load prod_envir/${prod_envir_ver}
+
 source $HOMEevs/dev/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
 
 evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
@@ -34,7 +35,7 @@ export KEEPDATA=NO
 export SENDDBN=NO
 
 export vhr=00
-export past_days=90
+export past_days=31
 
 
 export valid_time=both
--- dev/drivers/scripts/plots/global_ens/jevs_global_ens_atmos_gefs_snowfall_last90days_plots.sh
+++ dev/drivers/scripts/plots/global_ens/jevs_global_ens_atmos_gefs_snowfall_last90days_plots.sh
@@ -1,10 +1,10 @@
-#PBS -N jevs_global_ens_atmos_gefs_snowfall_past90days_plots
+#PBS -N jevs_global_ens_atmos_gefs_snowfall_last90days_plots
 #PBS -j oe 
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=01:00:00
-#PBS -l place=vscatter,select=1:ncpus=32:mem=150GB
+#PBS -l walltime=00:15:00
+#PBS -l place=vscatter,select=2:ncpus=95:mem=100GB
 #PBS -l debug=true
 
 set -x
