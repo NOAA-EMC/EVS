745fec62ca32fac47e07dacd4b659bc9b2e72fc5
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2025-09-09 13:33:19 +0000

    adjust mem=20GB

--- dev/drivers/scripts/plots/global_chem/jevs_global_chem_headline_grid2obs_plots_last90days.sh
+++ dev/drivers/scripts/plots/global_chem/jevs_global_chem_headline_grid2obs_plots_last90days.sh
@@ -4,7 +4,7 @@
 #PBS -q dev
 #PBS -A VERF-DEV
 #PBS -l walltime=00:15:00
-#PBS -l place=shared,select=1:ncpus=1:mem=30GB
+#PBS -l place=shared,select=1:ncpus=1:mem=20GB
 #PBS -l debug=true
 
 set -x
