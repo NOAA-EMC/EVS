633583c4cf993b18d1ce5b849aadfcc7563ec07d
alicia bentley <alicia.bentley@clogin09.cactus.wcoss2.ncep.noaa.gov>
2025-09-24 15:47:41 +0000

    Changed project code/COMIN/COMOUT: analyses precip

--- dev/drivers/scripts/prep/analyses/jevs_analyses_precip_prep.sh
+++ dev/drivers/scripts/prep/analyses/jevs_analyses_precip_prep.sh
@@ -47,7 +47,7 @@ evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
 # Developer Settings
 export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
-export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
+export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}_devonly/$evs_ver_2d/$STEP/$COMPONENT
 export vhr=${vhr:-${vhr}}
 
 # Job Settings and Run
--- dev/drivers/scripts/stats/analyses/jevs_analyses_urma_precip_stats.sh
+++ dev/drivers/scripts/stats/analyses/jevs_analyses_urma_precip_stats.sh
@@ -3,7 +3,7 @@
 #PBS -j oe
 #PBS -S /bin/bash
 #PBS -q "dev"
-#PBS -A EVS-DEV
+#PBS -A VERF-DEV
 #PBS -l walltime=00:10:00
 #PBS -l place=shared,select=1:ncpus=1:mem=10GB
 #PBS -l debug=true
@@ -47,8 +47,8 @@ source $HOMEevs/dev/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
 export job=${PBS_JOBNAME:-jevs_${MODELNAME}_${VERIF_CASE}_${STEP}}
 export jobid=$job.${PBS_JOBID:-$$}
 
-export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver_2d}
-export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver_2d}
+export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}_devonly/${evs_ver_2d}
+export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}_devonly/${evs_ver_2d}
 
 export vhr
 echo $vhr
