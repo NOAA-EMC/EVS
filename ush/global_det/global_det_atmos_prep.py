921f5bf2fae35799a3e11839d8993fc092767731
Qi Shi <qi.shi@noaa.gov>
2025-09-26 09:22:49 -0500

    Replace Ukmet with high resolution data from com for release/evs.v1.0.23 (#831)
    * switch ukmet source from dcom to com for high resolution
    
    * remove ukm_hires_merge package from source code
    
    * add back ukmet QPF verification and change DCOMINukmet to COMINukment
    
    * add COMINukmet int dev driver for testing
    
    * reverse the change in pre.sh file
    
    * fix list order in run.ver
    
    ---------
    
    Co-authored-by: Qi Shi <qi.shi@clogin01.cactus.wcoss2.ncep.noaa.gov>
    Co-authored-by: Qi Shi <qi.shi@clogin03.cactus.wcoss2.ncep.noaa.gov>
    Co-authored-by: Qi Shi <qi.shi@clogin06.cactus.wcoss2.ncep.noaa.gov>

--- ush/global_det/global_det_atmos_prep.py
+++ ush/global_det/global_det_atmos_prep.py
@@ -23,6 +23,7 @@ print("Working in: "+cwd)
 DATA = os.environ['DATA']
 COMINcfs = os.environ['COMINcfs']
 COMINcmc = os.environ['COMINcmc']
+COMINukmet = os.environ['COMINukmet']
 DCOMINcmc_precip = os.environ['DCOMINcmc_precip']
 DCOMINcmc_regional_precip = os.environ['DCOMINcmc_regional_precip']
 DCOMINdwd_precip = os.environ['DCOMINdwd_precip']
@@ -33,7 +34,6 @@ DCOMINimd = os.environ['DCOMINimd']
 DCOMINjma = os.environ['DCOMINjma']
 DCOMINjma_precip = os.environ['DCOMINjma_precip']
 DCOMINmetfra_precip = os.environ['DCOMINmetfra_precip']
-DCOMINukmet = os.environ['DCOMINukmet']
 DCOMINukmet_precip = os.environ['DCOMINukmet_precip']
 DCOMINosi_saf = os.environ['DCOMINosi_saf']
 DCOMINghrsst_ospo = os.environ['DCOMINghrsst_ospo']
@@ -151,11 +151,12 @@ global_det_model_dict = {
                                                         +'{init?fmt=%Y%m%d}.gz'),
                'inithours': ['00', '12'],
                'fcst_hrs': range(24, 72+12, 12)},
-    'ukmet': {'input_fcst_file_format': os.path.join(DCOMINukmet,
-                                                     'GAB{init?fmt=%2H}'
-                                                     +'{letter?fmt=str}.GRB'),
-              'input_anl_file_format': os.path.join(DCOMINukmet,
-                                                    'GAB{init?fmt=%2H}AAT.GRB'),
+    'ukmet': {'input_fcst_file_format': os.path.join(COMINukmet,
+                                                     'ukmet.t{init?fmt=%2H}z.'
+                                                     +'0p25.f{lead?fmt=%3H}.grib'),
+              'input_anl_file_format': os.path.join(COMINukmet,
+                                                     'ukmet.t{init?fmt=%2H}z.'
+                                                     +'0p25.f000.grib'),
               #'input_precip_file_format': os.path.join(DCOMINukmet_precip, 'ukmo.'
               #                                         +'{init?fmt=%Y%m%d%H}'),
               'inithours': ['00', '12'],
