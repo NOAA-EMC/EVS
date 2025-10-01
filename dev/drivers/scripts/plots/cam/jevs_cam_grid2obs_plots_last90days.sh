caf7e5d6d7f0bde857e225bdb87429fe77e20849
marcel caron <marcel.caron@clogin05.cactus.wcoss2.ncep.noaa.gov>
2025-10-01 03:30:07 +0000

    Adjust resources

--- dev/drivers/scripts/plots/cam/jevs_cam_grid2obs_plots_last90days.sh
+++ dev/drivers/scripts/plots/cam/jevs_cam_grid2obs_plots_last90days.sh
@@ -4,8 +4,8 @@
 #PBS -S /bin/bash
 #PBS -q dev
 #PBS -A VERF-DEV
-#PBS -l walltime=10:00:00
-#PBS -l place=vscatter:exclhost,select=12:ncpus=128:mem=330GB
+#PBS -l walltime=04:35:00
+#PBS -l place=vscatter:exclhost,select=12:ncpus=128:mem=250GB
 #PBS -l debug=true
 
 set -x
