522f3e88521ccd7851c8f4fce4d04a6d3eaf0358
Andrew.Benjamin <Andrew.Benjamin@noaa.gov>
2025-10-20 17:08:05 +0000

    removed line from jevs_stats_analyses_urma_grid2obs.sh

--- dev/drivers/scripts/stats/analyses/jevs_stats_analyses_urma_grid2obs.sh
+++ dev/drivers/scripts/stats/analyses/jevs_stats_analyses_urma_grid2obs.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
- 
 #PBS -N jevs_stats_analyses_urma_grid2obs
 #PBS -j oe
 #PBS -S /bin/bash
@@ -37,15 +36,15 @@ export COMPONENT=analyses
 export RUN=atmos
 export VERIF_CASE=grid2obs
 
-export KEEPDATA=NO
-export SENDMAIL=YES
+export KEEPDATA=YES
+export SENDMAIL=NO
 
 source $HOMEevs/dev/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
 
 export job=${PBS_JOBNAME:-jevs_${STEP}_${MODELNAME}_${VERIF_CASE}}
 export jobid=$job.${PBS_JOBID:-$$}
 
-export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver_2d}
+export COMIN=/lfs/h2/emc/vpppg/noscrub/cmv.vpppg/${NET}/${evs_ver_2d}
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/${NET}/${evs_ver_2d}
 
 export vhr
