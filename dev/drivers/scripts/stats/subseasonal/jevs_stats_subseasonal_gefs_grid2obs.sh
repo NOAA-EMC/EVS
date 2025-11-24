34cc9be78756d3850441dc4b0ae3018f630299d0
Andrew.Benjamin <Andrew.Benjamin@noaa.gov>
2025-11-24 14:41:21 +0000

    updated global_ens, nfcens, mesoscale, and subseasonal resources for consistency

--- dev/drivers/scripts/stats/subseasonal/jevs_stats_subseasonal_gefs_grid2obs.sh
+++ dev/drivers/scripts/stats/subseasonal/jevs_stats_subseasonal_gefs_grid2obs.sh
@@ -4,7 +4,7 @@
 #PBS -q "dev"
 #PBS -A VERF-DEV
 #PBS -l walltime=00:25:00
-#PBS -l place=vscatter,select=1:ncpus=8:ompthreads=1:mem=60GB
+#PBS -l place=vscatter:shared,select=1:ncpus=8:ompthreads=1:mem=60GB
 #PBS -l debug=true
 
 set -x
