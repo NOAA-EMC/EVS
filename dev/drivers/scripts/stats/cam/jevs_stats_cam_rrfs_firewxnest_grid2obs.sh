0a3673e6bf9bdca2f3815d713bc65d910ca4de8c
marcel caron <marcel.caron@dlogin05.dogwood.wcoss2.ncep.noaa.gov>
2026-01-16 18:40:02 +0000

    Add/modify dev drivers

--- dev/drivers/scripts/stats/cam/jevs_stats_cam_rrfs_firewxnest_grid2obs.sh
+++ dev/drivers/scripts/stats/cam/jevs_stats_cam_rrfs_firewxnest_grid2obs.sh
@@ -1,18 +1,19 @@
-#PBS -N jevs_plots_cam_nam_firewxnest_grid2obs_last31days
+#PBS -N jevs_stats_cam_rrfs_firewxnest_grid2obs_00
 #PBS -j oe
 #PBS -S /bin/bash
 #PBS -q "dev"
 #PBS -A VERF-DEV
 #PBS -l walltime=00:15:00
-#PBS -l place=shared,select=1:ncpus=1:mem=100GB
+#PBS -l place=shared,select=1:ncpus=1:mem=2GB
 #PBS -l debug=true
 
 export model=evs
 
+cd $PBS_O_WORKDIR
+
 export HOMEevs=/lfs/h2/emc/vpppg/noscrub/$USER/EVS
 
 source $HOMEevs/versions/run.ver
-
 
 ############################################################
 # Load modules
@@ -26,18 +27,16 @@ export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
 export KEEPDATA=NO
 export SENDMAIL=YES
-export SENDDBN=NO
 
-export vhr=00
-export envir=prod
+export vhr
 export NET=evs
-export STEP=plots
+export STEP=stats
 export COMPONENT=cam
 export RUN=atmos
 export VERIF_CASE=grid2obs
-export MODELNAME=nam_firewxnest
-export modsys=nam
-export mod_ver=${nam_ver}
+export MODELNAME=rrfs_firewxnest
+export modsys=rrfs
+export mod_ver=${rrfs_ver}
 
 source $HOMEevs/dev/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
 evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
@@ -46,14 +45,16 @@ export job=${PBS_JOBNAME:-jevs_${STEP}_${MODELNAME}_${VERIF_CASE}}
 export jobid=$job.${PBS_JOBID:-$$}
 
 export COMIN=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d
-export COMOUT=/lfs/h2/emc/ptmp/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
+export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
+
+export MAILTO=${MAILTO:-'marcel.caron@noaa.gov,andrew.benjamin@noaa.gov'}
 
 # CALL executable job script here
-$HOMEevs/jobs/JEVS_PLOTS_CAM
+$HOMEevs/jobs/JEVS_STATS_CAM
 
 ######################################################################
-## Purpose: This job will generate the grid2obs plots for the NAM_FIREWXNEST
-##          model and generate plots tar files.
+## Purpose: This job will generate the grid2obs statistics for the RRFS_FIREWXNEST
+##          model and generate stat files.
 #######################################################################
 #
 exit
