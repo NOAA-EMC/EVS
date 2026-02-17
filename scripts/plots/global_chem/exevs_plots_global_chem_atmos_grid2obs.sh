a8e995098a8f64fe5df24bb48761c7dbb8d5ad56
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2026-02-17 16:59:10 +0000

    remove log message

--- scripts/plots/global_chem/exevs_plots_global_chem_atmos_grid2obs.sh
+++ scripts/plots/global_chem/exevs_plots_global_chem_atmos_grid2obs.sh
@@ -9,7 +9,6 @@
 #                    
 #    05/22/2025  Ho-Chun Huang    move from global_ens to global_chem
 #    09/02/2025  Ho-Chun Huang    move cpreq to cp -v to comply with EE2
-#    02/17/2026  Ho-Chun Huang    modified for GCAFSv1
 ###############################################################################
 
 set -x
