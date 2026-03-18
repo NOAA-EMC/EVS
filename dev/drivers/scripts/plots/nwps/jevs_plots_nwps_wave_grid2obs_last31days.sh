f746b74013c6c91a467ccde69b206f178fc60e1b
Samira Ardani <samira.ardani@noaa.gov>
2026-03-18 13:07:09 +0000

    Updated the scripts.

--- dev/drivers/scripts/plots/nwps/jevs_plots_nwps_wave_grid2obs_last31days.sh
+++ dev/drivers/scripts/plots/nwps/jevs_plots_nwps_wave_grid2obs_last31days.sh
@@ -4,7 +4,7 @@
 #PBS -q dev
 #PBS -A VERF-DEV
 #PBS -l walltime=02:30:00
-#PBS -l place=vscatter,select=1:ncpus=48:mem=50G
+#PBS -l place=vscatter,select=2:ncpus=100:mem=100G
 #PBS -l debug=true
 
 set -x
