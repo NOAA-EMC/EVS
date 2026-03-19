1de7f1cfac8b29e34978d9d9d65908f2ef4232af
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2026-03-19 18:39:06 +0000

    adjust walltime based on info from daily cronjob run

--- dev/drivers/scripts/plots/cam/jevs_plots_cam_rrfs_chem_grid2obs_hourly_last31days.sh
+++ dev/drivers/scripts/plots/cam/jevs_plots_cam_rrfs_chem_grid2obs_hourly_last31days.sh
@@ -3,8 +3,8 @@
 #PBS -S /bin/bash
 #PBS -q "dev"
 #PBS -A VERF-DEV
-#PBS -l walltime=00:45:00
-#PBS -l place=vscatter:exclhost,select=5:ncpus=128:ompthreads=1:mem=275GB
+#PBS -l walltime=01:20:00
+#PBS -l place=vscatter:exclhost,select=5:ncpus=128:ompthreads=1:mem=500GB
 #PBS -l debug=true
 
 set -x
