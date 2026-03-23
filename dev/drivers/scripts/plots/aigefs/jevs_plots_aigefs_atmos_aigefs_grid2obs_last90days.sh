1c82e4c898c3ad72e191181662d7a28d548ec886
L. Gwen Chen <lichuan.chen@noaa.gov>
2026-03-23 15:26:26 +0000

    Add plots jobs and scripts

---
+++ dev/drivers/scripts/plots/aigefs/jevs_plots_aigefs_atmos_aigefs_grid2obs_last90days.sh
@@ -0,0 +1,44 @@
+#PBS -N jevs_plots_aigefs_atmos_aigefs_grid2obs_last90days
+#PBS -j oe
+#PBS -S /bin/bash
+#PBS -q dev
+#PBS -A VERF-DEV
+#PBS -l walltime=00:25:00
+#PBS -l place=vscatter:exclhost,select=4:ncpus=40:mpiprocs=40:mem=70GB:prepost=true
+#PBS -l debug=true
+
+set -x
+export OMP_NUM_THREADS=1
+export HOMEevs=/lfs/h2/emc/vpppg/noscrub/${USER}/EVS
+source $HOMEevs/versions/run.ver
+
+export envir=prod
+export NET=evs
+export STEP=plots
+export COMPONENT=aigefs
+export RUN=atmos
+export MODELNAME=aigefs
+export VERIF_CASE=grid2obs
+
+module reset
+module load prod_envir/${prod_envir_ver}
+source $HOMEevs/dev/modulefiles/$COMPONENT/${COMPONENT}_${STEP}.sh
+
+evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
+
+export vhr=00
+export past_days=90
+export valid_time=both
+
+export COMIN=/lfs/h2/emc/vpppg/noscrub/${USER}/$NET/$evs_ver_2d
+export COMOUT=/lfs/h2/emc/ptmp/${USER}/$NET/$evs_ver_2d
+export DATAROOT=/lfs/h2/emc/stmp/${USER}/evs_test/$envir/tmp
+
+export job=${PBS_JOBNAME:-jevs_${STEP}_${MODELNAME}_${VERIF_CASE}}
+export jobid=$job.${PBS_JOBID:-$$}
+
+export KEEPDATA=NO
+export SENDDBN=NO
+export SENDMAIL=NO
+
+${HOMEevs}/jobs/JEVS_PLOTS_AIGEFS
---
+++ dev/modulefiles/aigefs/aigefs_plots.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# modulefile for EVS aigefs component, plots step
+
+set -x
+
+module load PrgEnv-intel/${PrgEnvintel_ver}
+module load intel/${intel_ver}
+module load ve/evs/${ve_evs_ver}
+module load gsl/${gsl_ver}
+module load prod_util/${prod_util_ver}
+module load libjpeg/${libjpeg_ver}
+module load libpng/${libpng_ver}
+module load zlib/${zlib_ver}
+module load jasper/${jasper_ver}
+module load udunits/${udunits_ver}
+module load cray-pals/${craypals_ver}
+module load cfp/${cfp_ver}
+module load imagemagick/${imagemagick_ver}
+
+export SIPHONROOT=${UTILROOT}/fakedbn
+export DBNROOT=$SIPHONROOT
+
+module list
+
+set -x
+
