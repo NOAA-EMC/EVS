685d21a604a9a20dea80df06ce5e308c292653df
marcel caron <marcel.caron@clogin01.cactus.wcoss2.ncep.noaa.gov>
2026-05-04 15:45:59 +0000

    minor updates to precip prep (rrfs)

--- dev/drivers/scripts/prep/cam/jevs_prep_cam_rrfs_precip.sh
+++ dev/drivers/scripts/prep/cam/jevs_prep_cam_rrfs_precip.sh
@@ -12,6 +12,7 @@ export model=evs
 export machine=WCOSS2
 
 # ECF Settings
+export SENDMAIL=YES
 export SENDECF=YES
 export SENDCOM=YES
 export KEEPDATA=NO
@@ -48,6 +49,7 @@ export envir=prod
 export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
 export COMOUT=/lfs/h2/emc/vpppg/noscrub/$USER/$NET/$evs_ver_2d/$STEP/$COMPONENT
 export vhr=${vhr:-${vhr}}
+export MAILTO="andrew.benjamin@noaa.gov,marcel.caron@noaa.gov"
 
 # Job Settings and Run
 . ${HOMEevs}/jobs/JEVS_PREP_CAM
