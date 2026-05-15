5ee006235def71f003d3b7882412d7287ce0081d
marcel caron <marcel.caron@clogin02.cactus.wcoss2.ncep.noaa.gov>
2026-05-15 16:26:32 +0000

    Add "place=shared"

--- dev/drivers/scripts/prep/cam/jevs_prep_cam_hrrr_precip.sh
+++ dev/drivers/scripts/prep/cam/jevs_prep_cam_hrrr_precip.sh
@@ -4,7 +4,7 @@
 #PBS -q dev
 #PBS -A VERF-DEV
 #PBS -l walltime=00:15:00
-#PBS -l select=1:ncpus=3:ompthreads=1:mem=90GB
+#PBS -l place=shared,select=1:ncpus=3:ompthreads=1:mem=90GB
 #PBS -l debug=true
 
 set -x
