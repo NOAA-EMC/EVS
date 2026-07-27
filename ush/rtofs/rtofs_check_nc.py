66e46ae63170167690cf639be2871f1079e30e20
SamiraArdani-NOAA <samira.ardani@noaa.gov>
2026-07-27 15:35:21 +0000

    Made the print statement more meaningful.

--- ush/rtofs/rtofs_check_nc.py
+++ ush/rtofs/rtofs_check_nc.py
@@ -65,12 +65,12 @@ def rtofs_check_nc(filepath, target=None, file_type='fcst'):
         except subprocess.CalledProcessError:
             # If ANY primary variable is missing or corrupted -> FAIL
             status = 1
-            print(status)
+            print('Data is corrupted.')
             return status
 
     # 5. All required variables passed successfully -> PASS
     status = 0
-    print(status)
+    print('Data is passed successfully.')
     return status
 
 
