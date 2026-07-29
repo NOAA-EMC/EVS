ba8347e5e02469d90c718a8f5db0aba71379cdca
Mallory Row <mallory.row@noaa.gov>
2026-07-29 13:22:25 +0000

    Update accounts

--- dev/drivers/scripts/plots/aigefs/jevs_plots_aigefs_atmos_aigefs_grid2obs_last90days.sh
+++ dev/drivers/scripts/plots/aigefs/jevs_plots_aigefs_atmos_aigefs_grid2obs_last90days.sh
@@ -2,7 +2,7 @@
 #PBS -j oe
 #PBS -S /bin/bash
 #PBS -q dev
-#PBS -A VERF-DEV
+#PBS -A EVS-DEV
 #PBS -l walltime=00:25:00
 #PBS -l place=vscatter:exclhost,select=4:ncpus=40:mpiprocs=40:mem=70GB:prepost=true
 #PBS -l debug=true
