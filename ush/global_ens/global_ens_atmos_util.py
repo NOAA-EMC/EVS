2d0b50e09a14c22d2a8badd1d327c1df81bd26ac
Mallory Row <mallory.row@noaa.gov>
2026-08-26 17:04:48 +0000

    Make updates for AMSR3 for global_ens

--- ush/global_ens/global_ens_atmos_util.py
+++ ush/global_ens/global_ens_atmos_util.py
@@ -785,7 +785,7 @@ def prep_prod_osi_saf_file(daily_source_file_format, daily_dest_file,
     for hem in ['nh', 'sh']:
         hem_source_file = daily_source_file_format.replace('{hem?fmt=str}',
                                                            hem)
-        hem_dest_file = daily_dest_file.replace('amsr2.', 'amsr2.'+hem+'.')
+        hem_dest_file = daily_dest_file.replace('amsr3.', 'amsr3.'+hem+'.')
         hem_prepped_file = os.path.join(os.getcwd(), 'atmos.'
                                         +hem_dest_file.rpartition('/')[2])
         if check_file_exists_size(hem_source_file):
